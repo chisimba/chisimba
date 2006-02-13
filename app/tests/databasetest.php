2c9e025d0ba25eb2f4084ed4d5cf13c4bfbdd511
paulscott <paulscott@gmail.com>
2006-02-13 05:28:11 +0000

    added engine test

--- app/tests/databasetest.php
+++ app/tests/databasetest.php
@@ -6,5 +6,9 @@ require_once 'PHPUnit2/Framework/TestCase.php';
 //Set up the class to do the tests
 class DatabaseTests extends PHPUnit2_Framework_TestCase
 {
+    public function testDBConnIsValid()
+    {
+        $this->assertEquals(true, $this->connection);
 
+    }
 }
