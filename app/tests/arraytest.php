536d76062239c645c3e47a276b5e76931e337837
paulscott <paulscott@gmail.com>
2006-02-13 00:37:54 +0000

    arraytest

--- app/tests/arraytest.php
+++ app/tests/arraytest.php
@@ -3,7 +3,7 @@
 require_once 'PHPUnit2/Framework/TestCase.php';
 
 //Set up the class to do the tests
-class ExampleTest extends PHPUnit2_Framework_TestCase
+class arrayTest extends PHPUnit2_Framework_TestCase
 {
     /**
      * Method to do the test
@@ -19,12 +19,20 @@ class ExampleTest extends PHPUnit2_Framework_TestCase
      * }
      */
 
-    public function testExample()
+    public function testArray()
     {
         //create the fixture
-        $fixture = NULL;
+        $fixture = array();
         //assert that the fixture is NULL
-        $this->assertEquals(NULL, $fixture);
+        $this->assertEquals(0, $fixture);
+    }
+
+    public function testArrayNotEmpty()
+    { 
+        //create the fixture
+        $fixture = array();
+        $fixture[] = "Element";
+        $this->assertEquals(1,$fixture);
     }
 
 }
