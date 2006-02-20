3ce18e68320adb401026fdcd6549ea6d6954d23d
paulscott <paulscott@gmail.com>
2006-02-20 03:59:40 +0000

    Logging file for debug

---
+++ app/lib/logging.php
@@ -0,0 +1,31 @@
+<?php
+
+$enable_debug_logging = FALSE;
+
+if ($enable_debug_logging)
+{
+    require_once 'Log.php';
+    
+    $conf = array('mode' => 0644, 'timeFormat' => '%Y-%m-%d %H:%M:%S');
+    $log = &Log::singleton('file', '/tmp/kewl.log', 'kewl', $conf);
+    $GLOBALS['DEBUG_LOG_OBJ'] = $log;
+    
+    function log_debug($str)
+    {
+        ob_start();
+        print_r($str);
+        $logstr = ob_get_contents();
+        ob_end_clean();
+        
+        $logger = $GLOBALS['DEBUG_LOG_OBJ'];
+        $logger->log($logstr, LOG_DEBUG);
+    }
+}
+else
+{
+    function log_debug($str)
+    {
+    }
+}
+
+?>
