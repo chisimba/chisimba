980d3add8eebb32562a95bf210863e9677e269fb
paulscott <paulscott@gmail.com>
2006-02-20 04:32:45 +0000

    index file

--- app/index.php
+++ app/index.php
@@ -1,4 +1,4 @@
-<?PHP
+<?php
 
 /**
 * index.php
@@ -9,21 +9,21 @@
 *
 */
 // checks for configuration file, if none found loads installation page
-//if ( !file_exists( 'config/config_db_inc.php' ) ) {
-//        header( 'Location: install/index.php' );
-//        exit();
-//}
+if ( !file_exists( 'config/config_db_inc.php' ) ) {
+	header( 'Location: install/index.php' );
+	exit();
+}
 // this is a security measure, this can be checked by the included scripts and the script
 // execution aborted if it is not set.
 $GLOBALS['kewl_entry_point_run'] = true;
 
-// initialise error handling
-//require('lib/errorhandling/ErrorHandler.inc');
-//$error =& new ErrorHandler();
-
+// initialise error handling 
+//require('lib/errorhandling/ErrorHandler.inc'); 
+//$error =& new ErrorHandler(); 
+ 
 // initialise the engine object
 require_once 'classes/core/engine_class_inc.php';
-$_globalObjEngine = new engine;
+$_globalObjEngine =& new engine;
 
 //log_debug("page rendering started at ".strftime("%T"));
 
