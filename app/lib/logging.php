1b6118ac9aa735acc51f1e2c69a454560ee0b8b2
paulscott <paulscott@gmail.com>
2006-03-03 00:43:29 +0000

    changed file path to /lib within src tree. Not sure if this works....

--- app/lib/logging.php
+++ app/lib/logging.php
@@ -5,18 +5,18 @@ $enable_debug_logging = FALSE;
 if ($enable_debug_logging)
 {
     require_once 'Log.php';
-    
+
     $conf = array('mode' => 0644, 'timeFormat' => '%Y-%m-%d %H:%M:%S');
-    $log = &Log::singleton('file', '/tmp/kewl.log', 'kewl', $conf);
+    $log = &Log::singleton('file', 'kewl.log', 'kewl', $conf);
     $GLOBALS['DEBUG_LOG_OBJ'] = $log;
-    
+
     function log_debug($str)
     {
         ob_start();
         print_r($str);
         $logstr = ob_get_contents();
         ob_end_clean();
-        
+
         $logger = $GLOBALS['DEBUG_LOG_OBJ'];
         $logger->log($logstr, LOG_DEBUG);
     }
