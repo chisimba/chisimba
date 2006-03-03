63a0032fb729a29f13e9fb8e26e973c9495f29f4
paulscott <paulscott@gmail.com>
2006-03-03 11:59:24 +0000

    changed default connection to mysqli

--- app/config/config_db_inc.php
+++ app/config/config_db_inc.php
@@ -1,5 +1,5 @@
-<?PHP 
-/** 
+<?PHP
+/**
 * Database configuration information for the main
 * KEWL.NextGen database.
 */
@@ -9,7 +9,7 @@ define('KEWL_SERVERNAME', 'gen6Srv42Nme32');
 define('KEWL_MAINSERVERNAME', 'http://kngforge.uwc.ac.za/kng_unstable');
 // The server name was generated
 define('KEWL_SERVERNAME_GENERATED', 'TRUE');
-define('KEWL_DB_DRIVER', 'mysql');
+define('KEWL_DB_DRIVER', 'mysqli');
 define('KEWL_DB_SERVER', 'localhost');
 define('KEWL_DB_USER', 'root');
 define('KEWL_DB_PASSWORD', '');
@@ -19,7 +19,7 @@ define('KEWL_DB_DATABASE', 'php5alpha');
 * Use to provide the capability to have a second
 * database for read only queries, eg. students reading
 * content within a course
-*/ 
+*/
 define('KEWL_RODB_DRIVER', 'mysql');
 define('KEWL_RODB_SERVER', 'somesite.somewhere.com');
 define('KEWL_RODB_USER', 'kngro');
@@ -28,7 +28,7 @@ define('KEWL_RODB_DATABASE', 'dbkewl_readonly');
 
 /**
 * LDAP configuration
-*/ 
+*/
 $ldap_driver="ldap";
 $ldap_ip="191.10.9.8";
 $ldap_port="389";
