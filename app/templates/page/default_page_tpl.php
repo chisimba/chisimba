3b7dcce4f3b8c0280ddcca3181e8d7d4690c030f
paulscott <paulscott@gmail.com>
2006-06-05 07:41:39 +0000

    validating for w3c

--- app/templates/page/default_page_tpl.php
+++ app/templates/page/default_page_tpl.php
@@ -10,7 +10,7 @@ if (!isset($pageLanguage)) {
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang='<?php echo $pageLanguage; ?>'>
-<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-8">
+<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-8"></META>
 <head>
 <title><?php echo $objConfig->getsiteName(); ?></title>
 <?php
