6d2272a99393270a77eab1716a41669964b981f1
paulscott <paulscott@gmail.com>
2006-07-03 20:29:01 +0000

    changed absmiddle to middle to conform with w3c standards

--- app/core_modules/htmlelements/classes/dropdown_class_inc.php
+++ app/core_modules/htmlelements/classes/dropdown_class_inc.php
@@ -109,7 +109,7 @@ class dropdown extends htmlbase
 	{
 		$str.='<option value="'.$opt.'"';
 		if($this->selected==$opt){
-			$str.=' selected="true"';
+			$str.=' selected="selected"';
 		}
 		$str.='>';
 		$str.=$lbl;
