f89884b726ea9c6bbd3e3d3631827457a87e9b6c
paulscott <paulscott@gmail.com>
2006-07-04 12:11:37 +0000

    enabled security

--- app/core_modules/utilities/classes/jsalerts_class_inc.php
+++ app/core_modules/utilities/classes/jsalerts_class_inc.php
@@ -1,10 +1,10 @@
 <?php
 
 // security check - must be included in all scripts
-//if (!$GLOBALS['kewl_entry_point_run'])
-//{
-//    die("You cannot view this page directly");
-//}
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+    die("You cannot view this page directly");
+}
 // end security check
 
 /**
