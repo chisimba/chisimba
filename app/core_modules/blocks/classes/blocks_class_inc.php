29c0f646efcfacc7c7dc804f368dcdef7893570e
paulscott <paulscott@gmail.com>
2006-07-04 17:20:50 +0000

    added exception handling

--- app/core_modules/blocks/classes/blocks_class_inc.php
+++ app/core_modules/blocks/classes/blocks_class_inc.php
@@ -35,7 +35,15 @@ class blocks extends object
     {
         //Create an instance of the modulesadmin class for checking
         // if a module is registered
-        $this->objModule=&$this->getObject('modulesadmin','modulelist');
+        try {
+        	$this->objModule=&$this->getObject('modulesadmin','modulelist');
+        }
+        catch (customException $e)
+        {
+        	echo customException::cleanUp($e);
+        	die();
+        }
+
     }
 
     /**
@@ -70,12 +78,12 @@ class blocks extends object
             $title = $objBlock->title;
             $objWrap = & $this->getObject('trimstr', 'strings');
             $title = $objWrap->wrapString($title, 20);
-            
+
             switch ($blockType) {
                 case NULL:
                 	$objFeatureBox = & $this->newObject('featurebox', 'navigation');
                 	return $objFeatureBox->show($title, $objBlock->show());
-                
+
                 case "tabbedbox":
                     //Put it all inside a tabbed box
                     $objTab = $this->newObject('tabbedbox', 'htmlelements');
