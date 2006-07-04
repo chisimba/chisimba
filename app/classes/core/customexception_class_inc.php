76c6ca76b069d5852f58fcb4214f64fe1f534dad
paulscott <paulscott@gmail.com>
2006-07-04 10:34:59 +0000

    class to handle exceptions in a non-standard way

---
+++ app/classes/core/customexception_class_inc.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class that extends the SPL to handle exceptions in a custom way
+ * 
+ * @author Paul Scott
+ * @package core
+ * @copyright AVOIR GNU/GPL
+ */
+
+class customException extends Exception 
+{
+	
+}
