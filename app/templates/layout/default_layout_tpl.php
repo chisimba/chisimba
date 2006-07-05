38aa0db2f441b20a763943d13a6dba622be14f1b
paulscott <paulscott@gmail.com>
2006-07-05 06:04:01 +0000

    removed tidy bits

--- app/templates/layout/default_layout_tpl.php
+++ app/templates/layout/default_layout_tpl.php
@@ -1,17 +1,17 @@
 <?php
 //call on tidy to clean up...
 // Specify tidy configuration
-$config = array(
-         'indent'        => true,
-         'output-xhtml'  => true,
-         'wrap'          => 200);
+//$config = array(
+//         'indent'        => true,
+//         'output-xhtml'  => true,
+//         'wrap'          => 200);
 
 // Tidy
-$tidy = new tidy;
+//$tidy = new tidy;
 $output = $this->getContent().$this->footerStr;
-$tidy->parseString($output, $config, 'utf8');
-$tidy->cleanRepair();
+//$tidy->parseString($output, $config, 'utf8');
+//$tidy->cleanRepair();
 
-echo $tidy;
+echo $output;
 
 ?>
