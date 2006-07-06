78dad38c97d719bc2d8fbdeab933c67b3c38d313
paulscott <paulscott@gmail.com>
2006-07-06 09:43:57 +0000

    Lucene search engine start

---
+++ app/core_modules/lucene/classes/lucenedocument_class_inc.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *  Document indexer Lucene integration class
+ *
+ * @package lucene
+ * @category Chisimba
+ * @author Paul Scott
+ * @copyright AVOIR UWC GNU/GPL
+ * @filesource
+ */
+
+class lucenedocument extends object
+{
+	/**
+ 	 * A Document is a set of fields. Each field has a name and a textual value.
+ 	 */
+
+}
