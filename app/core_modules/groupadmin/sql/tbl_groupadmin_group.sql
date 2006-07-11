78b37bb9efd3a61dbf1f09d75b57bac08008e35e
tohir <tohir@gmail.com>
2006-07-11 07:48:24 +0000

    5ive version of sql

--- app/core_modules/groupadmin/sql/tbl_groupadmin_group.sql
+++ app/core_modules/groupadmin/sql/tbl_groupadmin_group.sql
@@ -1,4 +1,5 @@
 <?php
+/*
  $sqldata[] = "CREATE TABLE tbl_groupadmin_group (
   id VARCHAR(32) NOT NULL,
   parent_id VARCHAR(32) NULL,
@@ -20,4 +21,48 @@ $sqldata[]=sprintf("insert into `tbl_groupadmin_group` (id, parent_id, name, des
             values ('init_3', NULL , 'Students', 'The site wide Students user group', '%s', '1' )", date("Y:m:d H:i:s") );
 $sqldata[]=sprintf("insert into `tbl_groupadmin_group` (id, parent_id, name, description, last_updated, last_updated_by )
             values ('init_4', NULL , 'Guest', 'The site wide Guest user group', '%s', '1' )", date("Y:m:d H:i:s") );
+*/
+// Table Name
+$tablename = 'tbl_groupadmin_group';
+
+//Options line for comments, encoding and character set
+$options = array('comment' => 'The tbl_groupadmin_groups table is managed by the groupadmin module, its purpose to allow for groups and subgroups. This allows for users to have a context ( ie. In KNG, the permissions module will require information about the users context )', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+// Fields
+$fields = array(
+	'id' => array(
+		'type' => 'text',
+		'length' => 32
+		),
+	'parent_id' => array(
+		'type' => 'text',
+		'length' => 32
+		),
+    'name' => array(
+		'type' => 'text',
+		'length' => 32
+		),
+    'description' => array(
+		'type' => 'text',
+		'length' => 100
+		),
+    'last_updated' => array(
+		'type' => 'datetime',
+		'length' => 100
+		),
+    'last_updated_by' => array(
+		'type' => 'text',
+		'length' => 32
+		)
+    );
+
+//create other indexes here...
+
+$name = 'ind_groups_FK';
+
+$indexes = array(
+                'fields' => array(
+                	'parent_id' => array()
+                )
+        );
 ?>
--- app/core_modules/groupadmin/sql/tbl_groupadmin_groupuser.sql
+++ app/core_modules/groupadmin/sql/tbl_groupadmin_groupuser.sql
@@ -23,4 +23,47 @@ $sqldata[] = "CREATE TABLE tbl_groupadmin_groupuser (
  ) TYPE = INNODB COMMENT = 'This is the bridge table between user and group table';";
 $sqldata[]=sprintf("insert into `tbl_groupadmin_groupuser` (id, group_id, user_id, last_updated, last_updated_by )
             values ( 'init_1', 'init_1', 'init_1', '%s', '1' )", date("Y:m:d H:i:s"));
+            
+// Table Name
+$tablename = 'tbl_groupadmin_groupuser';
+
+//Options line for comments, encoding and character set
+$options = array('comment' => 'This is the bridge table between user and group table', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+// Fields
+$fields = array(
+	'id' => array(
+		'type' => 'text',
+		'length' => 32
+		),
+	'group_id' => array(
+		'type' => 'text',
+		'length' => 32,
+        'notnull' => TRUE
+		),
+    'user_id' => array(
+		'type' => 'text',
+		'length' => 32,
+        'notnull' => TRUE
+		),
+    'last_updated' => array(
+		'type' => 'datetime',
+		'length' => 100
+		),
+    'last_updated_by' => array(
+		'type' => 'text',
+		'length' => 32
+		)
+    );
+
+//create other indexes here...
+
+$name = 'ind_groupuser_FK';
+
+$indexes = array(
+                'fields' => array(
+                	'group_id' => array(),
+                    'user_id' => array(),
+                )
+        );
 ?>
