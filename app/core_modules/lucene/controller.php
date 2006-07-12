47436a6abd54e465b1267ff85b8822ebbe7c61cb
paulscott <paulscott@gmail.com>
2006-07-12 10:54:49 +0000

    lucene controller

---
+++ app/core_modules/lucene/controller.php
@@ -0,0 +1,40 @@
+<?php
+
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Controller class for the Lucene implementation of Chisimba
+ *
+ * @author Paul Scott
+ * @copyright AVOIR UWC
+ * @access public
+ * @package lucene
+ */
+
+class lucene extends controller
+{
+	public $indexer;
+	public $indexPath;
+	public $index;
+	public $doc;
+	/**
+	 * Constructor
+	 */
+	public function init()
+	{
+		// instantiate objects
+        try{
+			//the language object
+        	$this->objLanguage = $this->getObject('language','language');
+
+        }catch (customException $e){
+       		echo customException::cleanUp($e);
+        	exit();
+        }
+
+	}
+}
