dea5380d759e83497195d705e468a51e5d658cea
paulscott <paulscott@gmail.com>
2006-07-16 10:03:11 +0000

    removed old sql

--- app/core_modules/security/sql/core_tbl_userloginhistory.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tbl_userloginhistory` (
-  `id` varchar(32) NOT NULL,
-  `userId` char(25) NOT NULL default '0',
-  `lastLoginDateTime` datetime NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`id`),
-  KEY `userId` (`userId`)
-) TYPE=InnoDB  ROW_FORMAT=FIXED COMMENT='Used to hold the loginhistory of a user';
