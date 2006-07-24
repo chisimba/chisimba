d959b2065accfa72af79dee3334e1866cd662e06
tohir <tohir@gmail.com>
2006-07-24 08:27:21 +0000

    added class to format the size of file into a more userfriendly way

---
+++ app/core_modules/files/classes/formatfilesize_class_inc.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* Format File Size
+* Convert the size in bytes for a file into a more user-friendly format 
+* http://www.drquincy.com/resources/code/php/formatfilesize/
+*/
+class formatfilesize
+{
+    /**
+    * Constructor
+    */
+    function formatfilesize()
+    {  }
+    
+    /**
+    * Method to format the size of a file
+    * @param int $size Size of the file
+    * @return string Formatted Size of file
+    */
+    function formatsize($size)
+    {
+        // bytes
+        if( $size < 1024 ) {
+            return $size . " bytes";
+        }
+        // kilobytes
+        else if( $data < 1024000 ) {
+            return round( ( $size / 1024 ), 1 ) . "k";
+        }
+        // megabytes
+        else {
+            return round( ( $size / 1024000 ), 1 ) . " MB";
+        }
+    }
+    
+}
+?>
