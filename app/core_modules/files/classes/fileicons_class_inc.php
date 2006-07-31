e29f560d6831b849545c70a69860aa3874c9b6d9
tohir <tohir@gmail.com>
2006-07-31 05:44:42 +0000

    added more file icons

--- app/core_modules/files/classes/fileicons_class_inc.php
+++ app/core_modules/files/classes/fileicons_class_inc.php
@@ -69,24 +69,44 @@ class fileicons extends object
         switch (strtolower($extension))
         {
             case 'ace' : $imagename = 'winace'; break;
+            case 'ai' : $imagename = 'ai'; break;
+            case 'asx':
             case 'asf' : $imagename = 'asf'; break;
             case 'avi' : $imagename = 'avi'; break;
             case 'bat' : $imagename = 'bat'; break;
             case 'bmp' : $imagename = 'bmp'; break;
             case 'chm' : $imagename = 'chm'; break;
+            case 'csv' : $imagename = 'csv'; break;
+            case 'dbf' : $imagename = 'dbf'; break;
+            case 'dll' : $imagename = 'dll'; break;
             case 'doc' : $imagename = 'doc'; $filedesc = 'MS Word Document'; break;
             case 'eml' : $imagename = 'eml'; break;
+            case 'eps' : $imagename = 'eps'; break;
             case 'exe' : $imagename = 'exe'; break;
             case 'gif' : $imagename = 'gif'; break;
+            case 'gz' : $imagename = 'tgz'; break; // Tar.gz
             case 'hlp' : $imagename = 'hlp'; break;
             case 'html' :
             case 'htm' : $imagename = 'htm'; $filedesc = 'HTML Document'; break;
             case 'java' : $imagename = 'java'; break;
             case 'jpg' : $imagename = 'jpg'; break;
             case 'mdb' : $imagename = 'mdb'; break;
+            case 'mht' : $imagename = 'mht'; break;
+            case 'mm' : $imagename = 'freemind'; break;
             case 'mov' : $imagename = 'mov'; break;
             case 'mp3' : $imagename = 'mp3'; break;
             case 'mpg' : $imagename = 'mpg'; break;
+            case 'odb' : $imagename = 'odb'; break;
+            case 'odf' : $imagename = 'odf'; break;
+            case 'odg' : $imagename = 'odg'; break;
+            case 'odm' : $imagename = 'odm'; break;
+            case 'odp' : $imagename = 'odp'; break;
+            case 'odt' : $imagename = 'odt'; break;
+            case 'otg' : $imagename = 'otg'; break;
+            case 'oth' : $imagename = 'oth'; break;
+            case 'otp' : $imagename = 'otp'; break;
+            case 'ots' : $imagename = 'ots'; break;
+            case 'ott' : $imagename = 'ott'; break;
             case 'ogg' : $imagename = 'ogg'; break;
             case 'pdf' : $imagename = 'pdf'; break;
             case 'phps' :
@@ -94,8 +114,11 @@ class fileicons extends object
             case 'png' : $imagename = 'png'; break;
             case 'pps' : $imagename = 'pps'; break;
             case 'ppt' : $imagename = 'ppt'; break;
-            case 'mpp' : $imagename = 'project'; break;
+            case 'psd' : $imagename = 'psd'; break;
+            case 'mid' : $imagename = 'mid'; break;
+            case 'mpp' : $imagename = 'mpp'; break;
             case 'ps' : $imagename = 'ps'; break;
+            case 'py' : $imagename = 'python'; break;
             case 'quark' : $imagename = 'quark'; break; // check right extension
             case 'rar' : $imagename = 'rar'; break;
             case 'ra':
@@ -108,15 +131,18 @@ class fileicons extends object
             case 'sxi' : $imagename = 'oo-impress'; break;
             case 'sxw' : $imagename = 'oo-write'; break;
             case 'tgz' : $imagename = 'tgz'; break;
+            case 'tif' :
+            case 'tiff' : $imagename = 'tif'; break;
             case 'ttf' : $imagename = 'ttf'; break;
             case 'txt' : $imagename = 'txt'; break;
             case 'viewlet' : $imagename = 'viewlet'; break; // check right extension
             case 'vsd' : $imagename = 'visio'; $filedesc = 'MS Visio Document'; break;
             case 'wav' : $imagename = 'wav'; break;
-            case 'wma' : $imagename = 'wma'; break;
+            case 'wma' :
             case 'wmv' : $imagename = 'wmv'; break;
             case 'wri' : $imagename = 'wri'; break;
             case 'xls' : $imagename = 'xls'; break;
+            case 'xlt' : $imagename = 'xlt'; break;
             case 'xml' : $imagename = 'xml'; break;
             case 'zip' : $imagename = 'zip'; break;
             default : $imagename = 'clear'; // shows a transparent 16 x 16 image
