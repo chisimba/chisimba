8b5ee35c9998da1c3c3e2c8b28e863141ef1f99d
tohir <tohir@gmail.com>
2006-07-31 06:10:48 +0000

    moved methods from datetime to here

--- app/core_modules/utilities/classes/datetime_class_inc.php
+++ app/core_modules/utilities/classes/datetime_class_inc.php
@@ -881,6 +881,44 @@ class datetime extends object
         }
         return $format;
     }
+    
+    /**
+    * Method to format date into an English Format
+    * Given 2006-07-20 10:57:38, it will return it as 20 July 2006
+    * @access public
+    * @param string $date Date Time String
+    * @return string Formatted Date
+    */
+    function formatDateOnly($date)
+    {
+        
+        if (isset($date)) {
+            $date = getdate(strtotime($date)); 
+            
+            return ($date['mday'].' '.$date['month'].' '. $date['year']);
+        }
+    }
+    
+    /**
+    * Method to format time
+    * Given 2006-07-20 13:26:38, it will return it as 13:26
+    * @access public
+    * @param string $time Date Time String
+    * @return string Formatted Rime
+    */
+    function formatTime($time)
+    {
+        $time = getdate(strtotime($time)); 
+        
+        // Check whether to add a zero prior to the minute.
+        if ($time['minutes'] < 10) {
+            $zeroes = '0';
+        } else {
+            $zeroes = '';
+        }
+        
+        return ($time['hours'].':'.$zeroes.$time['minutes']);
+    }
 
     /**
     * Simple function to time the execution of a script
