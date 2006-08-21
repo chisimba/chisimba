8c68cc917abdd386e3a7dbcb93aaf7b1fb02acbd
tohir <tohir@gmail.com>
2006-08-21 10:16:31 +0000

    fixed class

--- app/core_modules/filemanager/classes/registerfileusage_class_inc.php
+++ app/core_modules/filemanager/classes/registerfileusage_class_inc.php
@@ -1,8 +1,10 @@
 <?php
 /**
-
+* Class to Register File Usage as part of file tracking purposes
+*
+* @author Tohir Solomons
 */ 
-class registerfileusage extends object
+class registerfileusage extends dbTable
 {
 
     /**
@@ -15,7 +17,16 @@ class registerfileusage extends object
     }
     
     /**
-    * Method to generate a random color
+    * Method to Register Use of a File in a Column of a Table
+    *
+    * @param string $fileId Record Id of File to be tracked
+    * @param string $module Module the file will be used in
+    * @param string $table Name of the Table holding the file
+    * @param string $recordId Record Id of Record Using the File
+    * @param string $column Column of Record Using the File
+    * @param string $context Context File will be used in
+    * @param string $workgroup Workgroup the File will be used in
+    * @param boolean $fileLock Flag whether to apply a lock on the file - prevents file from being deleted
     * @return string Color in Hexadecimal format
     */
     public function registerUse($fileId, $module, $table, $recordId, $column, $context='', $workgroup='', $fileLock=FALSE)
@@ -48,9 +59,39 @@ class registerfileusage extends object
         return $insertId;
     }
     
+    /**
+    * Method to Parse through a text, find the files that are being used, and register them
+    *
+    * @param string $text Record Id of File to be tracked
+    * @param string $module Module the file will be used in
+    * @param string $table Name of the Table holding the file
+    * @param string $recordId Record Id of Record Using the File
+    * @param string $column Column of Record Using the File
+    * @param string $context Context File will be used in
+    * @param string $workgroup Workgroup the File will be used in
+    * @param boolean $fileLock Flag whether to apply a lock on the file - prevents file from being deleted
+    * @return string Color in Hexadecimal format
+    */
+    public function parseFilesUse($text, $module, $table, $recordId, $column, $context='', $workgroup='', $fileLock=FALSE)
+    {
+        // ToDo
+    }
+    
+    /**
+    * Method to deregister File Usage
+    * @param string $module Name of the Module using file
+    * @param string $table Name of the Table holding the file
+    * @param string $recordId Record Id of the Record in the table
+    */
     public function deregisterUse($module, $table, $recordId)
     {
-        //$list = $this->getAll(' WHERE 
+        $list = $this->getAll(' WHERE module="'.$module.'" AND tablename="'.$table.'" AND recordid="'.$recordId.'"');
+        foreach ($list as $item)
+        {
+            $this->delete('id', $item['id']);
+        }
+        
+        return;
     }
 
 }
