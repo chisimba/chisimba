e87b2d1ecc57d90e938c398dba2a9005d933b2d5
paulscott <paulscott@gmail.com>
2006-08-24 05:41:50 +0000

    fixed up controller case statements from ; to :

--- app/core_modules/logger/controller.php
+++ app/core_modules/logger/controller.php
@@ -44,77 +44,77 @@ class logger extends controller
                 return "menu_tpl.php";
                 break;
 
-            case 'sortbydate';
-            $ar = $this->showLog->showForUser(NULL, " ORDER BY dateCreated DESC");
-            $this->setVarByRef('ar', $ar);
-            return "main_tpl.php";
-            break;
+            case 'sortbydate':
+                $ar = $this->showLog->showForUser(NULL, " ORDER BY dateCreated DESC");
+                $this->setVarByRef('ar', $ar);
+                return "main_tpl.php";
+                break;
 
-        case 'sortbymodule';
-        $ar = $this->showLog->showForUser(NULL, " ORDER BY module");
-        $this->setVarByRef('ar', $ar);
-        return "main_tpl.php";
-        break;
+            case 'sortbymodule':
+                $ar = $this->showLog->showForUser(NULL, " ORDER BY module");
+                $this->setVarByRef('ar', $ar);
+                return "main_tpl.php";
+                break;
 
-    case 'showmoduleslogged';
-    $ar = $this->showLog->showModulesLogged();
-    $this->setVarByRef('ar', $ar);
-    return "modslogged_tpl.php";
-    break;
+            case 'showmoduleslogged':
+                $ar = $this->showLog->showModulesLogged();
+                $this->setVarByRef('ar', $ar);
+                return "modslogged_tpl.php";
+                break;
 
-case 'showstatsbyuser';
-$ar = $this->showLog->showStatsByUser();
-$this->setVarByRef('ar', $ar);
-return "main_tpl.php";
-break;
+            case 'showstatsbyuser':
+                $ar = $this->showLog->showStatsByUser();
+                $this->setVarByRef('ar', $ar);
+                return "main_tpl.php";
+                break;
 
-case 'showstatsbymodule';
-$ar = $this->showLog->showStatsByModule();
-$this->setVarByRef('ar', $ar);
-return "main_tpl.php";
-break;
-//Set of date cases
+            case 'showstatsbymodule':
+                $ar = $this->showLog->showStatsByModule();
+                $this->setVarByRef('ar', $ar);
+                return "main_tpl.php";
+                break;
+                //Set of date cases
+                
+            case 'showstatsbydate':
+                //Retrieve the timeframe parameter
+                $timeframe = $this->getParam("timeframe", NULL);
+                //Create and instance of the datepair class for getting timeframes
+                $objDate = $this->newObject('datepair', 'datetime');
+                switch ($timeframe) {
+                    case 'today':
+                    case NULL:
+                        //Return the datetime for now
+                        $now = time();
+                        $timeframe = date("Y/m/d", $now);
+                        break;
 
-case 'showstatsbydate';
-//Retrieve the timeframe parameter
-$timeframe = $this->getParam("timeframe", NULL);
-//Create and instance of the datepair class for getting timeframes
-$objDate = $this->newObject('datepair', 'datetime');
-switch ($timeframe) {
-    case 'today':
-    case NULL:
-        //Return the datetime for now
-        $now = time();
-        $timeframe = date("Y/m/d", $now);
-        break;
+                    case 'thisweek':
+                        //Return the datetime for now
+                        $objDate->thisWeek();
+                        $timeframe = $objDate->startDate;
+                        break;
 
-    case 'thisweek':
-        //Return the datetime for now
-        $objDate->thisWeek();
-        $timeframe = $objDate->startDate;
-        break;
+                    case 'thismonth':
+                        //Return the datetime for now
+                        $objDate->thisMonth();
+                        $timeframe = $objDate->startDate;
+                        break;
 
-    case 'thismonth':
-        //Return the datetime for now
-        $objDate->thisMonth();
-        $timeframe = $objDate->startDate;
-        break;
+                    default:
+                        die("Error: bad value for timeframe: ".$timeframe);
+                        break;
+                } // switch
+                $ar = $this->showLog->showStatsByDate($timeframe);
+                $this->setVarByRef('ar', $ar);
+                return "main_tpl.php";
+                break;
 
-    default:
-        die("Error: bad value for timeframe: ".$timeframe);
-        break;
-} // switch
-$ar = $this->showLog->showStatsByDate($timeframe);
-$this->setVarByRef('ar', $ar);
-return "main_tpl.php";
-break;
-
-default:
-    die($this->objLanguage->languageText("phrase_actionunknown") .": ".$action);
-    break;
-} //switch
-
-}
+            default:
+                die($this->objLanguage->languageText("phrase_actionunknown") .": ".$action);
+                break;
+        } //switch
+        
+    }
 } // end of class
 
 ?>
