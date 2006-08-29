cf190fd3e9edee1c3de391ec8799396abe0e9e5a
paulscott <paulscott@gmail.com>
2006-08-29 13:31:52 +0000

    added additional comments box and email capability

--- app/core_modules/errors/templates/content/dberror_tpl.php
+++ app/core_modules/errors/templates/content/dberror_tpl.php
@@ -1,6 +1,12 @@
 <?php
 $objFeatureBox = $this->newObject('featurebox', 'navigation');
 $userMenu  = &$this->newObject('usermenu','toolbar');
+$objTextArea = $this->loadclass('textarea', 'htmlelements');
+$objHiddenInput = $this->loadclass('hiddeninput', 'htmlelements');
+
+$objForm = new form('errormail',$this->uri(array('action'=>'mailall')));
+$objTextArea = new textarea('comments','');
+
 // Create an instance of the css layout class
 $cssLayout =& $this->newObject('csslayout', 'htmlelements');
 // Set columns to 2
@@ -23,6 +29,7 @@ $cssLayout->setLeftColumnContent($leftSideColumn);
 $this->href = $this->getObject('href', 'htmlelements');
 
 $devmsg = urldecode($devmsg);
+$objHiddenInput = new hiddeninput('error', htmlentities($devmsg));
 $usrmsg = urldecode($usrmsg);
 $devmsg = nl2br($devmsg);
 $usrmsg = nl2br($usrmsg);
@@ -32,7 +39,24 @@ $blurb = $this->objLanguage->languagetext("mod_errors_blurb", "errors");
 $midcol .= $objFeatureBox->show($this->objLanguage->languagetext("mod_errors_usrtitle", "errors"), $usrmsg);//'<div class="featurebox">' . nl2br($usrmsg) . '</div>';
 $midcol .= $objFeatureBox->show($this->objLanguage->languagetext("mod_errors_devtitle", "errors"), $devmsg);//'<div class="featurebox">' . nl2br($devmsg) . '</div>';
 
+//$logfile = htmlentities(file_get_contents('error_log/system_errors.log'));
+//$midcol .= $objFeatureBox->show($this->objLanguage->languagetext("mod_errors_logfiletitle", "errors"), $logfile);
+
+//create the form
+$objForm->displayType = 4;
+$objForm->addToFormEx($objLanguage->languageText('mod_errors_submiterrs', 'errors'));
+$objForm->addToFormEx($objTextArea->show());
+$objForm->addToFormEx($objHiddenInput->show());
+
+$this->objButton=&new button($objLanguage->languageText('word_submit', 'system'));
+$this->objButton->setValue($objLanguage->languageText('word_submit', 'system'));
+$this->objButton->setToSubmit();
+$objForm->addToFormEx($this->objButton->show());
+
+
+$midcol .= $objFeatureBox->show($this->objLanguage->languagetext("mod_errors_mailadmins", "errors"),$objForm->show());
 $cssLayout->setMiddleColumnContent($midcol);
 
 echo $cssLayout->show();
+
 ?>
