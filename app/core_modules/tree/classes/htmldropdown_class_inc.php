b5c2e17d8048dffb7015ee96982f747d0cd6c6c5
tohir <tohir@gmail.com>
2006-09-05 07:31:42 +0000

    added htmldropdown as a presentation layer. This presents the tree as a form input, different from listbox, which does so as a navigation

---
+++ app/core_modules/tree/classes/htmldropdown_class_inc.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+* HTML_TreeMenu_Dropdown class
+*
+* This class presents the menu as a dropdown where the link is the value, and the text is the label.
+* This is different from the listbox that presents a form with a navigation. This one focussess on 
+* presentation as an input.
+*/
+require_once('presentation_class_inc.php');
+class htmldropdown extends presentation
+{
+    /**
+    * The Name of the <select> Input
+    * @var string
+    */
+    var $inputName;
+
+    /**
+    * The character used for indentation
+    * @var string
+    */
+    var $indentChar;
+
+    /**
+    * How many of the indent chars to use
+    * per indentation level
+    * @var integer
+    */
+    var $indentNum;
+
+    /**
+    * Target for the links generated
+    * @var string
+    */
+    var $linkTarget;
+
+    /**
+    * Constructor
+    *
+    * @param object $structure The menu structure
+    * @param array  $options   Options whic affect the display of the listbox.
+    *                          These can consist of:
+    *                           o inputName  Name of the <select input>
+    *                           o indentChar The character to use for indenting the nodes
+    *                                        Defaults to "&nbsp;"
+    *                           o indentNum  How many of the indentChars to use per indentation level
+    *                                        Defaults to 2
+    */
+    function htmldropdown($structure, $options = array())
+    {
+        $this->presentation($structure);
+
+        $this->inputName  = 'selectinput';
+        $this->indentChar = '&nbsp;';
+        $this->indentNum  = 4;
+
+        foreach ($options as $option => $value) {
+            $this->$option = $value;
+        }
+    }
+
+    /**
+    * Returns the HTML generated
+    */
+    function toHTML()
+    {
+        static $count = 0;
+        $nodeHTML = '';
+        
+        /**
+        * Loop through subnodes
+        */
+        if (isset($this->menu->items)) {
+            for ($i=0; $i<count($this->menu->items); $i++) {
+                $nodeHTML .= $this->_nodeToHTML($this->menu->items[$i]);
+            }
+        }
+        $count++;
+        return sprintf('<select name="%s" >%s</select>', $this->inputName, $nodeHTML);
+    }
+
+    /**
+    * Returns HTML for a single node
+    *
+    * @access private
+    */
+    function _nodeToHTML($node, $prefix = '')
+    {
+        $html = sprintf('<option value="%s">%s%s</option>', $node->link, $prefix, $node->text);
+        
+        /**
+        * Loop through subnodes
+        */
+        if ( isset($node->items) ) {
+            for ($i=0; $i<count($node->items); $i++) {
+                $html .= $this->_nodeToHTML($node->items[$i], $prefix . str_repeat($this->indentChar, $this->indentNum));
+            }
+        }
+
+        return $html;
+    }
+} // End class HTML_TreeMenu_Dropdown
+
+?>
