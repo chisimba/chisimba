6806808f91482933302f1e195306d690284dd34f
tohir <tohir@gmail.com>
2006-09-07 11:12:34 +0000

    new skin for unu merit

---
+++ app/skins/meritunu/splashscreen/splash_readfile_template.php
@@ -0,0 +1,93 @@
+<div id="wrap">
+	<div id="header">
+		<div id="site-name"><span>[-SITENAME-]</span>
+		</div>
+		<div id="search">
+			<form action="">
+			<label for="searchsite">Site Search:</label>
+			<input id="searchsite" name="searchsite" type="text" />
+			<input type="submit" value="Go" class="f-submit" />
+
+			</form>
+		</div>
+	</div>
+
+	<div id="content-wrap">
+        <div id="rightnav">
+			[-STARTFORM-]
+			<div class="featurebox">
+		            <h3>Login</h3>
+
+		                        [-USERNAMEBOX-]
+
+		                        [-PASSWORDBOX-]
+		                 <br />
+		                        [-USELDAPCHECK-][-LOGINBUTTON-]
+		                    <br />
+		                        [-NEWPASSWORD-]
+
+			</div>
+			<div class="featurebox">
+		            <h3>Registration</h3>
+
+		                        [-REGISTER-]
+
+			</div>
+		        [-SKIN-]
+		        [-ENDFORM-]
+		    <div class="featurebox">
+		            <h3>Languages</h3>
+		            <TABLE>
+		                    <TR>
+		                            <TD>
+		                                    [-LANGUAGECHOOSER-]
+		                            </TD>
+		                    </TR>
+		            </TABLE>
+		     </div>
+
+		     
+
+		</div>
+
+		<div id="content">
+			[-PRELOGINSTORIES-]
+			<div id="footer">
+			[-PRELOGINSTORIESFOOTER-]
+			</div>
+		</div>
+		
+		<div id="leftnav">
+				<div class="featurebox">
+		            <h3>Skinz</h3>
+		            <TABLE>
+		                <TR>
+		                    <TD>
+		                        [-SKINCHOOSER-]
+		                    </TD>
+		                </TR>
+		            </TABLE>
+				</div>
+				<div class="featurebox">
+		            <h3>Our other sites</h3>
+		                <ul>
+		                    <li><a href="http://avoir.uwc.ac.za">AVOIR development site</a><br /></li>
+		                    <li><a href="http://cvs.uwc.ac.za">CVS &amp; bug tracking</a></li>
+		                    <li><a href="http://elearn.hicte.uwc.ac.za">HictE KEWL</a></li>
+		                    <li><a href="http://www.hicte.uwc.ac.za">HictE Website</a></li>
+		                    <li><a href="http://nisl.uwc.ac.za">NISL KEWL</a></li>
+		                </ul>
+				 </div>
+				<div class="featurebox">
+		            <h3>Join this project</h3>
+					<TABLE>
+					<TR>
+						<TD>
+							Email pscott at uwc dot ac dot za
+						</TD>
+					</TR>
+				</TABLE>
+				</div>
+		</div>
+</div>
+</div>
