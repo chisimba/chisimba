83ec549c56946c0d3211036b9d5e38d8208d71d4
paulscott <paulscott@gmail.com>
2006-09-12 12:12:55 +0000

    modified js decleration to w3c standards

--- app/skins/echo/splashscreen/splash_readfile_template.php
+++ app/skins/echo/splashscreen/splash_readfile_template.php
@@ -93,6 +93,7 @@
     				<a href="http://validator.w3.org/check?uri=referer"><img
         				src="http://www.w3.org/Icons/valid-html401"
         				alt="Valid HTML 4.01 Transitional" height="31" width="88" /></a>
+
   					</center>
 		</div>
 
