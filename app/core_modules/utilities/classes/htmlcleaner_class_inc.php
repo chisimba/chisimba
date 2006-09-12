7e0ca4b229637e51cd6f2d6469e6359bf7c5f92b
wnitsckie <wnitsckie@gmail.com>
2006-09-12 10:52:47 +0000

    added the html cleaner

---
+++ app/core_modules/utilities/classes/htmlcleaner_class_inc.php
@@ -0,0 +1,54 @@
+<?php
+
+/* -------------------- dbTable class ----------------*/
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+/**
+* Class to clean html
+* @package safehtml
+* @category utilities
+* @copyright 2004, University of the Western Cape & AVOIR Project
+* @license GNU GPL
+* @version
+* @author Wesley  Nitsckie
+* @example :
+*/
+require_once("modules/utilities/resources/safehtml/safehtml.php");
+class htmlcleaner  extends object
+{
+   /**
+	 * @var object $_objSafeHtml The safehtml object
+	 * @access protected
+	 */
+    protected $_objSafeHtml;
+    
+    /**
+     * Construtor
+     */
+    public function init()
+    {
+        
+        $this->_objSafeHtml =& new safehtml();
+    }
+    
+    /**
+     * Method to clean the html
+     * @param string $html the html
+     * @return string the clean scrubbed html
+     * @access public
+     * @author Wesley Nitsckie
+     */
+    public function cleanHtml($hmtl = NULL)
+    {
+        if(is_null($hmtl))
+        {
+            return $html;
+        } else {
+            return $this->_objSafeHtml->parse($html);
+        }
+    }
+    
+}
