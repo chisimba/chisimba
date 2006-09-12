f5732191343427a38bc455402ba811694f400197
paulscott <paulscott@gmail.com>
2006-09-12 13:35:20 +0000

    messing with W3C

--- app/skins/echo/splashscreen/splash_readfile_template.php
+++ app/skins/echo/splashscreen/splash_readfile_template.php
@@ -91,10 +91,9 @@
 				</div>
 				<center>
     				<a href="http://validator.w3.org/check?uri=referer"><img
-        				src="http://www.w3.org/Icons/valid-html401"
-        				alt="Valid HTML 4.01 Transitional" height="31" width="88" /></a>
-
-  					</center>
+        				src="http://www.w3.org/Icons/valid-xhtml10"
+        				alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+				</center>
 		</div>
 
 
