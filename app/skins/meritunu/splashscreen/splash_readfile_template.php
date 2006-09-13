910bf85e1a155a4a910985d5a414708328372de1
paulscott <paulscott@gmail.com>
2006-09-13 08:08:33 +0000

    fixed up compliance issues

--- app/skins/meritunu/splashscreen/splash_readfile_template.php
+++ app/skins/meritunu/splashscreen/splash_readfile_template.php
@@ -88,6 +88,11 @@
 					</tr>
 				</table>
 				</div>
+				<center>
+    				<a href="http://validator.w3.org/check?uri=referer"><img
+        				src="http://www.w3.org/Icons/valid-xhtml10"
+        				alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+				</center>
 		</div>
 </div>
 </div>
