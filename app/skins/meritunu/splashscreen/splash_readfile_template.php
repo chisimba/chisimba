edcb0ecea97b9898af5577c3ef1b3570403cd617
paulscott <paulscott@gmail.com>
2006-09-13 08:07:33 +0000

    fixed up compliance issues

--- app/skins/meritunu/splashscreen/splash_readfile_template.php
+++ app/skins/meritunu/splashscreen/splash_readfile_template.php
@@ -37,16 +37,16 @@
 		        [-ENDFORM-]
 		    <div class="featurebox">
 		            <h3>Languages</h3>
-		            <TABLE>
-		                    <TR>
-		                            <TD>
+		            <table>
+		                    <tr>
+		                            <td>
 		                                    [-LANGUAGECHOOSER-]
-		                            </TD>
-		                    </TR>
-		            </TABLE>
+		                            </td>
+		                    </tr>
+		            </table>
 		     </div>
 
-		     
+
 
 		</div>
 
@@ -56,17 +56,17 @@
 			[-PRELOGINSTORIESFOOTER-]
 			</div>
 		</div>
-		
+
 		<div id="leftnav">
 				<div class="featurebox">
 		            <h3>Skinz</h3>
-		            <TABLE>
-		                <TR>
-		                    <TD>
+		            <table>
+		                <tr>
+		                    <td>
 		                        [-SKINCHOOSER-]
-		                    </TD>
-		                </TR>
-		            </TABLE>
+		                    </td>
+		                </tr>
+		            </table>
 				</div>
 				<div class="featurebox">
 		            <h3>Our other sites</h3>
@@ -80,13 +80,13 @@
 				 </div>
 				<div class="featurebox">
 		            <h3>Join this project</h3>
-					<TABLE>
-					<TR>
-						<TD>
+					<table>
+					<tr>
+						<td>
 							Email pscott at uwc dot ac dot za
-						</TD>
-					</TR>
-				</TABLE>
+						</td>
+					</tr>
+				</table>
 				</div>
 		</div>
 </div>
