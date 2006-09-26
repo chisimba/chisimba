e48e91bb7a3ac16c4e10a428a5e6ea4973755ca1
prince <prince@gmail.com>
2006-09-26 08:13:14 +0000

    added getLastLogin and getnowLogin

--- app/core_modules/toolbar/templates/layout/admin_layout_tpl.php
+++ app/core_modules/toolbar/templates/layout/admin_layout_tpl.php
@@ -9,9 +9,59 @@
 
 $cssLayout =& $this->newObject('csslayout', 'htmlelements');
 $leftMenu =& $this->newObject('usermenu','toolbar');
+$rightMenu =& $this->getObject('userLoginHistory','security');
+$tabpane =& $this->newObject('tabpane', 'htmlelements');
+$objTable =& $this->newObject('htmltable', 'htmlelements');
+$tab =& $this->newObject('tabbedbox', 'htmlelements');
+$nav = $rightMenu->getnowLogin();
+$arrayNav = array('current'=>$this->objLanguage->languageText('mod_loggedin','toolbar'));
+$tableRow=array();
+foreach($arrayNav as $category=>$items){
+        // set up table and column widths
+        $objTable->init();
+        $objTable->width='100%';
 
+        $objTable->startRow();
+        $objTable->addCell($this->objLanguage->languageText('word_username'), '33%');
+        $objTable->addCell($this->objLanguage->languageText('word_userid'), '33%');
+        $objTable->addCell($this->objLanguage->languageText('word_laston'), '33%');
+        $objTable->endRow();
+
+        $objTable->startRow();
+        $i = 0;
+        if(!empty($items)){
+            // Items (modules) in each category
+            foreach($nav as $key){
+                if($i++ % 3 == 0){
+                    $objTable->endRow();
+                    $objTable->addRow(array('&nbsp;'));
+                    $objTable->startRow();
+                }
+				// if the link text is specified
+				
+                if(isset($key['username']) && !empty($key['userid'])){
+                    $tableRow[] = $key['username'];
+                    $tableRow[] = $key['userid'];
+                    $tableRow[] = $key['laston'];
+                }
+
+                $objTable->addRow($tableRow, '', 'bottom', 'center');
+            }
+            $objTable->endRow();
+			
+        }
+        $tab->tabbedbox();
+        $tab->addTabLabel($items);
+        $tab->addBoxContent($objTable->show());
+       
+        //$tabpane->addTab(array('name'=>$category,'url'=>'http://localhost','content' => $objTable->show()),'luna-tab-style-sheet');
+    }
+
+//set columns to 2
+$cssLayout->setNumColumns(3);
 $cssLayout->setLeftColumnContent($leftMenu->show());
 $cssLayout->setMiddleColumnContent($this->getContent());
+$cssLayout->setRightColumnContent( $tab->show());
 
 echo $cssLayout->show();
 ?>
