1e0f31eed2321dbf1a25f33de2cd4fe62453a2c7
nic <nic@gmail.com>
2006-10-17 13:36:52 +0000

    added stories block

---
+++ app/core_modules/stories/classes/block_stories_class_inc.php
@@ -0,0 +1,58 @@
+<?
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+// end security check
+
+/**
+* 
+* A wide block class to display the site stories
+*
+* @author Nic Appleby
+* 
+* $Id$
+*
+*/
+class block_stories extends object
+{
+    /**
+    * @var object $objLanguage String to hold the language object
+    */
+    private $objLanguage;
+    
+    /**
+    * @var string $title The title of the block
+    */
+    public $title;
+
+    /**
+    * Standard init function to instantiate language object
+    * and create title, etc
+    */
+    public function init()
+    {
+    	try {
+    		$this->objLanguage = & $this->getObject('language', 'language');
+    		$this->title = &$this->objLanguage->languageText('word_stories');
+    	} catch (customException $e) {
+    		customException::cleanUp();
+    	}
+    }
+    
+    /**
+    * Standard block show method. It uses the renderform
+    * class to render the login box
+    */
+    public function show()
+    {
+    	try {
+        	$objStories = &$this->getObject('sitestories', 'stories');
+            return $objStories->fetchCategory('prelogin');
+        } catch (customException $e) {
+    		customException::cleanUp();
+    	}
+    }
+}
+?>
