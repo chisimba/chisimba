fe795bf3834faf7619476c796e7c40426bbc763b
nic <nic@gmail.com>
2006-10-23 11:42:55 +0000

    added language block class

---
+++ app/core_modules/language/classes/block_language_class_inc.php
@@ -0,0 +1,57 @@
+<?
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+// end security check
+
+/**
+* 
+* A block class to produce a language chooser
+*
+* @author Nic Appleby
+* 
+* $Id$
+*
+*/
+class block_language extends object
+{
+    /**
+    * @var string $title The title of the block
+    */
+    public $title;
+    
+    /**
+    * @var object $objLanguage String to hold the language object
+    */
+    private $objLanguage;
+
+    /**
+    * Standard init function to instantiate language object
+    * and create title, etc
+    */
+    public function init()
+    {
+    	try {
+    		$this->objLanguage = & $this->getObject('language', 'language');
+    		$this->title = $this->objLanguage->languageText("word_languages");
+    	} catch (customException $e) {
+    		customException::cleanUp();
+    	}
+    }
+    
+    /**
+    * Standard block show method. It uses the renderform
+    * class to render the login box
+    */
+    public function show()
+    {
+    	try {
+    		return $this->objLanguage->putlanguageChooser();
+    	} catch (customException $e) {
+    		customException::cleanUp();
+    	}
+    }
+}
+?>
