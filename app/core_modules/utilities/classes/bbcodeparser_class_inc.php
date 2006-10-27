00f37db1750426d5b847e3bfce1b9d875398184f
paulscott <paulscott@gmail.com>
2006-10-27 06:15:10 +0000

    bbcode parser utility

---
+++ app/core_modules/utilities/classes/bbcodeparser_class_inc.php
@@ -0,0 +1,34 @@
+<?php
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+class bbcodeparser extends object
+{
+	public function init()
+	{
+		if (!@require_once('HTML/BBCodeParser.php'))
+		{
+			throw new customException("Unable to locate PEAR::BBCodeParser, please install it with pear install --alldeps html_bbcodeparser!");
+		}
+
+
+	}
+
+	/**
+	 * Method to take a text string, parse it for BBCode and return a XHTML compliant string with divs
+	 *
+	 * @param string $text
+	 * @return string
+	 */
+	public function parse4bbcode($text)
+	{
+		$parser = new HTML_BBCodeParser(parse_ini_file('BBCodeParser.ini'));
+		$parser->setText($text);
+		$parser->parse();
+		return $parser->getParsed();
+	}
+}
+?>
