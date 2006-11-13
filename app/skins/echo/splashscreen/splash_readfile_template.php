f4ca64229fded8afb0b03da12a83f892e156741c
tohir <tohir@gmail.com>
2006-11-13 10:48:23 +0000

    changed link to Valid XHTML Banner from W3C to local _common dir

--- app/skins/echo/splashscreen/splash_readfile_template.php
+++ app/skins/echo/splashscreen/splash_readfile_template.php
@@ -92,7 +92,7 @@
 				</div>
 				<center>
     				<a href="http://validator.w3.org/check?uri=referer"><img
-        				src="http://www.w3.org/Icons/valid-xhtml10"
+        				src="skins/_common/valid-xhtml10.png"
         				alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
 				</center>
             </div>
