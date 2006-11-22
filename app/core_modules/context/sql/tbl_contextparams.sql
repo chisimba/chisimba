88e41ae39ec06a3b44475c744c49f0c6f1819406
wnitsckie <wnitsckie@gmail.com>
2006-11-22 14:47:06 +0000

    *** empty log message ***

---
+++ app/core_modules/context/sql/tbl_contextparams.sql
@@ -0,0 +1,34 @@
+<?php
+$tablename = 'tbl_contextparams';
+
+$options = array('comment' => 'Context Information', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+$fields = array(
+	'id' => array(
+		'type' => 'text',
+		'length' => 32
+		),
+	'contextcode' => array(
+		'type' => 'text',
+		'length' => 255,
+	        'notnull' => TRUE
+		),
+	'param' => array(
+		'type' => 'text',
+		'length' => 255,
+	        'notnull' => TRUE
+		),
+    	'value' => array(
+		'type' => 'text',
+		'length' => 255
+		)
+    );
+
+$name = 'contextcode';
+
+$indexes = array(
+                'fields' => array(
+                	'contextCode' => array()
+                )
+        );
+?>
