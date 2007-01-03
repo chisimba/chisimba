9c968f83797b7047f4f91b49318b4b531b07a033
tohir <tohir@gmail.com>
2007-01-03 11:41:34 +0000

    fixed parse for files. adjusted clear file usage

--- app/core_modules/filemanager/classes/registerfileusage_class_inc.php
+++ app/core_modules/filemanager/classes/registerfileusage_class_inc.php
@@ -17,8 +17,11 @@ class registerfileusage extends dbTable
     }
     
     /**
-    * Method to Register Use of a File in a Column of a Table
+    * Method to add the file usage information to the database
     *
+    * This is a private function that is called by the other functions for registering file usage
+    *
+    * @access private
     * @param string $fileId Record Id of File to be tracked
     * @param string $module Module the file will be used in
     * @param string $table Name of the Table holding the file
@@ -29,7 +32,7 @@ class registerfileusage extends dbTable
     * @param boolean $fileLock Flag whether to apply a lock on the file - prevents file from being deleted
     * @return string Color in Hexadecimal format
     */
-    public function registerUse($fileId, $module, $table, $recordId, $column, $context='', $workgroup='', $fileLock=FALSE)
+    private function addItem ($fileId, $module, $table, $recordId, $column, $context='', $workgroup='', $fileLock=FALSE)
     {
         $fileLock = $fileLock ? 'Y' : 'N';
         
@@ -60,7 +63,31 @@ class registerfileusage extends dbTable
     }
     
     /**
-    * Method to Parse through a text, find the files that are being used, and register them
+    * Method to Register Use of a File in a Column of a Table
+    *
+    * It first deregisters the usage, and then re-adds them to prevent duplication
+    *
+    * @param string $fileId Record Id of File to be tracked
+    * @param string $module Module the file will be used in
+    * @param string $table Name of the Table holding the file
+    * @param string $recordId Record Id of Record Using the File
+    * @param string $column Column of Record Using the File
+    * @param string $context Context File will be used in
+    * @param string $workgroup Workgroup the File will be used in
+    * @param boolean $fileLock Flag whether to apply a lock on the file - prevents file from being deleted
+    * @return string Color in Hexadecimal format
+    */
+    public function registerUse($fileId, $module, $table, $recordId, $column, $context='', $workgroup='', $fileLock=FALSE)
+    {
+        $this->deregisterUse($module, $table, $recordId, $column);
+        
+        $this->addItem ($fileId, $module, $table, $recordId, $column, $context, $workgroup, $fileLock);
+    }
+    
+    /**
+    * Method to Parse through a text, find the files that are being used, and register them.
+    * This function is better suited to checks of text that may or may not contain links to
+    * files in the file manager.
     *
     * @param string $text Record Id of File to be tracked
     * @param string $module Module the file will be used in
@@ -74,7 +101,18 @@ class registerfileusage extends dbTable
     */
     public function parseFilesUse($text, $module, $table, $recordId, $column, $context='', $workgroup='', $fileLock=FALSE)
     {
-        // ToDo
+        preg_match_all('/index\\.php\\?module=filemanager(?:&amp;|&)action=file(?:&amp;|&)id=(?P<fileid>[_|\\w]*)/', $text, $result, PREG_PATTERN_ORDER);
+        
+        $this->deregisterUse($module, $table, $recordId, $column);
+        
+        if (count($result['fileid']) > 0) {
+            foreach ($result['fileid'] as $fileId)
+            {
+                $this->addItem ($fileId, $module, $table, $recordId, $column, $context, $workgroup, $fileLock);
+            }
+        }
+        
+        return;
     }
     
     /**
@@ -82,10 +120,11 @@ class registerfileusage extends dbTable
     * @param string $module Name of the Module using file
     * @param string $table Name of the Table holding the file
     * @param string $recordId Record Id of the Record in the table
+    * @param string $columnName Name of the Column in which the file is stored
     */
-    public function deregisterUse($module, $table, $recordId)
+    public function deregisterUse($module, $table, $recordId, $columnName)
     {
-        $list = $this->getAll(' WHERE module=\''.$module.'\' AND tablename=\''.$table.'\' AND recordid=\''.$recordId.'\'');
+        $list = $this->getAll(' WHERE module=\''.$module.'\' AND tablename=\''.$table.'\' AND recordid=\''.$recordId.'\' AND columnname=\''.$columnName.'\'');
         foreach ($list as $item)
         {
             $this->delete('id', $item['id']);
