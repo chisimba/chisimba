5e9357a61b270a26f7c238bad5cadfe67e777aa2
tohir <tohir@gmail.com>
2007-01-09 07:17:46 +0000

    added skinchooser block

---
+++ app/core_modules/skin/classes/block_skinchooser_class_inc.php
@@ -0,0 +1,41 @@
+<?
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+// end security check
+
+/**
+* The class that shows the skin chooser block
+*
+* @author Tohir Solomons
+*
+*/
+class block_skinchooser extends object
+{
+    /**
+    * string $title Title of the Block
+    */
+    public $title;
+    
+    /**
+    * Constructor for the class
+    */
+    function init()
+    {
+ 		//Create an instance of the language object
+        $this->objLanguage =& $this->getObject('language','language');
+        //Set the title
+        $this->title=$this->objLanguage->languageText('mod_skin_name', 'skin');
+    }
+    
+    /**
+    * Method to output a block with information on how help works
+    */
+    function show()
+	{
+        $objSkin = $this->getObject('skin', 'skin');
+        return $objSkin->putSkinChooser();
+    }
+}
