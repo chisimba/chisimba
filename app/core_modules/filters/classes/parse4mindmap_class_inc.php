8bf54a915af1c2e0676f8bf855530d5e88ba18ad
paulscott <paulscott@gmail.com>
2007-01-22 13:15:33 +0000

    added mindmap parser

--- app/core_modules/filters/classes/parse4mindmap_class_inc.php
+++ app/core_modules/filters/classes/parse4mindmap_class_inc.php
@@ -1,25 +1,31 @@
 <?php
 /**
-* 
-* Class to parse a string (e.g. page content) that contains a link 
+*
+* Class to parse a string (e.g. page content) that contains a link
 * to a Freemind mind map and render the map in the page
-* 
+*
 * @author Derek Keats
-* 
+*
 */
 
-class parse4mindmap extends object 
+class parse4mindmap extends object
 {
     /**
-    * 
+    *
     * Method to parse the string
     * @param String $str The string to parse
     * @return The parsed string
-    * 
+    *
     */
     function parse($str)
     {
-        return $str;
+    	$str = '/var/www/cpgsql/5ive/app/usrfiles/users/1/freemind/ftisa-jan2007.mm';
+    	//echo "parsing mm";
+    	$objFlashFreemind = $this->newObject('flashfreemind', 'freemind');
+        $objFlashFreemind->getMindmapScript();
+    	$objFlashFreemind->setMindMap($str);
+        return $objFlashFreemind->show();
+        //return $str;
     }
- 
-} 
+
+}
