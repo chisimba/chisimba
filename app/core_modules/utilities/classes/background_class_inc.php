57b91d586f30f7a3568798c607db295d9e533c3a
paulscott <paulscott@gmail.com>
2007-01-24 10:42:39 +0000

    added example script

--- app/core_modules/utilities/classes/background_class_inc.php
+++ app/core_modules/utilities/classes/background_class_inc.php
@@ -12,6 +12,27 @@ if (!$GLOBALS['kewl_entry_point_run']) {
  * @author Paul Scott
  * @copyright GNU/GPL AVOIR/UWC 2006
  * @package utilities
+ *
+ * @example
+ * <?php
+ * //include the class source
+ * include('background_class_inc.php');
+ *
+ * $b = new background;
+ *
+ * //check the connection status
+ * $status = $b->isUserConn();
+ *
+ * //keep the user connection alive even if the browser is closed
+ * $callback = $b->keepAlive();
+ *
+ * //This is where you call your long running process
+ * sleep(60) //fake a 60 second process
+ *
+ * //fork the process and create the child process and call the callback function when done
+ * $call2 = $b->setCallBack("jdoe@example.com", "Your long thing is done", "The long running process that you requested is finished");
+ * ?>
+ *
  */
 class background extends object
 {
@@ -19,27 +40,27 @@ class background extends object
 	 * @var $connStatus Property to hold user connection status
 	 */
 	private $connStatus;
-	
+
 	/**
 	 * @var $callback Property to hold the callback function
 	 */
 	private $callback;
-	
+
 	/**
 	 * @var $address Property to hold the users email address
 	 */
 	private $address;
-	
+
 	/**
 	 * @var $subject Property to hold the email subject
 	 */
 	private $subject;
-	
+
 	/**
 	 * @var $message Property to hold the message
 	 */
 	private $message;
-	
+
 	/**
 	* Constructor
 	*/
@@ -66,7 +87,7 @@ class background extends object
 			return TRUE;
 		}
 	}
-	
+
 	/**
 	 * Method to keep the script going in the background
 	 * @param void
@@ -74,19 +95,19 @@ class background extends object
 	 */
 	public function keepAlive()
 	{
-		set_time_limit(0); 
+		set_time_limit(0);
 		//this will force the script to keep running till the end
-		ignore_user_abort(TRUE); 
+		ignore_user_abort(TRUE);
 		//funny thing with PHP, it needs output to keep running(?)!
-		while($this->connStatus != TRUE) 
+		while($this->connStatus != TRUE)
 		{
 			//this will save the loop
-			print "\n"; 
+			print "\n";
    			flush(); //Now php will check the connection
    			sleep(1);
-		} 
+		}
 	}
-	
+
 	/**
 	 * Method to send mail as the callback function
 	 * @param void
@@ -95,9 +116,9 @@ class background extends object
 	public function callBackFunc()
 	{
 		mail($this->address, $this->subject ,$this->message);
-		
+
 	}
-	
+
 	/**
 	 * Method for the callback function
 	 * This is called dynamically be the shutdown function callback
@@ -112,7 +133,7 @@ class background extends object
 		$this->address = $address;
 		$this->subject = $subject;
 		$this->message = $message;
-		
+
 		//pop a mail off to the user when his operation completes
 		//create the __Lambda Function
 		$callback = create_function('$address, $subject, $message','mail($address, $subject ,$message);');
@@ -121,7 +142,7 @@ class background extends object
 		//register the callback method
 		register_shutdown_function(array(&$this, "callBackFunc"));
 	}
-	
+
 }//end class
 ?>
 
