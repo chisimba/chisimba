21431c983d85da726cdaecb584f37e62c5a95730
paulscott <paulscott@gmail.com>
2007-01-26 10:14:04 +0000

    fixed module path

--- app/core_modules/utilities/resources/safehtml/HTMLSax3.php
+++ app/core_modules/utilities/resources/safehtml/HTMLSax3.php
@@ -30,7 +30,7 @@
 * Required classes
 */
 if (!defined('XML_HTMLSAX3')) {
-    define('XML_HTMLSAX3', 'modules/resources/safehtml/');
+    define('XML_HTMLSAX3', 'core_modules/resources/safehtml/');
 }
 require_once('HTMLSax3/States.php');
 require_once('HTMLSax3/Decorators.php');
@@ -359,7 +359,7 @@ class XML_HTMLSax3_StateParser_Lt430 extends XML_HTMLSax3_StateParser {
     * @return void
     */
     function ignoreWhitespace() {
-        while ($this->position < $this->length && 
+        while ($this->position < $this->length &&
             strpos(" \n\r\t", $this->rawtext{$this->position}) !== FALSE) {
             $this->position++;
         }
@@ -528,7 +528,7 @@ class XML_HTMLSax3 {
     * <li>XML_OPTION_ENTITIES_UNPARSED: XML entities are returned as
     * seperate data handler calls in unparsed form</li>
     * <li>XML_OPTION_ENTITIES_PARSED: (PHP 4.3.0+ only) XML entities are
-    * returned as seperate data handler calls and are parsed with 
+    * returned as seperate data handler calls and are parsed with
     * PHP's html_entity_decode() function</li>
     * <li>XML_OPTION_STRIP_ESCAPES: strips out the -- -- comment markers
     * or CDATA markup inside an XML escape, if found.</li>
