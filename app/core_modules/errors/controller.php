b2d789c39c5e4fab6fbc4d6ca8c8fff4ecb02ba3
paulscott <paulscott@gmail.com>
2007-01-31 07:02:15 +0000

    added in a check for db availability

--- app/core_modules/errors/controller.php
+++ app/core_modules/errors/controller.php
@@ -37,7 +37,7 @@ class errors extends controller
         	customException::cleanUp();
         }
     }
-    
+
     public function requiresLogin() {
     	return FALSE;
     }
@@ -55,6 +55,22 @@ class errors extends controller
             	return 'noaction_tpl.php';
             	break;
             case 'dberror':
+            	//check for a dead database as well
+            	$dsn = KEWL_DB_DSN;
+				$dsn = $this->parseDSN($dsn);
+				// Connect to the database
+				require_once 'MDB2.php';
+				//MDB2 has a factory method, so lets use it now...
+				$checkdb = &MDB2::connect($dsn);
+				//Check for errors on the connect method
+				if (PEAR::isError($checkdb)) {
+					$devmsg = $checkdb->getMessage();
+					$usrmsg = $checkdb->getUserInfo();
+					$this->setVarByRef('devmsg',$devmsg);
+	            	$this->setVarByRef('usrmsg',$usrmsg);
+    	        	return 'dberror_tpl.php';
+        	    	break;
+				}
             	$devmsg = $this->getParam('devmsg');
             	$usrmsg = $this->getParam('usrmsg');
             	$this->setVarByRef('devmsg',$devmsg);
@@ -104,6 +120,75 @@ class errors extends controller
 
         }
     }
+
+    	/**
+     * Method to parse the DSN from a string style DSN to an array for portability reasons
+     *
+     * @access public
+     * @param string $dsn
+     * @return void
+     * @TODO get the port settings too!
+     */
+	public function parseDSN($dsn)
+	{
+		$parsed = NULL;
+		$arr = NULL;
+		if (is_array($dsn)) {
+			$dsn = array_merge($parsed, $dsn);
+			return $dsn;
+		}
+		//find the protocol
+		if (($pos = strpos($dsn, '://')) !== false) {
+			$str = substr($dsn, 0, $pos);
+			$dsn = substr($dsn, $pos + 3);
+		} else {
+			$str = $dsn;
+			$dsn = null;
+		}
+		if (preg_match('|^(.+?)\((.*?)\)$|', $str, $arr)) {
+			$parsed['phptype']  = $arr[1];
+			$parsed['phptype'] = !$arr[2] ? $arr[1] : $arr[2];
+		} else {
+			$parsed['phptype']  = $str;
+			$parsed['phptype'] = $str;
+		}
+
+		if (!count($dsn)) {
+			return $parsed;
+		}
+		// Get (if found): username and password
+		if (($at = strrpos($dsn,'@')) !== false) {
+			$str = substr($dsn, 0, $at);
+			$dsn = substr($dsn, $at + 1);
+			if (($pos = strpos($str, ':')) !== false) {
+				$parsed['username'] = rawurldecode(substr($str, 0, $pos));
+				$parsed['password'] = rawurldecode(substr($str, $pos + 1));
+			} else {
+				$parsed['username'] = rawurldecode($str);
+			}
+		}
+		//server
+		if (($col = strrpos($dsn,':')) !== false) {
+			$strcol = substr($dsn, 0, $col);
+			$dsn = substr($dsn, $col + 1);
+			if (($pos = strpos($strcol, '+')) !== false) {
+				$parsed['hostspec'] = rawurldecode(substr($strcol, 0, $pos));
+			} else {
+				$parsed['hostspec'] = rawurldecode($strcol);
+			}
+		}
+
+		//now we are left with the port and databsource so we can just explode the string and clobber the arrays together
+		$pm = explode("/",$dsn);
+		$parsed['hostspec'] = $pm[0];
+		$parsed['database'] = $pm[1];
+		$dsn = NULL;
+
+		$parsed['hostspec'] = str_replace("+","/",$parsed['hostspec']);
+
+		return $parsed;
+	}
+
 
 }
 ?>
