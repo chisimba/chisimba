d84e07ae5f4c8b9e0deeabf12d0e6b8df0082fdc
dkeats <dkeats@gmail.com>
2007-02-20 19:33:54 +0000

    Put the display in a frame

--- app/core_modules/timeline/templates/content/viewtimeline_tpl.php
+++ app/core_modules/timeline/templates/content/viewtimeline_tpl.php
@@ -2,19 +2,40 @@
 /*
  * Created on Jan 28, 2007
  */
-echo $str;
-$hideBanner=$this->getParam("mode", FALSE);
-if (!$hideBanner == "TRUE") {
-	$showLinkFrame = $this->getParam('showLinkFrame', FALSE);
-	if ($showLinkFrame == "TRUE") {
-	    $showHide = "Hide region to display linked content";
-	    $link = "<a href=\"" . $this->uri(array(), "timeline") . "\">"
-	      . $showHide . "</a>";
-	} else {
-	    $showHide = "Show region to display linked content";
-	    $link = "<a href=\"" . $this->uri(array('showLinkFrame' => 'TRUE'), "timeline") . "\">"
-	      . $showHide . "</a>";
-	} 
-	echo "<br />" . $link;
+//Create an instance of the css layout class
+$cssLayout = $this->newObject('csslayout', 'htmlelements');
+//Set columns to 2
+$cssLayout->setNumColumns(2);
+//Initialize NULL content for the left side column
+$leftSideColumn = "";
+
+
+//Add the templage heading to the main layer
+$objH = $this->getObject('htmlheading', 'htmlelements');
+//Heading H3 tag
+$objH->type=3; 
+$objH->str = $this->getParam("title", $objLanguage->languageText("mod_timeline_title_viewall", 'timeline'));
+//Add the heading to the output string for the main display area
+$rightSideColumn = $objH->show();
+
+$rightSideColumn .= $str;
+
+$showLinkFrame = $this->getParam('showLinkFrame', FALSE);
+if ($showLinkFrame == "TRUE") {
+    $showHide = "Hide region to display linked content";
+    $link = "<a href=\"" . $this->uri(array(), "timeline") . "\">"
+      . $showHide . "</a>";
+} else {
+    $showHide = "Show region to display linked content";
+    $link = "<a href=\"" . $this->uri(array('showLinkFrame' => 'TRUE'), "timeline") . "\">"
+      . $showHide . "</a>";
 }
+$rightSideColumn .= "<br />" . $link;
+
+//Add Left column
+$cssLayout->setLeftColumnContent($leftSideColumn);
+// Add Right Column
+$cssLayout->setMiddleColumnContent($rightSideColumn);
+//Output the content to the page
+echo $cssLayout->show();
 ?>  
