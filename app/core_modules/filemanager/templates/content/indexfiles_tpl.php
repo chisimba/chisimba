4141a05b16f68b2b5f58752a61d079fa2644e684
tohir <tohir@gmail.com>
2007-03-02 13:17:33 +0000

    added template to show index results

---
+++ app/core_modules/filemanager/templates/content/indexfiles_tpl.php
@@ -0,0 +1,29 @@
+<?php
+
+echo '<h1>File Indexer</h1>';
+
+$this->loadClass('link', 'htmlelements');
+
+$objIndexFileProcessor = $this->getObject('indexfileprocessor');
+
+$list = $objIndexFileProcessor->indexUserFiles($this->objUser->userId());
+
+if (count($list) > 0) {
+    echo '<ul>';
+    foreach ($list as $item)
+    {
+        $record = $this->objFiles->getFile($item);
+        
+        if ($record != FALSE) {
+            $link = new link ($this->uri(array('action'=>'fileinfo', 'id'=>$item)));
+            $link->link = $record['filename'];
+            echo '<li>'.$link->show().'</li>';
+        }
+    }
+    echo '</ul>';
+} else {
+    echo '<div class="noRecordsMessage">All files in your directory have been added to index.</div>';
+}
+
+
+?>
