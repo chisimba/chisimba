febafc71e7afc00af90372017882dc1903b287fb
megan <megan@gmail.com>
2007-03-05 09:57:02 +0000

    Added a second function to display a "double alphabet" = AA|AB|AC

--- app/core_modules/navigation/classes/alphabet_class_inc.php
+++ app/core_modules/navigation/classes/alphabet_class_inc.php
@@ -65,5 +65,48 @@ class alphabet extends object
 		return $ret;
 	}
 
+	/**
+	* Method to display the alphabet as in function putAlpha() with a second double alphabet below.
+	* A | B | C | ...
+	* AA | AB | AC | ...
+	*
+	* @author Megan Watson
+	*/
+	function putDoubleAlpha($link=NULL, $letter, $listAllName=Null,$target=Null)
+    {
+		if ($target!=Null){
+		    $target=" target=\"".$target."\" ";
+		} else {
+		    $target='';
+		}
+
+		if ($listAllName == '') {
+			$this->listAllName = $this->objLanguage->languageText("word_list");
+		} else {
+			$this->listAllName = $listAllName;
+		}
+
+		$alpha1 = '';
+		$alpha2 = '';
+		$lBound=65;
+	    $uBound=90;
+			
+		for ($i=$lBound; $i<=$uBound; $i++) {
+			$link2=str_replace("LETTER",chr($i),$link);
+			$alpha1 .= ' | <a href="'.$link2.'"'.$target.'>'.chr($i)."</a>\n";
+		}
+		$linkall=str_replace("LETTER", "listall", $link);
+		$alpha1 .=' | <a href="'.$linkall.'"'.$target.'>'.$this->listAllName.'</a> |';
+		
+		// Double alphabet
+		for ($i = $lBound; $i <= $uBound; $i++) {
+			$link2 = str_replace("LETTER", $letter.chr($i), $link);
+			$alpha2 .= ' | <a href="'.$link2.'"'.$target.'>'.$letter.chr($i)."</a>\n";
+		}
+		
+		$ret = $alpha1.'<br /><font size="1">'.$alpha2.'</font>';
+		return $ret;
+	}
+
 }
 ?>
