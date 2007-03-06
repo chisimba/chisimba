ed6dd257bb4120d8165ef1c5f356c961d2e1d526
dkeats <dkeats@gmail.com>
2007-03-06 07:52:30 +0000

    Added the missing parse4simplemap class DUH

---
+++ app/core_modules/filters/classes/parse4simplemap_class_inc.php
@@ -0,0 +1,95 @@
+<?php
+/**
+*
+* Class to parse a string (e.g. page content) that contains a link
+* to a simplemap and render the simplemap in the page
+*
+* @author Derek Keats
+*
+*/
+
+class parse4simplemap extends object
+{
+    
+    function init()
+    {
+        $this->objTlParser = $this->getObject('smapparser', 'simplemap');
+    }
+    
+    /**
+    *
+    * Method to parse the string
+    * @param String $str The string to parse
+    * @return The parsed string
+    *
+    */
+    function show($str)
+    {
+        preg_match_all('/\\[SIMPLEMAP]<a.*?href="(?P<simplemaplink>.*?)".*?>.*?<\/a>\\[\/SIMPLEMAP]/', $str, $results, PREG_PATTERN_ORDER);
+        $counter = 0;
+        foreach ($results[0] as $item)
+        {
+            $replacement = $this->objTlParser->getRemote($results['simplemaplink'][$counter]);
+            $str = str_replace($item, $replacement, $str);
+            $counter++;
+        }
+        return $str;
+    }
+    
+    /**
+    *
+    * Method to parse the string
+    * @param String $str The string to parse
+    * @return The parsed string
+    *
+    */
+    function showLocal($str)
+    {
+        preg_match_all('/\\[SIMPLEMAP_LOCAL]<a.*?href="(?P<simplemaplink>.*?)".*?>.*?<\/a>\\[\/SIMPLEMAP_LOCAL]/', $str, $results, PREG_PATTERN_ORDER);
+        $counter = 0;
+        foreach ($results[0] as $item)
+        {
+            $replacement = $this->objTlParser->getLocal($results['simplemaplink'][$counter]);
+            $str = str_replace($item, $replacement, $str);
+            $counter++;
+        }
+        
+        return $str;
+    }
+    
+    /**
+     * 
+     * A parse method as required by the washout filter parser that washes all the output for 
+     * parseable filters
+     * 
+     * @access Public
+     * @param string $str The text to parse
+     * @return string The parsed text 
+     * 
+     */
+    public function parse($str)
+    {
+    	//check for type
+    	preg_match_all('/\\[SIMPLEMAP]<a.*?href="(?P<simplemaplink>.*?)".*?>.*?<\/a>\\[\/SIMPLEMAP]/', $str, $remoteresults, PREG_PATTERN_ORDER);
+    	preg_match_all('/\\[SIMPLEMAP_LOCAL]<a.*?href="(?P<simplemaplink>.*?)".*?>.*?<\/a>\\[\/SIMPLEMAP_LOCAL]/', $str, $localresults, PREG_PATTERN_ORDER);
+    	if(!empty($remoteresults['simplemaplink']))
+    	{
+    		$type = 'remote';
+    	}
+    	else {
+    		$type = 'local';
+    	}
+    	switch($type)
+    	{
+    		case 'remote':
+    			return $this->show($str);
+    			break;
+    		case 'local':
+    			return $this->showLocal($str);
+    			break;
+    			
+    	}
+    	
+    }
+
+}
