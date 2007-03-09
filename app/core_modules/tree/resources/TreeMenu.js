b443228649246e6d45456a503c0a6cc810db6038
tohir <tohir@gmail.com>
2007-03-09 13:12:25 +0000

    added a function to adjust the columns when the tree expands

--- app/core_modules/tree/resources/TreeMenu.js
+++ app/core_modules/tree/resources/TreeMenu.js
@@ -53,6 +53,18 @@ function arrayCopy(input)
 	return output;
 }
 
+/* This is an addition that resizes the layouts in Chisimba when a tab is clicked
+It first checks whether the css layout class is being used, and then runs the function to adjust layouts
+It does so by detecting whether the function exists
+*/
+function adjustPageLayout()
+{
+    if ('function' == typeof window.adjustLayout) {
+        adjustLayout();
+    }
+
+}
+
 /**
 * TreeMenu class
 */
@@ -218,7 +230,7 @@ function arrayCopy(input)
 			* document.write()ing the string: '<div style="display:...' will screw up nn4.x
 	        */
 			var layerTag  = this.doesMenu() ? this.stringFormat('<div id="{0}" style="display: {1}" class="{2}">', layerID, visibility, (nodes[i].cssClass ? nodes[i].cssClass : this.defaultClass)) : this.stringFormat('<div class="{0}">', nodes[i].cssClass ? nodes[i].cssClass : this.defaultClass);
-			var onMDown   = this.doesMenu() && nodes[i].n.length  && nodes[i].isDynamic ? this.stringFormat('onmousedown="{0}.toggleBranch(\'{1}\', true)" style="cursor: pointer; cursor: hand"', this.myname, layerID) : '';
+			var onMDown   = this.doesMenu() && nodes[i].n.length  && nodes[i].isDynamic ? this.stringFormat('onmousedown="{0}.toggleBranch(\'{1}\', true);adjustPageLayout();" style="cursor: pointer; cursor: hand"', this.myname, layerID) : '';
 			var imgTag    = this.stringFormat('<img src="{0}/{1}{2}.gif" width="20" height="20" align="top" border="0" name="img_{3}" {4}>', this.iconpath, gifname, modifier, layerID, onMDown);
 			var linkTarget= nodes[i].linkTarget ? nodes[i].linkTarget : this.linkTarget;
 			var linkStart = nodes[i].link ? this.stringFormat('<a href="{0}" target="{1}">', nodes[i].link, linkTarget) : '';
