6890db2022af2b3bb1594b2b618ee07366abdbcc
dkeats <dkeats@gmail.com>
2007-03-26 19:26:30 +0000

    Separated out the timeline parser and the timeline local parser

--- app/core_modules/filters/classes/parse4timelinelocal_class_inc.php
+++ app/core_modules/filters/classes/parse4timelinelocal_class_inc.php
@@ -1,21 +1,21 @@
 <?php
 /**
 *
-* Class to parse a string (e.g. page content) that contains a link
-* to a timeline and render the timeline in the page
+* Class to parse a string (e.g. page content) that contains a timeline by
+* its id render the timeline in the page
 *
 * @author Derek Keats
 *
 */
 
-class parse4timeline extends object
+class parse4timelinelocal extends object
 {
     
     public function init()
     {
         $this->objTlParser = $this->getObject('timelineparser', 'timeline');
     }
-    
+       
     /**
     *
     * Method to parse the string
@@ -25,15 +25,15 @@ class parse4timeline extends object
     */
     public function parse($str)
     {
-        preg_match_all('/\\[TIMELINE]<a.*?href="(?P<timelinelink>.*?)".*?>.*?<\/a>\\[\/TIMELINE]/', $str, $results, PREG_PATTERN_ORDER);
+    	$str = stripslashes($str);
+        preg_match_all('/\\[TIMELINE_LOCAL](.*?)\\[\/TIMELINE_LOCAL]/', $str, $results, PREG_PATTERN_ORDER);
         $counter = 0;
         foreach ($results[0] as $item)
         {
-            $replacement = $this->objTlParser->getRemote($results['timelinelink'][$counter]);
+            $replacement = $this->objTlParser->getLocal($results[1][$counter]);
             $str = str_replace($item, $replacement, $str);
             $counter++;
         }
-        
         return $str;
     }
 }
