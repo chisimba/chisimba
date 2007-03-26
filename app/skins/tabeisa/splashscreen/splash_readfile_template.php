d9df08b74eb19218ae2be4790f2dae93c272fab5
adrian <adrian@gmail.com>
2007-03-26 12:02:27 +0000

    moved to chisimba_skins repository

--- app/skins/tabeisa/splashscreen/splash_readfile_template.php
+++
@@ -1,7 +0,0 @@
-<div id="introflash">
-<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="800" height="600">
-  <param name="movie" value="skins/tabeisa/flash/flashinterface.swf" />
-  <param name="quality" value="high" />
-  <embed src="skins/tabeisa/flash/flashinterface.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="800" height="600"></embed>
-</object>
-</div>
