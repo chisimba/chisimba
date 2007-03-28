41aaedefd28f6d5064078145523b31b2cbca8a2a
kevinc <kevinc@gmail.com>
2007-03-28 07:08:10 +0000

    Fixed the writing of the Javascript

--- app/core_modules/htmlelements/classes/windowpop_class_inc.php
+++ app/core_modules/htmlelements/classes/windowpop_class_inc.php
@@ -163,7 +163,7 @@ class windowPop implements ifhtml
             $this->js_iswritten=TRUE;
             return $this->js;
         } else {
-            return $this->js;
+            return '';
         }
     }
 
