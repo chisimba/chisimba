9d4222b1b77d488ed40e884b39fbcba2e759da3c
tohir <tohir@gmail.com>
2007-03-30 12:44:35 +0000

    added creative commons input as a drop down

---
+++ app/core_modules/creativecommons/classes/licensechooserdropdown_class_inc.php
@@ -0,0 +1,83 @@
+<?php
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Class to Generate a Drop down List of available licenses
+ * @author Tohir Solomons
+ */
+class licensechooserdropdown extends object
+{
+    /**
+     * Name of the Form Input
+     *
+     * @var string
+     */
+    public $inputName = 'creativecommons';
+    
+    /**
+     * Name of the Default Value
+     *
+     * @var string
+     */
+    public $defaultValue;
+    
+    /**
+     * Constructor
+     */
+    public function init()
+    {
+        // Load the Creative Commons Object
+        $this->objCC =& $this->getObject('dbcreativecommons');
+        $this->objSysConfig =& $this->getObject('dbsysconfig', 'sysconfig');
+        
+        // Load the GetIcon Object
+        $this->objIcon = $this->newObject('geticon', 'htmlelements');
+    }
+    
+    /**
+     * Method to display the list
+     *
+     * @return string Rendered Input
+     */
+    public function show()
+    {
+        // Get All Licenses
+        $licenses = $this->objCC->getAll();
+        
+        $options = '';
+        
+        // Loop through Licenses
+        foreach ($licenses as $license)
+        {
+            
+            if ($this->objSysConfig->getValue($license['code'], 'creativecommons') == 'Y') {
+                
+                $title = $license['title'];
+                if ($title == 'Attribution Non-commercial Share') {
+                    $title = 'Attribution Non-commercial Share Alike';
+                }
+                
+                $selected = ($license['code'] == $this->defaultValue) ? ' selected="selected"' : '';
+                
+                // Add to Radio Group
+                $options .= '<option value="'.$license['code'].'" class="'.$license['code'].'" '.$selected.'>'.$title.'</option>';
+            }
+        }
+        
+        $select = '<select name="'.$this->inputName.'" id="input_'.$this->inputName.'">';
+        
+        $select .= $options;
+        
+        $select .= '</select>';
+        
+        
+        // Return Radio Button
+        return $select;
+    }
+}
+
+?>
--- app/core_modules/creativecommons/templates/content/template_selectdemo.php
+++ app/core_modules/creativecommons/templates/content/template_selectdemo.php
@@ -12,4 +12,15 @@ $cc->defaultValue = 'by';
 // Show
 echo $cc->show();
 
+// Instantiate Class
+$cc = $this->newObject('licensechooserdropdown');
+
+// Set Default Value
+$cc->defaultValue = 'by';
+
+// Show
+echo '<form>'.$cc->show().'</form>';
+
+
+$this->setVar('pageSuppressXML', TRUE);
 ?>
