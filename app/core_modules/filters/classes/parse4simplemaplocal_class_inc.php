04170ee7e34155dfa374f905b0cd811f27d42b6b
dkeats <dkeats@gmail.com>
2007-04-07 18:38:18 +0000

    Separated simplemap local and simplemap remote into separate filters, removed the need for local map id to be in a link, added check for simplemap module before executing the filter, added return error message if simplemap not installed.

--- app/core_modules/filters/classes/parse4simplemaplocal_class_inc.php
+++ app/core_modules/filters/classes/parse4simplemaplocal_class_inc.php
@@ -8,46 +8,49 @@
 *
 */
 
-class parse4simplemap extends object
+class parse4simplemaplocal extends object
 {
     
-    public function init()
+    function init()
     {
-        //There is nothing to do here
-    }  
-   
+        
+    }
+    
     /**
-    * 
-    * A parse method as required by the washout filter parser that washes all the output for 
-    * parseable filters
-    * 
-    * @access Public
-    * @param string $str The text to parse
-    * @return string The parsed text 
-    * 
-    */
+    *
+    
+    /**
+     * 
+     * A parse method as required by the washout filter parser that washes all the output for 
+     * parseable filters. It replaces the id of the map with the actual map itself.
+     * 
+     * @access Public
+     * @param string $str The text to parse
+     * @return string The parsed text 
+     * 
+     */
     public function parse($str)
     {
     	//Instantiate the modules class to check if simplemap is registered
     	$objModule = $this->getObject('modules','modulecatalogue');
     	//See if the simple map module is registered and set a param
     	$isRegistered = $objModule->checkIfRegistered('simplemap', 'simplemap');
-    	if ($isRegistered){
-	    	//Instantiate the simplemap parser
-	    	$objSMParser = $this->getObject('smapparser', 'simplemap');
+    	//If the module is registered then instantiate it
+    	if ($isRegistered) {
+    	    $this->objSMParser = $this->getObject('smapparser', 'simplemap');
     	}
-	    //Extract the URL
-    	preg_match_all('/\\[SIMPLEMAP]<a.*?href="(?P<simplemaplink>.*?)".*?>.*?<\/a>\\[\/SIMPLEMAP]/', $str, $results, PREG_PATTERN_ORDER);
-	    $counter = 0;
-	    foreach ($results[0] as $item) {
+    	preg_match_all('/\\[SIMPLEMAP_LOCAL](.*?)\\[\/SIMPLEMAP_LOCAL]/', $str, $results, PREG_PATTERN_ORDER);
+        $counter = 0;
+        foreach ($results[0] as $item)
+	    {
 	    	if ($isRegistered) {
-	            $replacement = $objSMParser->getRemote($results['simplemaplink'][$counter]);
-	    	} else {
+	            $replacement = $this->objSMParser->getLocal($results[1][$counter]);
+    		} else {
     			$objLanguage = $this->getObject('language', 'language');
     	    	$replacement = $results[1][$counter] . "<br /><div class=\"error\"><h3>" 
     	      	  . $objLanguage->languageText("mod_filters_error_smapnotinstalled", "filters")
     	      	  . "</h3></div>";
-	    	}
+	    	}    	      	  
             $str = str_replace($item, $replacement, $str);
             $counter++;
     	}
