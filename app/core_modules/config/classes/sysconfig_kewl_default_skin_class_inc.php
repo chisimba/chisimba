3ee6dbeec6a49cbef43fa2364bc148545b197616
tohir <tohir@gmail.com>
2007-05-14 12:53:47 +0000

    added sysconfig input for default skin

---
+++ app/core_modules/config/classes/sysconfig_kewl_default_skin_class_inc.php
@@ -0,0 +1,65 @@
+<?
+
+/**
+* Class to provide SysConfig an input for the KEWL_DEFAULT_SKIN parameter
+* @author Tohir Solomons
+*/
+class sysconfig_kewl_default_skin extends object
+{
+
+    /**
+    * Standard Constructor
+    */
+    public function init()
+    { }
+    
+    /**
+    * Method to set the current default value
+    */
+    public function setDefaultValue($value)
+    {
+        $this->defaultVaule = $value;
+    }
+    
+    /**
+    * Method to display the sysconfig interface
+    */
+    public function show()
+    {
+        // Load the Radio button class
+        $this->loadClass('radio', 'htmlelements');
+        
+        // Load the Skin Object
+        $objSkin = $this->getObject('skin', 'skin');
+        
+        $skinsList = $objSkin->getListofSkins();
+        
+        // Input MUST be called 'pvalue'
+        $objElement = new radio ('pvalue');
+        
+        foreach ($skinsList as $element=> $value) {
+           $objElement->addOption($element, $value);
+        }
+        
+        // Set Default Selected
+        $objElement->setSelected($this->defaultVaule);
+        
+        // Set radio buttons to be one per line
+        $objElement->setBreakSpace('<br />');
+        
+        // return finished radio button
+        return $objElement->show();
+    }
+    
+    /**
+    * Method to run actions that need to occur once the parameter is updated
+    */
+    public function postUpdateActions()
+    {
+        return NULL;
+    }
+    
+    
+}
+
+?>
