1d18faea021bb71d58d3c0d40dcf22dd090a9d14
tohir <tohir@gmail.com>
2007-05-14 12:14:11 +0000

    fixed to work in chisimba framework

--- app/core_modules/utilities/classes/checkobject_class_inc.php
+++ app/core_modules/utilities/classes/checkobject_class_inc.php
@@ -15,7 +15,9 @@ class checkobject extends object
     * Standard Constructor
     */
     public function init()
-    { }
+    { 
+        $this->objConfig = $this->getObject('altconfig', 'config');
+    }
     
     /**
     * Method to check whether an objects file exists
@@ -25,6 +27,8 @@ class checkobject extends object
     */
     public function objectFileExists($name, $moduleName)
     {
+        $filename2 = '';
+        
         if ($moduleName == '_core') {
             $filename = "classes/core/".strtolower($name)."_class_inc.php";
         } else {
@@ -32,12 +36,19 @@ class checkobject extends object
             if ($moduleName == '_site_') {
                 $moduleName = 'config';
             }
-            $filename = "modules/".$moduleName."/classes/".strtolower($name)."_class_inc.php";
+            
+            // Generate Filename for core_modules
+            $filename = $this->objConfig->getSiteRootPath() . "core_modules/".$moduleName."/classes/".strtolower($name)."_class_inc.php";
+            
+            // Generate Filename for chisimba_modules
+            $filename2 = $this->objConfig->getModulePath().$moduleName."/classes/".strtolower($name)."_class_inc.php";
         }
         
         $filename = strtolower($filename);
         
         if (file_exists($filename)) {
+            return TRUE;
+        } else if ($filename2 != '' && file_exists($filename2)){
             return TRUE;
         } else {
             return FALSE;
