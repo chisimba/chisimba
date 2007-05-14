d2fadb404486f8248214bc1d3f8b808b090f193a
paulscott <paulscott@gmail.com>
2007-05-14 12:03:12 +0000

    modified to fix for bug #1843

--- app/installer/index.php
+++ app/installer/index.php
@@ -3,7 +3,7 @@ $abspath = getcwd();
 $path = preg_replace("/\/installer/i",'',$abspath);
 $pear_path = $path . "/lib/pear/";
 ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.$pear_path);
-
+define('PEAR_DIR', $pear_path);
 //echo ini_get('include_path');
 
 /**
