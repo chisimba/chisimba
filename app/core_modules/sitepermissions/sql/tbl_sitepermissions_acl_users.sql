7ecf30599e2e940b7140fe7463ea499536b5ccd0
kevinc <kevinc@gmail.com>
2007-05-29 12:45:06 +0000

    Added tables for access control lists

---
+++ app/core_modules/sitepermissions/sql/tbl_sitepermissions_acl_users.sql
@@ -0,0 +1,36 @@
+<?php
+// Table Name
+$tablename = 'tbl_sitepermissions_acl_users';
+
+//Options line for comments, encoding and character set
+$options = array('comment' => 'Table used to keep a list of users for each acl.', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+// Fields
+$fields = array(
+	'id' => array(
+		'type' => 'text',
+		'length' => 32,
+	),
+    'aclid' => array(
+        'type' => 'text',
+        'length' => 32,
+    ),
+	'type' => array( // user, group, acl,
+		'type' => 'integer',
+		'length' => 1,
+	),
+	'typeid' => array(
+		'type' => 'text',
+		'length' => 32,
+	),
+);
+//create other indexes here...
+
+$name = 'FK_acl_users';
+
+$indexes = array(
+    'fields' => array(
+        'typeid' => array(),
+    )
+);
+?>
