3e3b08c6b66c986ce91dd1776888b06a9df39cd0
paulscott <paulscott@gmail.com>
2007-07-11 07:06:12 +0000

    updated GPLv2.0

--- app/lib/pear/PHP/DocBlockGenerator/Block.php
+++ app/lib/pear/PHP/DocBlockGenerator/Block.php
@@ -122,7 +122,20 @@ class PHP_DocBlockGenerator_Block
      * @var    array
      * @access private
      */
-    private $licenseText = array('The license text...');
+    private $licenseText = array('This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the
+Free Software Foundation, Inc.,
+59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.');
 
     /**
      * The package name
@@ -150,7 +163,7 @@ class PHP_DocBlockGenerator_Block
         'author' => 'Author\'s name',
         'category' => 'CategoryName',
         'email' => 'author@mail.com',
-        'license' => 'bsd',
+        'license' => 'gpl',
         'link' => null,
         'package' => null,
         'see' => 'References to other sections (if any)...',
