ef7dd7d575e9730f5c7999698028d8f72d6368d8
paulscott <paulscott@gmail.com>
2007-07-11 10:33:20 +0000

    YAML

---
+++ app/core_modules/utilities/classes/yaml_class_inc.php
@@ -0,0 +1,36 @@
+<?php
+
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+class yaml extends object 
+{
+	public $objYaml;
+	
+	public function init()
+	{
+		include($this->getResourcePath('yaml/spyc.php'));
+		$this->objYaml = new Spyc;
+	}
+	
+	public function parseYaml($file)
+	{
+		if(file_exists($file))
+		{
+			return $this->objYaml->load($file);
+		}
+		else {
+			// create the file
+			touch($file);
+			return $this->objYaml->load($file);
+		}
+	}
+	
+	public function saveYaml($array,$indent = false,$wordwrap = false)
+	{
+		return $this->objYaml->dump($array, $indent, $wordwrap);
+	}
+}
