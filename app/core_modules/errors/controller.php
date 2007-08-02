73488a7d215e833f710ef21227610038ac94a14a
paulscott <paulscott@gmail.com>
2007-08-02 13:58:30 +0000

    fixed up docs

--- app/core_modules/errors/controller.php
+++ app/core_modules/errors/controller.php
@@ -1,25 +1,98 @@
 <?php
+
+/**
+ * Error handler
+ * 
+ * CustomException extension to the Exception classes in PHP5
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  Chisimba
+ * @package   errors
+ * @author    Paul Scott <pscott@uwc.ac.za>
+ * @copyright 2007 Paul Scott
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @version   CVS: $Id$
+ * @link      http://avoir.uwc.ac.za
+ * @see       core
+ */
 // security check - must be included in all scripts
-if (!$GLOBALS['kewl_entry_point_run'])
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run'])
 {
     die("You cannot view this page directly");
 }
 // end security check
 
+
 /**
-* error module Controller
-*
-* @author Paul Scott
-* @copyright (c) 2004 University of the Western Cape
-* @package httpclient
-* @version 1
-*/
+ * Error handler
+ * 
+ * CustomException extension to the Exception classes in PHP5
+ * 
+ * @category  Chisimba
+ * @package   errors
+ * @author    Paul Scott <pscott@uwc.ac.za>
+ * @copyright 2007 Paul Scott
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @version   Release: @package_version@
+ * @link      http://avoir.uwc.ac.za
+ * @see       core
+ */
 class errors extends controller
 {
+
+    /**
+     * Logging object
+     * @var    unknown
+     * @access public 
+     */
     public $objLog;
+
+    /**
+     * Config object
+     * @var    mixed 
+     * @access public
+     */
     public $objConfig;
+
+    /**
+     * Language Object
+     * @var    object
+     * @access public
+     */
     public $objLanguage;
+
+    /**
+     * Mail Object
+     * @var    unknown
+     * @access public 
+     */
     public $objMail;
+
+    /**
+     * Users Object
+     * @var    object
+     * @access public
+     */
     public $objUser;
 
 	/**
@@ -38,6 +111,14 @@ class errors extends controller
         }
     }
 
+    /**
+     * login override
+     * 
+     * Overrides the parent login function requirement
+     * 
+     * @return boolean Return
+     * @access public 
+     */
     public function requiresLogin() {
     	return FALSE;
     }
@@ -125,9 +206,9 @@ class errors extends controller
      * Method to parse the DSN from a string style DSN to an array for portability reasons
      *
      * @access public
-     * @param string $dsn
-     * @return void
-     * @TODO get the port settings too!
+     * @param  string $dsn
+     * @return void  
+     * @TODO   get the port settings too!
      */
 	public function parseDSN($dsn)
 	{
