898b3ca7dea0ee52bed0fcb968d54df02b6c3381
paulscott <paulscott@gmail.com>
2007-08-02 16:56:45 +0000

    fixed up docs

--- app/core_modules/filemanager/controller.php
+++ app/core_modules/filemanager/controller.php
@@ -1,11 +1,50 @@
 <?php
+
 /**
-* File Manager Controller
-*
-* @author Tohir Solomons
-* @package filemanager
-* @version 0.2
-*/
+ * Filemanager Controller
+ * 
+ * Top level controller for the Chisimba File Manager
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  Chisimba
+ * @package   filemanager
+ * @author    Tohir Solomons <tsolomons@uwc.ac.za>
+ * @copyright 2007 Tohir Solomons
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @version   CVS: $Id$
+ * @link      http://avoir.uwc.ac.za
+ * @see       core
+ */
+
+
+/**
+ * Filemanager Controller
+ * 
+ * Top level controller for the Chisimba File Manager
+ * 
+ * @category  Chisimba
+ * @package   filemanager
+ * @author    Tohir Solomons <tsolomons@uwc.ac.za>
+ * @copyright 2007 Tohir Solomons
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @version   Release: @package_version@
+ * @link      http://avoir.uwc.ac.za
+ * @see       core
+ */
 class filemanager extends controller
 {
 
@@ -151,7 +190,7 @@ class filemanager extends controller
 
     /**
     * Method to download a file
-    * @param string $id Record Id of the File
+    * @param string $id       Record Id of the File
     * @param string $filename Filename of the File
     */
     public function showFile($id, $filename)
@@ -239,7 +278,7 @@ class filemanager extends controller
 
     /**
     * Method to Show Information about a file
-    * @param string $id Record Id of the File
+    * @param string $id       Record Id of the File
     * @param string $filename Filename of the File
     */
     public function showFileInfo($id, $filename)
@@ -563,7 +602,7 @@ function checkWindowOpener()
     /**
     * Ajax function to send preview of files
     * @param string $fileId Record Id of the File
-    * @param string $jsId JavaScript Id of the File
+    * @param string $jsId   JavaScript Id of the File
     */
     public function sendPreview($fileId, $jsId)
     {
