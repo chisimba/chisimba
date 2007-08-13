79795ef8502dd5b74bb51825f24336c45e58ff56
jsc <jsc@gmail.com>
2007-08-13 14:34:14 +0000

    added GPL and other comments

--- app/core_modules/groupadmin/classes/groupadminmodel_class_inc.php
+++ app/core_modules/groupadmin/classes/groupadminmodel_class_inc.php
@@ -1,4 +1,35 @@
 <?php
+/**
+ * groupAdminModel class
+ *
+ * The group admin model class is used to maintain the groups hierachy.
+ * It processes and maintains all groups data, and acts as the
+ * interface for external modules, making availale all its functionality
+ * 
+ * PHP version 5
+ *  
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * 
+ * @category  Chisimba
+ * @package   groupadmin
+ * @author    Paul Scott <pscott@uwc.ac.za>
+ * @copyright 2004-2007, University of the Western Cape & AVOIR Project
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @version   CVS: $Id$
+ * @link      http://avoir.uwc.ac.za
+ */
 // security check - must be included in all scripts
 if (!
 /**
--- app/core_modules/groupadmin/classes/tree_dropdown_class_inc.php
+++ app/core_modules/groupadmin/classes/tree_dropdown_class_inc.php
@@ -1,5 +1,46 @@
 <?php
 /**
+ * tree_dropdown class
+ * 
+ * This class presents the menu as a listbox
+ *
+ * PHP version 5
+ *  
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * 
+ * @category  Chisimba
+ * @package   groupadmin
+ * @author    Jonathan Abrahams, Paul Scott <pscott@uwc.ac.za>
+ * @copyright 2004-2007, University of the Western Cape & AVOIR Project
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @version   CVS: $Id$
+ * @link      http://avoir.uwc.ac.za
+ */
+
+// security check - must be included in all scripts
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+
+
+/**
 * @copyright  (c) 2000-2004, Kewl.NextGen ( http://kngforge.uwc.ac.za )
 * @package    groupadmin
 * @subpackage view
