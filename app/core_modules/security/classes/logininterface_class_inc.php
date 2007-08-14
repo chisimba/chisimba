6cba45b7627ba5c9ff49f3bb660d6147f8eb4a98
jsc <jsc@gmail.com>
2007-08-14 16:32:04 +0000

    added API docs

--- app/core_modules/security/classes/logininterface_class_inc.php
+++ app/core_modules/security/classes/logininterface_class_inc.php
@@ -1,9 +1,42 @@
 <?php
+ /**
+ * Logininterface class
+ * 
+ * Class to render login box, register links, and do other pre login duties
+ * 
+ * PHP version 5
+ *  
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * 
+ * @category  Chisimba
+ * @package   security
+ * @author FSIU
+ * @copyright 2004-2007, University of the Western Cape & AVOIR Project
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @link      http://avoir.uwc.ac.za
+ */
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+// end security check
 
 /**
 *
-* Class to render login box, register links, and do other
-* pre login duties
+* Class to render login box, register links, and do other pre login duties
 *
 * @version $Id$
 * @copyright 2003
@@ -38,6 +71,7 @@ class loginInterface extends object
     }
     /**
     * Method to render a login box
+    * @returns string
     */
     public function renderLoginBox($module = NULL)
     {
--- app/core_modules/security/classes/permissions_model_class_inc.php
+++ app/core_modules/security/classes/permissions_model_class_inc.php
@@ -1,4 +1,40 @@
 <?php
+ /**
+ * Permissions_model class
+ * 
+ * The permissions module GUI is used to select acls for use in your table.
+ * These acls are for your permission requirements.
+ * 
+ * PHP version 5
+ *  
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * 
+ * @category  Chisimba
+ * @package   security
+ * @author Jonathan Abrahams
+ * @copyright 2004-2007, University of the Western Cape & AVOIR Project
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @link      http://avoir.uwc.ac.za
+ */
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+// end security check
+
   /** 
   * Class to manage the access control list data. ( NOT COMPLETED ).
   *
