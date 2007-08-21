9a69ca0db6ab70bf603977d91d3564a236140979
kevinc <kevinc@gmail.com>
2007-08-21 12:33:08 +0000

    Fixed php tags

--- app/core_modules/lucene/search.php
+++ app/core_modules/lucene/search.php
@@ -22,42 +22,42 @@
 ?>
 
 <form method="get" action="search.php">
-    <input type="text" name="query" value="<?= htmlSpecialChars($query) ?>" />
+    <input type="text" name="query" value="<?php htmlSpecialChars($query) ?>" />
     <input type="submit" value="Search" />
 </form>
 
 <?php if (strlen($query) > 0) { ?>
     <p>
-        Found <?= $numHits ?> result(s) for query <?= $query ?>.
+        Found <?php $numHits ?> result(s) for query <?php $query ?>.
     </p>
 
     <?php foreach ($hits as $hit) {
-    	//var_dump($hit);
-    	?>
-        <h3><?= $hit->title ?> (score: <?= $hit->score ?>)</h3>
+        //var_dump($hit);
+        ?>
+        <h3><?php $hit->title ?> (score: <?php $hit->score ?>)</h3>
         <p>
-            content: <?//= $hit->contents ?>
+            content: <?php //= $hit->contents ?>
         </p>
         <p>
-            <?//=// $hit->teaser ?><br />
-            <a href="<?//= //$hit->url ?>">Read more...</a>
+            <?php //=// $hit->teaser ?><br />
+            <a href="<?php //= //$hit->url ?>">Read more...</a>
         </p>
     <?php } ?>
-<?php 
+<?php
 /**
  * Short description for file
- * 
+ *
  * Long description (if any) ...
- * 
+ *
  * PHP version 3
- * 
+ *
  * The license text...
- * 
+ *
  * @category  Chisimba
  * @package   lucene
  * @author    Paul Scott <pscott@uwc.ac.za>
  * @copyright 2007 Paul Scott
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   CVS: $Id$
  * @link      http://avoir.uwc.ac.za
  * @see       References to other sections (if any)...
