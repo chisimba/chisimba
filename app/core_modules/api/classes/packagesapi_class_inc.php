df3d2df2209c04b8ba1648c59f83922d31341849
paulscott <paulscott@gmail.com>
2007-08-29 08:35:20 +0000

    added getMsg function to api

--- app/core_modules/api/classes/packagesapi_class_inc.php
+++ app/core_modules/api/classes/packagesapi_class_inc.php
@@ -99,7 +99,7 @@ class packagesapi extends object
 		//log_debug("grabbing $path");
 		//zip up the module
 		$objZip = $this->getObject('wzip', 'utilities');
-		log_debug($objZip->addArchive($path, $filepath, $this->objConfig->getModulePath()));
+		log_debug($objZip);
 		$zipfile = $objZip->addArchive($path, $filepath, $this->objConfig->getModulePath());
 		log_debug($zipfile);
 		$filetosend = file_get_contents($zipfile);
