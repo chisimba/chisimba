40d04ea343156af4803f8d2903f8f99ecaea4c4f
dkeats <dkeats@gmail.com>
2007-09-01 19:30:16 +0000

    Added a parser for flickr feed URLs. Use as
    [FLICKR: type=feed, url=URL, cols=NUMBER_OF_COLUMNS]

--- app/core_modules/filters/classes/parse4flickr_class_inc.php
+++ app/core_modules/filters/classes/parse4flickr_class_inc.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Class to parse a string (e.g. page content) that contains a FLICKR
-* API item, and return the content inside the Chisimba page
+* item, and return the content inside the Chisimba page
 * 
 * PHP version 5
 * 
@@ -32,7 +32,7 @@
 /**
 *
 * Class to parse a string (e.g. page content) that contains a link
-* to a yout tube video and render the video in the page
+* to a flickr filter and render th desired content.
 *
 * @author Derek Keats
 *         
@@ -91,6 +91,11 @@ class parse4flickr extends object
                 case "slideshow":
                     $replacement = $this->getSlideshow();
                     break; 
+                    
+                case "feed":
+                    $this->objRss = $this->getObject('rssreader', 'feed');
+                    $replacement = "<div class=\"feedhoppe\" id=\"feedhopper" . $counter . "\">" . $this->getFeed() . "</div>";
+                    break; 
             
                 default:
                     $replacement = $item . "<br .<span class=\"error\">"
@@ -144,6 +149,111 @@ class parse4flickr extends object
           . "$userid&tags=$tag\" "
           . "width=\"$width\" height=\"$height\" $extras> "
           . "</object>";
+    }
+    /**
+     * 
+     * Method to get a flickr feed by parsing a filter of the form
+     * [FLICKR: type=feed,feed=http://flickrfeedurl]
+     * 
+     * @return string A formatted table with the flickr feed in it
+     * 
+     */
+    private function getFeed()
+    {
+        //http://api.flickr.com/services/feeds/photos_public.gne?id=93242958@N00&lang=en-us&format=rss_200
+        //Get the FEED URL
+        if (isset($this->objExpar->url)) {
+            $feed = $this->objExpar->url;
+        } else {
+            return "<span class=\"error\">"
+              . $this->objLanguage->languageText("mod_filters_error_flickr_nofeedurl", "filters")
+              . "</span>";
+        }
+        // Make sure it has a good chance of being a flickr site
+        $pos = strpos($feed, "flickr.com");
+        if ($pos === FALSE) {
+            return "<span class=\"error\">"
+              . $this->objLanguage->languageText("mod_filters_error_flickr_notfeed", "filters")
+              . ": " . $feed . "</span>";
+        }
+        // See how many columns to display, defaulting to two
+        if (isset($this->objExpar->cols)) {
+            $cols = $this->objExpar->cols;
+        } else {
+            $cols = 2;
+        }
+        //Clean up the &amp; that might be inserted by the editor or &eq; for named entities
+        $feed =  str_replace("&amp;", "&", $feed);
+        $feed =  str_replace("&eq;", "=", $feed);
+        //die ($feed);
+        $this->objRss->parseRss($feed);
+        $ar = $this->objRss->getRssItems();
+        $total = count($ar);
+        // Set up the table for output and start the first row
+        $ret = "<table>\n";
+        $fillCells = $total % $cols;
+        if ($fillCells > 0) {
+            $count = $fillCells - 1;
+            $filler = "";
+            while ($count !== 0)
+            {
+                $count--;
+                $filler .="<td>&nbsp;</td>";
+            }
+        }
+        if ($fillCells !==0) {
+            $closingCell = $filler . "</tr>";
+        } else {
+            $closingCell = "";
+        }
+        //Loop and build the output string
+        $counter=0;
+        $newRowCounter=0;
+        foreach ($ar as $item) {
+            $counter++;
+            if(!isset($item['link'])) {
+                $item['link'] = NULL;
+            }
+            // Putting them in two rows
+            if ($this->isNewRow($newRowCounter, $cols)==TRUE) {
+                @$ret .= "<tr><td><a href=\"" . htmlentities($item['link']) 
+                  . "\">" . htmlentities($item['title']) . "</a><br />\n"
+                  . $item['description'] . "</td>";
+                $newRowCounter = 0;
+            } else  {
+                @$ret .= "<td><a href=\"" . htmlentities($item['link']) 
+                  . "\">" . htmlentities($item['title']) . "</a><br />\n"
+                  . $item['description'] . "</td>";
+                  if ($newRowCounter == $cols) {
+                      $ret .= "</tr>";
+                  }
+            }
+            $newRowCounter++;
+            
+        }
+        $ret .= $closingCell . "</table>\n";
+        return $ret;      
+    }
+    
+    /**
+    * 
+    * Method to determine if we should insert a new row
+    * according to the desired number of columns
+    * 
+    * @access public 
+    * @param  int $num The number to test
+    * @param  int $cols The desired number of columns
+    * @return boolean TRUE|FALSE depending on status of $num
+    *                 
+    */
+    function isNewRow($num, $cols)
+    {
+        $remainder = $num % $cols;
+        if( $remainder == 0 ) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 }
 ?>
