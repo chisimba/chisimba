4c335f65227903e5633a0b5160fc0f86bcdcf058
paulscott <paulscott@gmail.com>
2007-09-14 06:08:28 +0000

    fixed up for api

--- app/core_modules/api/classes/wikiapi_class_inc.php
+++ app/core_modules/api/classes/wikiapi_class_inc.php
@@ -155,9 +155,8 @@ class wikiapi extends object
             log_debug($param);
     	}
     	$pagename = $param->scalarval();
-    	
     	$data = $this->objDbWiki->getPage($pagename);
-    	$val = $data[0]['page_content'];
+    	$val = $data['page_content'];
     	// send it back as a base64 encoded value
     	$val2send = new XML_RPC_Value($val, "base64");
     	return new XML_RPC_Response($val2send);
