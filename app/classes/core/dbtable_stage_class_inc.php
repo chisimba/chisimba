82168d5128fae5ffa667801e827f710f2588e2fd
joconnor <joconnor@gmail.com>
2007-10-02 14:32:11 +0000

    *** empty log message ***

--- app/classes/core/dbtable_stage_class_inc.php
+++ app/classes/core/dbtable_stage_class_inc.php
@@ -394,11 +394,11 @@ class dbTable extends object
             $last_id = -1;
         }
         else {
-            $sql = "SELECT last_id FROM sys_autoincr WHERE table_name='$tablename'";
+            $sql = "SELECT last_id FROM sys_autoincr WHERE table_name='{$tablename}'";
             $_ret = $this->_db->queryRow($sql, array());
             $last_id = $_ret[0]['last_id'];
         }
-        $last_id = $last_id++;
+        $last_id = $last_id + 1;
         $sql = "UPDATE sys_autoincr SET last_id = '{$last_id}' WHERE table_name = '{$tablename}'";
         if($this->_db->phptype == 'mysql')
         {
