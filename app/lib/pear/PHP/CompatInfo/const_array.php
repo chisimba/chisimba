ad59138aee4cef0a0cde26949d8212d1e2be25a4
paulscott <paulscott@gmail.com>
2007-10-22 08:05:25 +0000

    added files to fix bug #0002268

---
+++ app/lib/pear/PHP/CompatInfo/const_array.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Constants for PHP_CompatInfo 1.1.1 or better
+ *
+ * @global     array $GLOBALS['_PHP_COMPATINFO_CONST']
+ * @version    $Id$
+ * @author     Davey Shafik <davey@php.net>
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @package    PHP_CompatInfo
+ * @ignore
+ */
+
+$GLOBALS['_PHP_COMPATINFO_CONST'] = array (
+  '__FILE__' =>
+  array (
+    'init' => '4.0.0',
+    'name' => '__FILE__',
+  ),
+  '__LINE__' =>
+  array (
+    'init' => '4.0.0',
+    'name' => '__LINE__',
+  ),
+  '__FUNCTION__' =>
+  array (
+    'init' => '4.3.0',
+    'name' => '__FUNCTION__',
+  ),
+  '__CLASS__' =>
+  array (
+    'init' => '4.3.0',
+    'name' => '__CLASS__',
+  ),
+  '__METHOD__' =>
+  array (
+    'name' => '__METHOD__',
+    'init' => '5.0.0',
+  ),
+  'ABSTRACT' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'abstract',
+  ),
+  'CATCH' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'catch',
+  ),
+  'FINAL' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'final',
+  ),
+  'INSTANCEOF' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'instanceof',
+  ),
+  'PRIVATE' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'private',
+  ),
+  'PROTECTED' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'protected',
+  ),
+  'PUBLIC' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'public',
+  ),
+  'THROW' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'throw',
+  ),
+  'TRY' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'try',
+  ),
+  'CLONE' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'clone',
+  ),
+  'INTERFACE' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'interface',
+  ),
+  'IMPLEMENTS' =>
+  array (
+    'init' => '5.0.0',
+    'name' => 'implements',
+  ),
+);
+?>
