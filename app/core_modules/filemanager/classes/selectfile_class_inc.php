fcf5fee0ddd1af813637fe4088339e0dc9193277
tohir <tohir@gmail.com>
2007-10-24 08:47:54 +0000

    increased default size of window

--- app/core_modules/filemanager/classes/selectfile_class_inc.php
+++ app/core_modules/filemanager/classes/selectfile_class_inc.php
@@ -2,44 +2,44 @@
 
 /**
  * Class to Show a File Selector Input
- * 
+ *
  * PHP version 5
- * 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @category  Chisimba
  * @package   filemanager
  * @author    Tohir Solomons <tsolomons@uwc.ac.za>
  * @copyright 2007 Tohir Solomons
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   CVS: $Id$
  * @link      http://avoir.uwc.ac.za
- * @see       
+ * @see
  */
 
 
 /**
  * Class to Show a File Selector Input
- * 
+ *
  * @category  Chisimba
  * @package   filemanager
  * @author    Tohir Solomons <tsolomons@uwc.ac.za>
  * @copyright 2007 Tohir Solomons
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   Release: @package_version@
  * @link      http://avoir.uwc.ac.za
- * @see       
+ * @see
  */
 class selectfile extends object
 {
@@ -47,34 +47,34 @@ class selectfile extends object
     * @var string $name Name of the File Selector Input
     */
     public $name;
-    
+
     /**
     * @var array $subFolders List of Possible Subfolders for storing files
     */
     public $restrictFileList;
-    
+
     /**
     * @var string $defaultFile Record Id of the Default File
     */
     public $defaultFile;
-    
+
     /**
     * @var  boolean $context Flag to only include Context Files
     * @todo Implement this Feature
     */
     public $context;
-    
+
     /**
     * @var  boolean $workgroup Flag to only include Workgroup Files
     * @todo Implement this Feature
     */
     public $workgroup;
-    
+
     /**
     * @var int $widthOfInput Width of Text Input
     */
     public $widthOfInput;
-    
+
     /**
     * Constructor
     */
@@ -82,24 +82,24 @@ class selectfile extends object
     {
         $this->name = 'fileselect';
         $this->restrictFileList = array();
-        
+
         $this->defaultFile = '';
-        
+
         $this->context = FALSE;
         $this->workgroup = FALSE;
-        
+
         $this->objIcon = $this->newObject('geticon', 'htmlelements');
-        
+
         $this->objFile = $this->getObject('dbfile');
-        
+
         $this->loadClass('hiddeninput', 'htmlelements');
         $this->loadClass('textinput', 'htmlelements');
         $this->loadClass('windowpop', 'htmlelements');
         $this->loadClass('button', 'htmlelements');
-        
+
         $this->widthOfInput = '80%';
     }
-    
+
     /**
     * Method to set the default File
     * @access public
@@ -109,7 +109,7 @@ class selectfile extends object
     {
         $this->defaultFile = $fileId;
     }
-    
+
     /**
     * Method to return the JavaScript to clear an input
     * @return string
@@ -128,7 +128,7 @@ function clearFileInputJS(name)
 
         return $script;
     }
-    
+
     /**
     * Method to show the file selector input
     * @return string File Selector
@@ -141,7 +141,7 @@ function clearFileInputJS(name)
             $defaultName = '';
         } else {
             $file = $this->objFile->getFile($this->defaultFile);
-            
+
             if ($file == FALSE) {
                 $defaultId = '';
                 $defaultName = '';
@@ -150,82 +150,82 @@ function clearFileInputJS(name)
                 $defaultName = $file['filename'];
             }
         }
-        
+
         $input = new hiddeninput($this->name, $defaultId);
         $input->extra = ' id="hidden_'.$this->name.'"';
-        
+
         if (count($this->restrictFileList) == 0) {
             $ext = '';
         } else {
-            
+
             $ext = '';
             $divider = '';
-            
+
             foreach ($this->restrictFileList as $type)
             {
                 $ext .= $divider.$type;
                 $divider = '____';
             }
         }
-        
+
         $objPop = new windowpop;
-        
-        
+
+
         if ($this->context) {
             $context = 'yes';
         } else {
             $context = 'no';
         }
-        
+
         if ($this->workgroup) {
             $workgroup = 'yes';
         } else {
             $workgroup = 'no';
         }
-        
-        
+
+
         $location = $this->uri(array('action'=>'selectfilewindow', 'restrict'=>$ext, 'name'=>$this->name, 'context'=>$context, 'workgroup' => $workgroup), 'filemanager');
-        
+
         // Couldnt do this via uri function due to embedded JS
         $location .= '&amp;value=\'+document.getElementById(\'hidden_'.$this->name.'\').value+\'&amp;';
-        
+
         $objPop->set('location', $location);
-        
+
         $this->objIcon->setIcon('find_file');
         $this->objIcon->alt = 'Select File';
         $this->objIcon->title = 'Select File';
-        
-        
+
+
         //$objPop->set('linktext', 'Select File');
         $objPop->set('linktext', $this->objIcon->show());
         $objPop->set('linktext', 'Browse');
         $objPop->set('linkType', 'button');
-        $objPop->set('width','600'); 
-        $objPop->set('height','400');
+        $objPop->set('width','750');
+        $objPop->set('height','500');
         $objPop->set('resizable','yes');
         $objPop->set('scrollbars','auto');
-        $objPop->set('left','300');
-        $objPop->set('top','200');
+        $objPop->set('left','50');
+        $objPop->set('top','100');
         $objPop->set('status','yes');
         //leave the rest at default values
         $objPop->putJs();
-        
+
         $textinput = new textinput ('selectfile_'.$this->name, $defaultName);
         $textinput->setId('input_selectfile_'.$this->name);
         $textinput->extra = ' readonly="true" style="width:'.$this->widthOfInput.'" ';
-        
+
         $button = new button('clear', 'Clear', 'clearFileInputJS(\''.$this->name.'\');');
-        
+
         // Option for showing via submodal window
         // $objSubModalWindow = $this->getObject('submodalwindow', 'htmlelements');
         // $subModal = $objSubModalWindow->show('Select', $location, 'button');
         // return $input->show().$textinput->show().' &nbsp; '.$subModal.$button->show();
-        
+
         return $input->show().$textinput->show().' '.$objPop->show().' '.$button->show();
     }
-    
-    
-    
+
+
+
 
 }
 
--- app/core_modules/filemanager/classes/selectimage_class_inc.php
+++ app/core_modules/filemanager/classes/selectimage_class_inc.php
@@ -2,44 +2,44 @@
 
 /**
  * Class to Show an Image Selector Input
- * 
+ *
  * PHP version 5
- * 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @category  Chisimba
  * @package   filemanager
  * @author    Tohir Solomons <tsolomons@uwc.ac.za>
  * @copyright 2007 Tohir Solomons
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   CVS: $Id$
  * @link      http://avoir.uwc.ac.za
- * @see       
+ * @see
  */
 
 
 /**
  * Class to Show an Image Selector Input
- * 
+ *
  * @category  Chisimba
  * @package   filemanager
  * @author    Tohir Solomons <tsolomons@uwc.ac.za>
  * @copyright 2007 Tohir Solomons
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   Release: @package_version@
  * @link      http://avoir.uwc.ac.za
- * @see       
+ * @see
  */
 class selectimage extends object
 {
@@ -47,29 +47,29 @@ class selectimage extends object
     * @var string $name Name of the Image Selector Input
     */
     public $name;
-    
+
     /**
     * @var string $defaultFile Record Id of the Default File
     */
     public $defaultFile;
-    
+
     /**
     * @var  boolean $context Flag to only include Context Files
     * @todo Implement this Feature
     */
     public $context;
-    
+
     /**
     * @var  boolean $workgroup Flag to only include Workgroup Files
     * @todo Implement this Feature
     */
     public $workgroup;
-    
+
     /**
     * @var int $widthOfInput Width of Text Input
     */
     public $widthOfInput;
-    
+
     /**
     * Constructor
     */
@@ -77,25 +77,25 @@ class selectimage extends object
     {
         $this->name = 'imageselect';
         $this->restrictFileList = array();
-        
+
         $this->defaultFile = '';
-        
+
         $this->context = FALSE;
         $this->workgroup = FALSE;
-        
+
         $this->objIcon = $this->newObject('geticon', 'htmlelements');
-        
+
         $this->objFile = $this->getObject('dbfile');
         $this->objThumbnails = $this->getObject('thumbnails');
-        
+
         $this->loadClass('hiddeninput', 'htmlelements');
         $this->loadClass('textinput', 'htmlelements');
         $this->loadClass('windowpop', 'htmlelements');
         $this->loadClass('button', 'htmlelements');
-        
+
         $this->widthOfInput = '80%';
     }
-    
+
     /**
     * Method to set the default File
     * @access public
@@ -105,7 +105,7 @@ class selectimage extends object
     {
         $this->defaultFile = $fileId;
     }
-    
+
     /**
     * Method to return the JavaScript to clear an input
     * @return string
@@ -125,7 +125,7 @@ function clearFileInputJS(name)
 
         return $script;
     }
-    
+
     /**
     * Method to show the file selector input
     * @return string File Selector
@@ -138,7 +138,7 @@ function clearFileInputJS(name)
             $defaultName = '';
         } else {
             $file = $this->objFile->getFile($this->defaultFile);
-            
+
             if ($file == FALSE) {
                 $defaultId = '';
                 $defaultName = '';
@@ -147,54 +147,54 @@ function clearFileInputJS(name)
                 $defaultName = $file['filename'];
             }
         }
-        
+
         $input = new hiddeninput($this->name, $defaultId);
         $input->extra = ' id="hidden_'.$this->name.'"';
-        
-       
+
+
         $objPop = new windowpop;
-        
-        
+
+
         if ($this->context) {
             $context = 'yes';
         } else {
             $context = 'no';
         }
-        
+
         if ($this->workgroup) {
             $workgroup = 'yes';
         } else {
             $workgroup = 'no';
         }
-        
-        
+
+
         $location = $this->uri(array('action'=>'selectimagewindow', 'name'=>$this->name, 'context'=>$context, 'workgroup' => $workgroup), 'filemanager');
-        
+
         // Couldnt do this via uri function due to embedded JS
         $location .= '&amp;value=\'+document.getElementById(\'hidden_'.$this->name.'\').value+\'&amp;';
-        
+
         $objPop->set('location', $location);
-        
+
         $this->objIcon->setIcon('find_file');
         $this->objIcon->alt = 'Select File';
         $this->objIcon->title = 'Select File';
-        
-        
-        
+
+
+
         $objPop->set('linkType', 'button');
         $objPop->set('linktext', 'Select File');
-        
+
         //$objPop->set('linktext', $this->objIcon->show());
-        $objPop->set('width','600'); 
-        $objPop->set('height','400');
+        $objPop->set('width','750');
+        $objPop->set('height','500');
         $objPop->set('resizable','yes');
         $objPop->set('scrollbars','auto');
-        $objPop->set('left','300');
-        $objPop->set('top','200');
+        $objPop->set('left','50');
+        $objPop->set('top','100');
         $objPop->set('status','yes');
         //leave the rest at default values
         $objPop->putJs();
-        
+
         if ($defaultId == '') {
             $this->objIcon->setIcon('imagepreview');
             $this->objIcon->alt = 'Image Preview';
@@ -203,27 +203,27 @@ function clearFileInputJS(name)
             $previewImg = $this->objIcon->show();
         } else {
             $img = $this->objThumbnails->getThumbnail($defaultId, $file['filename']);
-            
+
             $previewImg = '<img src="'.$img.'" id="imagepreview_'.$this->name.'" />';
         }
-        
+
         $textinput = new textinput ('selectfile_'.$this->name, $defaultName);
         $textinput->setId('selectfile_'.$this->name);
         $textinput->extra = ' readonly="true" style="width:'.$this->widthOfInput.'" ';
-        
+
         $button = new button('clear', 'Reset', 'clearFileInputJS(\''.$this->name.'\');');
-        
+
         // Option for showing via submodal window
         // $objSubModalWindow = $this->getObject('submodalwindow', 'htmlelements');
         // $subModal = $objSubModalWindow->show('Select', $location, 'button');
         // return $input->show().$textinput->show().' &nbsp; '.$subModal.$button->show();
-        
+
         return $input->show().'<div style="width:100px; height:100px;line-height:100px;vertical-align:center;text-align:center;">'.$previewImg.'</div><br /><div>'.$objPop->show().' '.$button->show().'</div>';
         //$textinput->show()
     }
-    
-    
-    
+
+
+
 
 }
 
