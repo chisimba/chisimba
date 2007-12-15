88b67f8cef072b9f87a2b12c01144503dcd865a5
dkeats <dkeats@gmail.com>
2007-12-15 22:22:13 +0000

    Fixed some minor code formatting issues

--- app/core_modules/filters/classes/parse4flickr_class_inc.php
+++ app/core_modules/filters/classes/parse4flickr_class_inc.php
@@ -2,58 +2,58 @@
 /**
 * Class to parse a string (e.g. page content) that contains a FLICKR
 * item, and return the content inside the Chisimba page
-* 
+*
 * PHP version 5
-* 
-* This program is free software; you can redistribute it and/or modify 
-* it under the terms of the GNU General Public License as published by 
-* the Free Software Foundation; either version 2 of the License, or 
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
 * (at your option) any later version.
-* This program is distributed in the hope that it will be useful, 
-* but WITHOUT ANY WARRANTY; without even the implied warranty of 
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License 
-* along with this program; if not, write to the 
-* Free Software Foundation, Inc., 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the
+* Free Software Foundation, Inc.,
 * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-* 
+*
 * @category  Chisimba
 * @package   filters
 * @author    Derek Keats <dkeats@uwc.ac.za>
 * @copyright 2007 Derek Keats
-* @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+* @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
 * @version   CVS: $Id$
 * @link      http://avoir.uwc.ac.za
 */
- 
- 
- 
+
+
+
 /**
 *
 * Class to parse a string (e.g. page content) that contains a link
 * to a flickr filter and render th desired content.
 *
 * @author Derek Keats
-*         
+*
 */
 
 class parse4flickr extends object
 {
 	/**
-	* 
+	*
 	* String to hold an error message
-	* @accesss private 
+	* @accesss private
 	*/
 	private $errorMessage;
-    
+
     /**
-     * 
+     *
      * Constructor for the wikipedia parser
-     * 
-     * @return void  
+     *
+     * @return void
      * @access public
-     * 
+     *
      */
     function init()
     {
@@ -61,13 +61,13 @@ class parse4flickr extends object
         //Get an instance of the params extractor
         $this->objExpar = $this->getObject("extractparams", "utilities");
     }
-    
+
     /**
     *
     * Method to parse the string
     * @param  String $str The string to parse
     * @return The parsed string
-    *                
+    *
     */
     public function parse($txt)
     {
@@ -79,24 +79,24 @@ class parse4flickr extends object
         foreach ($results[0] as $item)
         {
             $str = $results[1][$counter];
-            $ar= $this->objExpar->getArrayParams($str, ",");
+            $ar = $this->objExpar->getArrayParams($str, ",");
             if (isset($this->objExpar->type)) {
                 $type = $this->objExpar->type;
             } else {
                 $type="error";
             }
-            
+
             switch ($type)
             {
                 case "slideshow":
                     $replacement = $this->getSlideshow();
-                    break; 
-                    
+                    break;
+
                 case "feed":
                     $this->objRss = $this->getObject('rssreader', 'feed');
                     $replacement = "<div class=\"feedhoppe\" id=\"feedhopper" . $counter . "\">" . $this->getFeed() . "</div>";
-                    break; 
-            
+                    break;
+
                 default:
                     $replacement = $item . "<br .<span class=\"error\">"
                       . $this->objLanguage->languageText("mod_filters_error_flickr_invalid" , "filters") . "</span>";
@@ -109,15 +109,15 @@ class parse4flickr extends object
     }
 
     /**
-     * 
+     *
      * Method to return a flickr slideshow
-     * 
+     *
      * @return string The formatted slideshow object
-     * 
+     *
      */
     private function getSlideshow()
     {
-        //Initialize extras 
+        //Initialize extras
         $extras = "";
         //Get and set the width
         if (isset($this->objExpar->width)) {
@@ -143,7 +143,7 @@ class parse4flickr extends object
         } else {
             $userid = "NOTFOUND";
         }
-        
+
         return "<object type=\"text/html\" "
           . "data=\"http://www.flickr.com/slideShow/index.gne?user_id="
           . "$userid&tags=$tag\" "
@@ -151,12 +151,12 @@ class parse4flickr extends object
           . "</object>";
     }
     /**
-     * 
+     *
      * Method to get a flickr feed by parsing a filter of the form
      * [FLICKR: type=feed,feed=http://flickrfeedurl]
-     * 
+     *
      * @return string A formatted table with the flickr feed in it
-     * 
+     *
      */
     private function getFeed()
     {
@@ -216,12 +216,12 @@ class parse4flickr extends object
             }
             // Putting them in two rows
             if ($this->isNewRow($newRowCounter, $cols)==TRUE) {
-                @$ret .= "<tr><td><a href=\"" . htmlentities($item['link']) 
+                @$ret .= "<tr><td><a href=\"" . htmlentities($item['link'])
                   . "\">" . htmlentities($item['title']) . "</a><br />\n"
                   . $item['description'] . "</td>";
                 $newRowCounter = 0;
             } else  {
-                @$ret .= "<td><a href=\"" . htmlentities($item['link']) 
+                @$ret .= "<td><a href=\"" . htmlentities($item['link'])
                   . "\">" . htmlentities($item['title']) . "</a><br />\n"
                   . $item['description'] . "</td>";
                   if ($newRowCounter == $cols) {
@@ -229,22 +229,22 @@ class parse4flickr extends object
                   }
             }
             $newRowCounter++;
-            
+
         }
         $ret .= $closingCell . "</table>\n";
-        return $ret;      
+        return $ret;
     }
-    
+
     /**
-    * 
+    *
     * Method to determine if we should insert a new row
     * according to the desired number of columns
-    * 
-    * @access public 
+    *
+    * @access public
     * @param  int $num The number to test
     * @param  int $cols The desired number of columns
     * @return boolean TRUE|FALSE depending on status of $num
-    *                 
+    *
     */
     function isNewRow($num, $cols)
     {
