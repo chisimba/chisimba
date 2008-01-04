749a40cae19f1aeb50566655219fd2d745884505
paulscott <paulscott@gmail.com>
2008-01-04 08:35:30 +0000

    fixed paths for core modules upgrade option

--- app/core_modules/api/classes/packagesapi_class_inc.php
+++ app/core_modules/api/classes/packagesapi_class_inc.php
@@ -111,7 +111,7 @@ class packagesapi extends object
 		$filetosend = file_get_contents($zipfile);
 		$filetosend = base64_encode($filetosend);
 		$val = new XML_RPC_Value($filetosend, 'string');
-		unlink($filepath);
+		//unlink($filepath);
 		log_debug("Sent ".$mod->scalarval()." to client");
 		return new XML_RPC_Response($val);
 		// Ooops, couldn't open the file so return an error message.
