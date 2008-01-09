943892e7373c24062ab31bb06205f71da1208a58
paulscott <paulscott@gmail.com>
2008-01-09 12:32:58 +0000

    updated post view as per bug #0002350

--- app/core_modules/modulecatalogue/templates/content/skins_tpl.php
+++ app/core_modules/modulecatalogue/templates/content/skins_tpl.php
@@ -1,6 +1,18 @@
 <?php
+$cssLayout = $this->newObject('csslayout', 'htmlelements');
+
+//set columns to 2
+$cssLayout->setNumColumns(2);
+
+//add left column
+$cssLayout->setLeftColumnContent($this->objSideMenu->show($activeCat).$this->objTagCloud);
+
 $this->appendArrayVar('headerParams',"<script type='text/javascript' src='core_modules/modulecatalogue/resources/remote.js'></script>");
 
-echo $this->objCatalogueConfig->skinRemoter($skins);
+
+//set middle content
+$cssLayout->setMiddleColumnContent($this->objCatalogueConfig->skinRemoter($skins));
+
+echo $cssLayout->show(); 
 
 ?>
