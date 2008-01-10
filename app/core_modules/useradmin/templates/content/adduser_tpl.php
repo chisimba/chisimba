46c6d2d1444a3d682633a03bc2b2578ef7fc7524
dkeats <dkeats@gmail.com>
2008-01-10 13:34:29 +0000

    Made the country dropdown alphabetical

--- app/core_modules/useradmin/templates/content/adduser_tpl.php
+++ app/core_modules/useradmin/templates/content/adduser_tpl.php
@@ -1,6 +1,6 @@
 <script type="text/javascript">
 //<![CDATA[
-    
+
     function checkAccountType(value)
     {
         if (value == 'ldap') {
@@ -15,7 +15,7 @@
             document.getElementById('ldappass').style.display = 'none';
         }
     }
-    
+
 //]]>
 </script>
 <?php
@@ -49,34 +49,34 @@ $messages = array();
     $firstname = new textinput ('useradmin_firstname');
     $firstname->size = 30;
     $firstname->extra = ' maxlength="50"';
-    
+
     if ($mode == 'addfixup') {
         $firstname->value = $this->getParam('useradmin_firstname');
-        
+
         if ($this->getParam('useradmin_firstname') == '') {
             $messages[] = $this->objLanguage->languageText('mod_userdetails_enterfirstname', 'userdetails');
         }
     }
-    
+
     $surname = new textinput ('useradmin_surname');
     $surname->size = 30;
     $surname->extra = ' maxlength="50"';
-    
+
     if ($mode == 'addfixup') {
         $surname->value = $this->getParam('useradmin_surname');
-        
+
         if ($this->getParam('useradmin_surname') == '') {
             $messages[] = $this->objLanguage->languageText('mod_userdetails_entersurname', 'userdetails');
         }
     }
-    
+
     $email = new textinput ('useradmin_email');
     $email->size = 30;
     $email->extra = ' maxlength="100"';
-    
+
     if ($mode == 'addfixup') {
         $email->value = $this->getParam('useradmin_email');
-        
+
         if ($this->getParam('useradmin_email') == '') {
             $messages[] = $this->objLanguage->languageText('mod_userdetails_enteremailaddress', 'userdetails');
         } else if (!$this->objUrl->isValidFormedEmailAddress($this->getParam('useradmin_email'))) {
@@ -96,7 +96,7 @@ if ($mode == 'addfixup') {
 
 if ($mode == 'addfixup' && count($messages) > 0) {
     echo '<ul><li><span class="error">'.$this->objLanguage->languageText('mod_userdetails_infonotsavedduetoerrors', 'userdetails').'</span>';
-    
+
     echo '<ul>';
         foreach ($messages as $message)
         {
@@ -122,7 +122,7 @@ $table = $this->newObject('htmltable', 'htmlelements');
 // Title
 $table->startRow();
     $label = new label ($this->objLanguage->languageText('word_title', 'system'), 'input_useradmin_title');
-    
+
     $objDropdown = new dropdown('useradmin_title');
     $titles=array("title_mr", "title_miss", "title_mrs", "title_ms", "title_dr", "title_prof", "title_rev", "title_assocprof");
     foreach ($titles as $title)
@@ -130,11 +130,11 @@ $table->startRow();
         $_title=trim($objLanguage->languageText($title));
         $objDropdown->addOption($_title,$_title);
     }
-    
+
     if ($mode == 'addfixup') {
         $objDropdown->setSelected($this->getParam('useradmin_title'));
     }
-    
+
     $table->addCell($label->show(), 140);
     $table->addCell('&nbsp;');
     $table->addCell($objDropdown->show());
@@ -143,9 +143,9 @@ $table->endRow();
 // Firstname
 $table->startRow();
     $label = new label ($this->objLanguage->languageText('phrase_firstname', 'system'), 'input_useradmin_firstname');
-    
-    
-    
+
+
+
     $table->addCell($label->show());
     $table->addCell('&nbsp;');
     $table->addCell($firstname->show().' * <span class="warning">Required</span>');
@@ -154,9 +154,9 @@ $table->endRow();
 // Surname
 $table->startRow();
     $label = new label ($this->objLanguage->languageText('word_surname', 'system'), 'input_useradmin_surname');
-    
-    
-    
+
+
+
     $table->addCell($label->show());
     $table->addCell('&nbsp;');
     $table->addCell($surname->show().' * <span class="warning">Required</span>');
@@ -175,9 +175,9 @@ $table->endRow();
 // Email
 $table->startRow();
     $label = new label ($this->objLanguage->languageText('phrase_emailaddress', 'system'), 'input_useradmin_email');
-    
 
-    
+
+
     $table->addCell($label->show());
     $table->addCell('&nbsp;');
     $table->addCell($email->show().' * <span class="warning">Required</span>');
@@ -199,13 +199,13 @@ $table->startRow();
     $sexRadio->addOption('M', $this->objLanguage->languageText('word_male', 'system'));
     $sexRadio->addOption('F', $this->objLanguage->languageText('word_female', 'system'));
     $sexRadio->setBreakSpace(' &nbsp; ');
-    
+
     $sexRadio->setSelected('M');
-    
+
     if ($mode == 'addfixup') {
         $sexRadio->setSelected($this->getParam('useradmin_sex'));
     }
-    
+
     $table->addCell($this->objLanguage->languageText('word_sex', 'system'));
     $table->addCell('&nbsp;');
     $table->addCell($sexRadio->show());
@@ -215,13 +215,13 @@ $table->endRow();
 $table->startRow();
     //$objCountries=&$this->getObject('countries','utilities');
     $objCountries=$this->getObject('languagecode','language');
-    
+
     $table->addCell($this->objLanguage->languageText('word_country', 'system'));
     $table->addCell('&nbsp;');
     //if ($mode == 'addfixup') {
-        $table->addCell($objCountries->country());
+        $table->addCell($objCountries->countryAlpha());
     // } else {
-    
+
     // }
 $table->endRow();
 
@@ -235,19 +235,19 @@ $table->endRow();
 $table->startRow();
     $table->addCell('Account Status');
     $table->addCell('&nbsp;');
-    
+
     $accountStatusRadio = new radio('accountstatus');
     $accountStatusRadio->addOption(1, 'Active');
     $accountStatusRadio->addOption(0, 'Inactive');
     $accountStatusRadio->setBreakSpace(' ');
     //$accountTypeRadio->extra = 'onclick="checkAccountType(this.value);"';
-    
+
     if ($mode == 'addfixup') {
         $accountStatusRadio->setSelected($this->getParam('accountstatus'));
     } else {
         $accountStatusRadio->setSelected(1);
     }
-    
+
     $table->addCell($accountStatusRadio->show());
 $table->endRow();
 
@@ -256,19 +256,19 @@ if ($this->objConfig->getuseLDAP() == 'TRUE') {
     $table->startRow();
         $table->addCell('Type of Account');
         $table->addCell('&nbsp;');
-        
+
         $accountTypeRadio = new radio('accounttype');
         $accountTypeRadio->addOption('ldap', ' Network ID Authentication');
         $accountTypeRadio->addOption('useradmin', ' Site / Database Authentication');
         $accountTypeRadio->setBreakSpace('<br />');
         $accountTypeRadio->extra = 'onclick="checkAccountType(this.value);"';
-        
+
         if ($mode == 'addfixup') {
             $accountTypeRadio->setSelected($this->getParam('accounttype'));
         } else {
             $accountTypeRadio->setSelected('useradmin');
         }
-        
+
         $table->addCell($accountTypeRadio->show());
     $table->endRow();
 } else {
@@ -279,25 +279,25 @@ if ($this->objConfig->getuseLDAP() == 'TRUE') {
 // Username
 $table->startRow();
     $label = new label ('Username', 'input_useradmin_username');
-    
+
     $textinput = new textinput ('useradmin_username');
     $textinput->size = 30;
     $textinput->extra = ' maxlength="25"';
-    
+
     $usernameAddition = ' * <span class="warning">Required</span>';
-    
+
     if ($mode == 'addfixup') {
         $textinput->value = $this->getParam('useradmin_username');
-        
+
         if ($this->getParam('useradmin_username') == '') {
             $messages[] = 'Username cannot be blank';
         }
-        
+
         if (in_array('usernametaken', $problems)) {
             $usernameAddition = ' * <span class="warning">Username Taken</span>';
         }
     }
-    
+
     $table->addCell($label->show());
     $table->addCell('&nbsp;');
     $table->addCell($textinput->show().$usernameAddition);
@@ -306,27 +306,27 @@ $table->endRow();
 // Password
 $table->startRow();
     $label = new label ('Password', 'input_useradmin_password');
-    
+
     $textinput = new textinput ('useradmin_password');
     $textinput->fldType = 'password';
     $textinput->size = 15;
     $textinput->extra = ' autocomplete="off"';
-    
+
     if ($mode == 'addfixup') {
         $howcreated = strtoupper($this->getParam('accounttype'));
     } else {
         $howcreated = 'useradmin';
     }
-    
+
     $ldappass = '';
     $leaveblank = ' * Required';
-    
+
     if ($mode == 'addfixup' && $this->getParam('accounttype') == 'useradmin') {
         $leaveblank = ' * <span class="warning">Required</span>';
     } else if ($howcreated == 'LDAP'){
         $leaveblank = '* Required if you want Site Authentication';
     }
-    
+
     if ($howcreated == 'LDAP') {
         $textinput->extra .= ' disabled="disabled"';
         $passMsg = '<span id="leaveblank" style="display:none;">'.$leaveblank.'</span>';
@@ -335,27 +335,27 @@ $table->startRow();
         $passMsg = '<span id="leaveblank">'.$leaveblank.'</span>';
         $ldapMsg = '<span id="ldappass" style="display:none;">'.$ldappass.'</span>';
     }
-    
+
     $table->addCell($label->show());
     $table->addCell('&nbsp;');
-    
-    
+
+
     $table->addCell($textinput->show().' '.$passMsg.$ldapMsg);
 $table->endRow();
 
 // Repeat Password
 $table->startRow();
     $label = new label ('Repeat Password', 'input_useradmin_repeatpassword');
-    
+
     $textinput = new textinput ('useradmin_repeatpassword');
     $textinput->fldType = 'password';
     $textinput->size = 15;
     $textinput->extra = ' autocomplete="off"';
-    
+
     if ($howcreated == 'LDAP') {
         $textinput->extra .= ' disabled="disabled"';
     }
-    
+
     $table->addCell($label->show());
     $table->addCell('&nbsp;');
     $table->addCell($textinput->show());
@@ -378,7 +378,7 @@ $form->addToForm('<div style="width:60%; float:left; padding:5px;">');
 $form->addToForm('<h3>'.$this->objLanguage->languageText('phrase_userinformation', 'userdetails').':</h3>');
 $form->addToForm($table->show());
 $form->addToForm('</div>');
-    
+
 $objModule = $this->getObject('modules', 'modulecatalogue');
 if ($objModule->checkIfRegistered('filemanager')) {
     $form->addToForm('<div><div style="width:25%;  float: left; padding: 5px;">');
@@ -386,13 +386,13 @@ if ($objModule->checkIfRegistered('filemanager')) {
 
 
 
-    
 
- 
+
+
     $objSelectFile = $this->getObject('selectimage', 'filemanager');
     $objSelectFile->name = 'imageselect';
     $objSelectFile->restrictFileList = array('jpg', 'gif', 'png', 'jpeg', 'bmp');
-    
+
     if ($mode == 'addfixup') {
         $objSelectFile->setDefaultFile($this->getParam('imageselect'));
     }
