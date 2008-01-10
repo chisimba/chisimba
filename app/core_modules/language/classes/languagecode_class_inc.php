06bc71eb902d9f822f92988bfd35f2b2700d6b12
dkeats <dkeats@gmail.com>
2008-01-10 12:15:16 +0000

    Cleaned up cruft

--- app/core_modules/language/classes/languagecode_class_inc.php
+++ app/core_modules/language/classes/languagecode_class_inc.php
@@ -3,35 +3,35 @@
 /**
  * This class converts retrieves the name of a language by providing the ISO code and also vice versa
  *
- * The original list of code was taken from a class written by Florian Breit (florian at phpws dot org): 
+ * The original list of code was taken from a class written by Florian Breit (florian at phpws dot org):
  *  http://www.phpclasses.org/browse/file/8143.html
- * 
+ *
  * PHP version 5
- * 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @category  Chisimba
  * @package   language
  * @author    Prince Mbekwa <pmbekwa@uwc.ac.za>
  * @copyright 2007 Prince Mbekwa
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   CVS: $Id$
  * @link      http://avoir.uwc.ac.za
  * @see       http://www.phpclasses.org/browse/file/8143.html
  */
 /**
-*This is a Languagecode class 
+*This is a Languagecode class
 * @author    Prince Mbekwa
 * @copyright (c) 200-2004 University of the Western Cape
 * @Version   1
@@ -41,7 +41,7 @@
  *Description of the class
 * This class converts retrieves the name of a language by providing the ISO code and also vice versa
 *
-* The original list of code was taken from a class written by Florian Breit (florian at phpws dot org): 
+* The original list of code was taken from a class written by Florian Breit (florian at phpws dot org):
 *  http://www.phpclasses.org/browse/file/8143.html
 */
 require_once 'I18Nv2/Country.php';
@@ -63,19 +63,19 @@ require_once 'I18Nv2/DecoratedList/HtmlEntities.php';
 
 /**
  * Short description for class
- * 
+ *
  * Long description (if any) ...
- * 
+ *
  * @category  Chisimba
  * @package   language
  * @author    Prince Mbekwa <pmbekwa@uwc.ac.za>
  * @copyright 2007 Prince Mbekwa
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   Release: @package_version@
  * @link      http://avoir.uwc.ac.za
  * @see       References to other sections (if any)...
  */
-class languagecode extends object 
+class languagecode extends object
 {
 	/**
      * Config object
@@ -118,22 +118,14 @@ class languagecode extends object
      * @var    string
     */
     private $_errorCallback;
-    
+
     /**
-    * Standard constructor method 
+    * Standard constructor method
     */
     function init()
-    { 
+    {
     	try {
     	 	$this->objConfig = $this->getObject('altconfig','config');
-    	 	if(!class_exists('I18Nv2_Country'))
-    	 	{
-    	 		//echo "no country class yet...";
-//    	 		require_once $this->getPearResource('I18Nv2/Country.php');
-	//	 		require_once $this->getPearResource('I18Nv2/Negotiator.php');
-		//		require_once $this->getPearResource('I18Nv2/DecoratedList/HtmlSelect.php');
-			//	require_once $this->getPearResource('I18Nv2/DecoratedList/HtmlEntities.php');
-    	 	}
     	 	$this->lan = $this->objConfig->getdefaultLanguage();
     	 	$neg = &new I18Nv2_Negotiator;
     	 	$this->objcountry = &new I18Nv2_Country("{$this->lan}", 'iso-8859-1');
@@ -147,7 +139,7 @@ class languagecode extends object
     		die();
     	}
 	}
-    
+
     /**
     * Method to get the name of a language by providing the ISO Code
     *
@@ -157,14 +149,14 @@ class languagecode extends object
     * @return string |NULL The Name of the Language
     */
     public function getLanguage($isoKey)
-    {    	
+    {
         if (array_key_exists(strtolower($isoKey), $this->iso_639_2_tags->codes)) {
             return $this->iso_639_2_tags->codes[strtolower($isoKey)];
         } else {
             return NULL;
         }
     }
-    
+
     /**
     * Method to get the name of a ISO Code of a language by providing the ISO Code
     *
@@ -173,28 +165,27 @@ class languagecode extends object
     */
     public function getISO($language)
     {
-    	
+
         // Flip Array - makes key the values, and values the key
-        //$tempArray = array_flip($this->iso_639_2_tags); 
         $tempArray = $this->iso_639_2_tags->codes;
         // Upper Case the first letter of the Word
         $language = strtolower($language);
         if (array_key_exists($language, $tempArray)) {
         	//$tempArray = array_flip($tempArray);
-        	      	
+
             return $language;
         } else {
-        	       	
+
             return NULL;
         }
     }
     /**
      *  This method utilizes ob_iconv_handler(), so you should call it at the beginning of your script (prior to any output).
      * Automatically transform output between character sets
-     * 
+     *
      * @param string $ocs desired output character set
-     * @param string $ics current intput character set 
-	 * @return Returns TRUE on success, PEAR_Error on failure. 
+     * @param string $ics current intput character set
+	 * @return Returns TRUE on success, PEAR_Error on failure.
      */
     public function autoConv($ocs,$ics)
     {
@@ -204,7 +195,7 @@ class languagecode extends object
     		$this->errorCallback ('Caught exception: '.$e->getMessage());
     		 exit();
     	}
-    	    	
+
     }//end function
     /**
      *  Function provides country and language lists.
@@ -213,7 +204,6 @@ class languagecode extends object
      */
     public function country($country=NULL)
     {
-    		
 		// set a selected entry
 		$language = strtoupper($this->objConfig->getCountry());
 		$this->objselect->attributes['select']['name'] = 'country';
@@ -221,14 +211,13 @@ class languagecode extends object
 		if ($country) {
 			$language = $country;
 		}else{
-		$language = strtoupper($this->objConfig->getCountry());
+            $language = strtoupper($this->objConfig->getCountry());
 		}
 		$this->objselect->selected["{$language}"] = true;
-		
 		// print a HTML safe select box
 		return  $this->objselect->getAllCodes();
     }
-    
+
      /**
      *  Function provides country and language lists.
      *
@@ -243,14 +232,14 @@ class languagecode extends object
 		if ($country) {
 			$language = $country;
 		}else{
-		$language = strtoupper($this->objConfig->getCountry());
+		  $language = strtoupper($this->objConfig->getCountry());
 		}
 		$this->objselect->selected["{$language}"] = true;
-		
+
 		// print a HTML safe select box
 		return  $this->objentity->getAllCodes();
     }
-    
+
     /**
      *  Function provides decorated classes for country and language lists.
      *
@@ -261,15 +250,15 @@ class languagecode extends object
     	// set some attributes
 		$this->objselect->attributes['select']['name'] = 'country';
 		$this->objselect->attributes['select']['onchange'] = 'this.form.submit()';
-		
+
 		// set a selected entry
 		$language = strtoupper($this->objConfig->getCountry());
 		$this->objselect->selected["{$language}"] = true;
-		
+
 		// print a HTML safe select box
 		return  $this->objselect->getAllCodes();
     }
-    
+
     /**
      * Get the corresponding country name of the supplied two letter country code.
      *
@@ -277,14 +266,14 @@ class languagecode extends object
      * @return countryname
      */
     public function getName ($code){
-    	
+
     	return $this->objcountry->getName($code);
     }
     /**
     * The error callback function, defers to configured error handler
     *
     * @param  string $error
-    * @return void  
+    * @return void
     * @access public
     */
     public function errorCallback($exception)
