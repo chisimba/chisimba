c2e5ebfe8f8f3a3e145b3198f7741279c98c1ac0
paulscott <paulscott@gmail.com>
2008-01-11 14:06:48 +0000

    removed cruft

--- app/index.php
+++ app/index.php
@@ -1,19 +1,4 @@
 <?php
-
-/*
-// Commented out by Jeremy O'Connor 27/8/7
-// For timing. Please leave in.
-
-if (!function_exists('getMicrotime')) {
-		function getMicrotime() {
-			list($usec, $sec) = explode(" ", microtime());
-			return ((float)$usec + (float)$sec);
-		}
-	}
-
-$TIME_START = getMicrotime();
-*/
-
 /**
 * index.php
 *
@@ -21,36 +6,6 @@ $TIME_START = getMicrotime();
 *
 * @author Paul Scott based on methods by Sean Legassick
 *
-*/
-
-// URL Re-writing
-// by Jeremy O'Connor
-
-/*$uri = $_SERVER["REQUEST_URI"];
-//echo "[$uri]";
-//exit();
-
-// Matches module name
-// e.g. http://localhost:8080/chisimba_framework2/app/index.php/blog/
-
-if (preg_match('/^(.*)\/index.php\/(.+?)\/$/i', $uri, $matches)) {
-	header( 'Location: '.$matches[1].'/index.php?module='.$matches[2] );
-}
-
-// Matches module name and action
-// e.g. http://localhost:8080/chisimba_framework2/app/index.php/blog/viewblog/
-
-if (preg_match('/^(.*)\/index.php\/(.+?)\/(.+?)\/$/i', $uri, $matches)) {
-	header( 'Location: '.$matches[1].'/index.php?module='.$matches[2].'&action='.$matches[3] );
-}
-
-// Matches module name and action and id
-// e.g. http://localhost:8080/chisimba_framework2/app/index.php/blog/viewblog/
-
-if (preg_match('/^(.*)\/index.php\/(.+?)\/(.+?)\/(.+?)\/$/i', $uri, $matches)) {
-	header( 'Location: '.$matches[1].'/index.php?module='.$matches[2].'&action='.$matches[3].'&id='.$matches[4] );
-}
-// End of URL Re-writing
 */
 // checks for configuration file, if none found loads installation page
 if ( !file_exists( 'config/config.xml' ) ) {
@@ -61,20 +16,11 @@ if ( !file_exists( 'config/config.xml' ) ) {
 // execution aborted if it is not set.
 $GLOBALS['kewl_entry_point_run'] = true;
 
-// initialise error handling
-//require('lib/errorhandling/ErrorHandler.inc');
-//$error =& new ErrorHandler();
-
 // initialise the engine object
 require_once 'classes/core/engine_class_inc.php';
 $_globalObjEngine = new engine;
 
-//log_debug("page rendering started at ".strftime("%T"));
-
 // engine object created by core_classes_inc.php
 $_globalObjEngine->run();
-
-//echo $undefined_variable;
-//log_debug("page rendering finished at ".strftime("%T"));
 
 ?>
