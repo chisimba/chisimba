5a070f5174f3417ecd64f09b9d4bff4780074cca
paulscott <paulscott@gmail.com>
2008-01-15 14:22:26 +0000

    added adm rpc client class for mirroring to api

--- app/core_modules/config/classes/ini_class_inc.php
+++ app/core_modules/config/classes/ini_class_inc.php
@@ -415,7 +415,7 @@ class ini extends object
 		else {
 			// update the xml with the new server
 			$root =& $config->parseConfig($cfile, 'XML', array('name' => 'adm'));
-			log_debug($root->toArray());
+			log_debug($root);
 			$conf_serv =& $root->createSection($servarray['name']);
 			$conf_serv->createDirective('servername', $servarray['name']);
 			$conf_serv->createDirective('serverapiurl', $servarray['url']);
