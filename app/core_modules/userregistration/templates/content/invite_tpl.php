5ec305e4d4229fde2d7e78cabe6036938eda80d2
paulscott <paulscott@gmail.com>
2008-01-21 14:08:47 +0000

    strat of invite template

--- app/core_modules/userregistration/templates/content/invite_tpl.php
+++ app/core_modules/userregistration/templates/content/invite_tpl.php
@@ -4,6 +4,9 @@ $this->loadClass('form', 'htmlelements');
 $this->loadClass('textinput', 'htmlelements');
 $this->loadClass('label', 'htmlelements');
 $this->loadClass('htmlheading', 'htmlelements');
+$this->loadClass('htmlarea', 'htmlelements');
+
+$required = '<span class="warning"> * '.$this->objLanguage->languageText('word_required', 'system', 'Required').'</span>';
 
 $headerinv = new htmlheading();
 $headerinv->type = 1;
@@ -12,6 +15,40 @@ $headerinv->str = $this->objLanguage->languageText('phrase_invitemate', 'userreg
 $middleColumnContent = NULL;
 $middleColumnContent .= $headerinv->show();
 
+// start the form
+$form = new form ('invite', $this->uri(array('action'=>'sendinvite')));
+
+// form elements
+$table = $this->newObject('htmltable', 'htmlelements');
+$table->startRow();
+$friendname = new textinput('friend_firstname');
+$friendnameLabel = new label($this->objLanguage->languageText('friendname', 'userregistration').'&nbsp;', 'input_friendname');
+$table->addCell($friendnameLabel->show(), 150, NULL, 'right');
+$table->addCell('&nbsp;', 5);
+$table->addCell($friendname->show().$required);
+$table->endRow();
+
+// surname
+$table->startRow();
+$friendsurname = new textinput('friend_surname');
+$friendsurnameLabel = new label($this->objLanguage->languageText('friendsurname', 'userregistration').'&nbsp;', 'input_friendsurname');
+$table->addCell($friendsurnameLabel->show(), 150, NULL, 'right');
+$table->addCell('&nbsp;', 5);
+$table->addCell($friendsurname->show().$required);
+$table->endRow();
+
+// message to include to mate
+$table->startRow();
+$friendmsg = $this->newObject('htmlarea', 'htmlelements');
+$friendmsg->name = 'friend_msg';
+$friendmsgLabel = new label($this->objLanguage->languageText('friendmessage', 'userregistration').'&nbsp;', 'input_friendmsg');
+$table->addCell($friendmsgLabel->show(), 150, NULL, 'right');
+$table->addCell('&nbsp;', 5);
+$friendmsg->toolbarSet = 'simple';
+$table->addCell($friendmsg->show());
+$table->endRow();
+
+$middleColumnContent .= $table->show();
 $cssLayout = $this->getObject('csslayout', 'htmlelements');
 $cssLayout->setLeftColumnContent("DUDE!");
 $cssLayout->setMiddleColumnContent($middleColumnContent);
