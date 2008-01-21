63d524b3da9514f98152c2c52bbf3b636807dd67
paulscott <paulscott@gmail.com>
2008-01-21 13:01:02 +0000

    cleaned up

--- app/core_modules/userregistration/templates/layout/login_layout_tpl.php
+++ app/core_modules/userregistration/templates/layout/login_layout_tpl.php
@@ -1,13 +1,7 @@
 <?php
-
 $objBlocks = $this->getObject('blocks', 'blocks');
-
-
-
 $cssLayout = $this->getObject('csslayout', 'htmlelements');
 $cssLayout->setLeftColumnContent($objBlocks->showBlock('login', 'security'));
 $cssLayout->setMiddleColumnContent($this->getContent());
-
 echo $cssLayout->show();
-
 ?>
