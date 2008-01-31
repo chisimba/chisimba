8c76c9c3dcb2599876519fe4e8d51ce77e51c58e
tohir <tohir@gmail.com>
2008-01-31 17:22:49 +0000

    do not show legend if it doesn't have a value

--- app/core_modules/htmlelements/classes/fieldset_class_inc.php
+++ app/core_modules/htmlelements/classes/fieldset_class_inc.php
@@ -35,40 +35,40 @@ Public License
 
 class fieldset extends object implements ifhtml
 {
-	/**
-	*@var $legend The heading of the frameset
-	*/
-	public $legend;
-	/**
-	*@var $legendalign The alignment for the legend
-	*/
-	public $legendalign;
+    /**
+    *@var $legend The heading of the frameset
+    */
+    public $legend;
+    /**
+    *@var $legendalign The alignment for the legend
+    */
+    public $legendalign;
 
-	/**
-	*@var $content The contents of the frameset
-	*/
-	public $contents;
+    /**
+    *@var $content The contents of the frameset
+    */
+    public $contents;
         /**
-	*@var $width The width attribute
-	*/
-	public $width;
+    *@var $width The width attribute
+    */
+    public $width;
         /**
-	*@var $extra Any other extra items that needs to be added
-	*/
-	public $extra;
+    *@var $extra Any other extra items that needs to be added
+    */
+    public $extra;
 
         /**
         * @var $align how the table is aligned - added 2005 03 31 by James Scoble
         */
         var $align;
 
-	/**
-	*Initialize
-	*/
-	public function init()
-	{
-		$this->contents="";
-	}
+    /**
+    *Initialize
+    */
+    public function init()
+    {
+        $this->contents="";
+    }
 
     /**
     * Alternate constructor for including the file
@@ -78,13 +78,13 @@ class fieldset extends object implements ifhtml
         return $this->init();
     }
 
-	/**
-	*The show Method
+    /**
+    *The show Method
         * @return null
         * @access public
-	*/
-	public function show()
-	{
+    */
+    public function show()
+    {
             $str="";
             //Add the width if it exists !added by derek
             $align='';
@@ -94,7 +94,7 @@ class fieldset extends object implements ifhtml
             if (isset($this->width)) {
                 $str.= "<table $align width=\"" . $this->width . "\"><tr><td>";
             }
-	    $str .= '<fieldset';
+        $str .= '<fieldset';
 
         if (isset($this->extra)) {
             $str.=' '.$this->extra;
@@ -104,60 +104,60 @@ class fieldset extends object implements ifhtml
 
         $str .= '>';
 
-        if (isset($this->legend)) {
+        if (isset($this->legend) && $this->legend != '') {
             $str .= '<legend';
             if (isset($this->legendalign)) {
                 $str .= ' align="' . $this->legendalign . '"';
             }
             $str .= '>'.$this->legend.'</legend>';
         }
-		$str.=$this->contents;
-		$str.='</fieldset>';
+        $str.=$this->contents;
+        $str.='</fieldset>';
         //End the width if it exists !added by derek
         if (isset($this->width)) {
             $str.= "</td></tr></table>";
         }
-		return $str;
-	}
+        return $str;
+    }
 
-	/**
-	*Method to add contents
-	*@param $content string  The contents to be added to the fieldset
+    /**
+    *Method to add contents
+    *@param $content string  The contents to be added to the fieldset
         * @return null  
         * @access public
-	*/
-	public function addContent($content=null){
-		$this->contents.=$content;
-	}
+    */
+    public function addContent($content=null){
+        $this->contents.=$content;
+    }
 
-	/**
-	*Method to reset the fields
+    /**
+    *Method to reset the fields
         * @return null  
         * @access public
-	*/
-	public function reset(){
-		$this->contents=null;
-		$this->legend=null;
-	}
+    */
+    public function reset(){
+        $this->contents=null;
+        $this->legend=null;
+    }
 
-	/**
-	*Method to add the legend
-	*@param $legend string  The legend to be added to the fieldset
+    /**
+    *Method to add the legend
+    *@param $legend string  The legend to be added to the fieldset
         * @return null  
         * @access public
-	*/
-	function setLegend($legend){
-		$this->legend=$legend;
-	}
+    */
+    function setLegend($legend){
+        $this->legend=$legend;
+    }
 
         /**
-	*Method to add extra parameters
-	*@param $parameters string  String of parameters that can be added
+    *Method to add extra parameters
+    *@param $parameters string  String of parameters that can be added
         * @return null  
         * @access public
-	*/
-	public function setExtra($parameters){
-		$this->extra=$parameters;
-	}
+    */
+    public function setExtra($parameters){
+        $this->extra=$parameters;
+    }
 }
 ?>
