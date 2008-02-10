9caa00aaaf8ad3df97564eba5c5f8e8a08119ea6
dkeats <dkeats@gmail.com>
2008-02-10 17:06:15 +0000

    Added a filter to insert a colourbox.

---
+++ app/core_modules/filters/classes/parse4colorbox_class_inc.php
@@ -0,0 +1,139 @@
+<?php
+/**
+* Class to parse a string (e.g. page content) that contains a filter
+* code for including the enclosed text in a coloured box (pastel colours)
+*
+* PHP version 5
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the
+* Free Software Foundation, Inc.,
+* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+* @category  Chisimba
+* @package   filters
+* @author    Derek Keats <dkeats@uwc.ac.za>
+* @copyright 2007 Derek Keats
+* @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+* @version   CVS: $Id$
+* @link      http://avoir.uwc.ac.za
+*/
+
+
+
+/**
+*
+* Class to parse a string (e.g. page content) that contains a filter
+* code for including the enclosed text in a coloured box (pastel colours))
+*
+* @author Derek Keats
+*
+*/
+class parse4colorbox extends object
+{
+
+    /**
+    *
+    * @var string $objLanguage String object property for holding the
+    * language object
+    * @access public
+    *
+    */
+    public $objLanguage;
+
+    /**
+    *
+    * String object $objExpar is a string to hold the parameter extractor object
+    * @access public
+    *
+    */
+    public $objExpar;
+
+    /**
+     *
+     * String $type is the type of display item
+     * Valid: text, textime
+     * @access public
+     *
+     */
+    public $type;
+
+    /**
+     *
+     * Constructor for the TWITTER filter parser
+     *
+     * @return void
+     * @access public
+     *
+     */
+    public function init()
+    {
+        // Get an instance of the language object
+        $this->objLanguage = $this->getObject('language', 'language');
+        // Get an instance of the params extractor
+        $this->objExpar = $this->getObject("extractparams", "utilities");
+
+    }
+
+    /**
+    *
+    * Method to parse the string
+    * @param  string $str The string to parse
+    * @return string The parsed string
+    *
+    */
+    public function parse($txt)
+    {
+       	//Match filters based on a wordpress style
+        //Note the ? in the regex is important to enable the multiline
+        //   feature, else it greedy
+        preg_match_all('/(\\[COLORBOX:)(.*?)\\](.*?)(\\[\\/COLORBOX\\])/ism', $txt, $results);
+       	$counter = 0;
+       	foreach ($results[3] as $item) {
+            //Parse for the parameters
+            $str = trim($results[2][$counter]);
+            //The whole match must be replaced
+            $replaceable = $results[0][$counter];
+            //echo "!!!$str!!!<br/>";
+        	$ar= $this->objExpar->getArrayParams($str, ",");
+            $this->setupPage();
+          	$replacement = $this->getBox($this->boxtype, $item);
+        	$txt = str_replace($replaceable, $replacement, $txt);
+        	$counter++;
+        }
+        return $txt;
+    }
+
+    /**
+    *
+    * Method to set up the parameter / value pairs for th efilter
+    * @access public
+    * @return VOID
+    *
+    */
+    private function setUpPage()
+    {
+        //Get username
+        if (isset($this->objExpar->boxtype)) {
+            $this->boxtype = $this->objExpar->boxtype;
+        } else {
+            $this->boxtype=NULL;
+        }
+
+    }
+
+    private function getBox($boxtype, &$item)
+    {
+        $oT = $this->getObject("colorbox", "utilities");
+        return $oT->show($boxtype, $item);
+    }
+
+}
---
+++ app/core_modules/utilities/classes/colorbox_class_inc.php
@@ -0,0 +1,89 @@
+<?php
+/**
+*
+* Colorbox inserts text into coloured boxes
+*
+* Colorbox inserts text into coloured boxes, for example as used in
+* the colorbox filter
+*
+*
+* PHP version 5
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the
+* Free Software Foundation, Inc.,
+* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+* @category  Chisimba
+* @package   sdfasad
+* @author    Administrative User <admin@localhost.local>
+* @copyright 2007 AVOIR
+* @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+* @version   CVS: $Id$
+* @link      http://avoir.uwc.ac.za
+*/
+
+// security check - must be included in all scripts
+if (!
+/**
+ * The $GLOBALS is an array used to control access to certain constants.
+ * Here it is used to check if the file is opening in engine, if not it
+ * stops the file from running.
+ *
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ *
+ */
+$GLOBALS['kewl_entry_point_run'])
+{
+        die("You cannot view this page directly");
+}
+// end security check
+/**
+*
+* Colorbox inserts text into coloured boxes
+*
+* Colorbox inserts text into coloured boxes, for example as used in
+* the colorbox filter
+*
+* @category  Chisimba
+* @author Derek Keats
+* @author Paul Scott <pscott@uwc.ac.za>
+* @copyright UWC and AVOIR under the GPL
+* @package   utilities
+* @copyright 2007 AVOIR
+* @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General
+Public License
+* @version   $Id$
+* @link      http://avoir.uwc.ac.za
+*/
+class colorbox extends object
+{
+
+
+	/**
+	 * Constructor method. It does nothing here
+	 *
+	 * @access public
+	 * @param void
+	 * @return VOID
+	 *
+	 */
+	public function init()
+	{
+	}
+
+    public function show($boxColor, $txt)
+    {
+        return "<div class=\"colorbox $boxColor\">$txt</div>";
+    }
+}
+?>
