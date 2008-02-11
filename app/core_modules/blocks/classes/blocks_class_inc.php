1d46c646a6c7cbd5310c29312072f8e7e6d2025e
paulscott <paulscott@gmail.com>
2008-02-11 13:12:25 +0000

    fixed call time pass by ref

--- app/core_modules/blocks/classes/blocks_class_inc.php
+++ app/core_modules/blocks/classes/blocks_class_inc.php
@@ -185,7 +185,7 @@ class blocks extends object
 
     public function blockExists(&$block, &$module)
     {
-        if ($this->isCoreBlock(&$block, &$module) || $this->isModuleBlock(&$block, &$module)) {
+        if ($this->isCoreBlock($block, $module) || $this->isModuleBlock($block, $module)) {
             return TRUE;
         } else {
             return FALSE;
