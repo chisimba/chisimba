a9ad8d7854745cec924e6dd7e14f677717f9588d
paulscott <paulscott@gmail.com>
2008-02-14 12:48:06 +0000

    adedd exception handling to PDO support

--- app/core_modules/modulecatalogue/templates/content/remote_tpl.php
+++ app/core_modules/modulecatalogue/templates/content/remote_tpl.php
@@ -1,4 +1,8 @@
 <?php
+if(!isset($upgradables))
+{
+	$upgradables = NULL;
+}
 $this->appendArrayVar('headerParams',"<script type='text/javascript' src='core_modules/modulecatalogue/resources/remote.js'></script>");
 $this->loadClass('checkbox','htmlelements');
 $this->loadClass('link','htmlelements');
