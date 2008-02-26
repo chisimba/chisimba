620f7a516f0480e478fe7a24344e3eae0a08c22e
dkeats <dkeats@gmail.com>
2008-02-26 20:28:34 +0000

    Oops, changed path back to core_modules

--- app/core_modules/utilities/classes/washout_class_inc.php
+++ app/core_modules/utilities/classes/washout_class_inc.php
@@ -66,8 +66,7 @@ class washout extends object
 			// save cwd
 			$savedDir = getcwd();
 			//load up all of the parsers from filters
-			//$filterDir = $this->objConfig->getsiteRootPath() . "core_modules/filters/classes/";
-            $filterDir = $this->objConfig->getModulePath() . "filters/classes/";
+			$filterDir = $this->objConfig->getsiteRootPath() . "core_modules/filters/classes/";
 			chdir($filterDir);
 			$parsers = glob("parse4*_class_inc.php");
 			// restore path
