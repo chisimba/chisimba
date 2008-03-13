e0299d7116fce01308e878b2e76bd0b95370c606
dkeats <dkeats@gmail.com>
2008-03-13 20:20:58 +0000

    started work on using the curl wrapper

--- app/core_modules/utilities/classes/s3_class_inc.php
+++ app/core_modules/utilities/classes/s3_class_inc.php
@@ -1,192 +1,192 @@
 <?php
-class s3 //extends object
+class s3 extends object
 {
 	public $mime_types = array(
-	"323" => "text/h323", 
-	"acx" => "application/internet-property-stream", 
-	"ai" => "application/postscript", 
-	"aif" => "audio/x-aiff", 
-	"aifc" => "audio/x-aiff", 
+	"323" => "text/h323",
+	"acx" => "application/internet-property-stream",
+	"ai" => "application/postscript",
+	"aif" => "audio/x-aiff",
+	"aifc" => "audio/x-aiff",
 	"aiff" => "audio/x-aiff",
-	"asf" => "video/x-ms-asf", 
-	"asr" => "video/x-ms-asf", 
-	"asx" => "video/x-ms-asf", 
-	"au" => "audio/basic", 
-	"avi" => "video/quicktime", 
-	"axs" => "application/olescript", 
-	"bas" => "text/plain", 
-	"bcpio" => "application/x-bcpio", 
-	"bin" => "application/octet-stream", 
+	"asf" => "video/x-ms-asf",
+	"asr" => "video/x-ms-asf",
+	"asx" => "video/x-ms-asf",
+	"au" => "audio/basic",
+	"avi" => "video/quicktime",
+	"axs" => "application/olescript",
+	"bas" => "text/plain",
+	"bcpio" => "application/x-bcpio",
+	"bin" => "application/octet-stream",
 	"bmp" => "image/bmp",
-	"c" => "text/plain", 
-	"cat" => "application/vnd.ms-pkiseccat", 
-	"cdf" => "application/x-cdf", 
-	"cer" => "application/x-x509-ca-cert", 
-	"class" => "application/octet-stream", 
-	"clp" => "application/x-msclip", 
-	"cmx" => "image/x-cmx", 
-	"cod" => "image/cis-cod", 
-	"cpio" => "application/x-cpio", 
+	"c" => "text/plain",
+	"cat" => "application/vnd.ms-pkiseccat",
+	"cdf" => "application/x-cdf",
+	"cer" => "application/x-x509-ca-cert",
+	"class" => "application/octet-stream",
+	"clp" => "application/x-msclip",
+	"cmx" => "image/x-cmx",
+	"cod" => "image/cis-cod",
+	"cpio" => "application/x-cpio",
 	"crd" => "application/x-mscardfile",
-	"crl" => "application/pkix-crl", 
-	"crt" => "application/x-x509-ca-cert", 
-	"csh" => "application/x-csh", 
-	"css" => "text/css", 
-	"dcr" => "application/x-director", 
-	"der" => "application/x-x509-ca-cert", 
-	"dir" => "application/x-director", 
-	"dll" => "application/x-msdownload", 
-	"dms" => "application/octet-stream", 
+	"crl" => "application/pkix-crl",
+	"crt" => "application/x-x509-ca-cert",
+	"csh" => "application/x-csh",
+	"css" => "text/css",
+	"dcr" => "application/x-director",
+	"der" => "application/x-x509-ca-cert",
+	"dir" => "application/x-director",
+	"dll" => "application/x-msdownload",
+	"dms" => "application/octet-stream",
 	"doc" => "application/msword",
-	"dot" => "application/msword", 
-	"dvi" => "application/x-dvi", 
-	"dxr" => "application/x-director", 
-	"eps" => "application/postscript", 
-	"etx" => "text/x-setext", 
-	"evy" => "application/envoy", 
-	"exe" => "application/octet-stream", 
-	"fif" => "application/fractals", 
-	"flr" => "x-world/x-vrml", 
+	"dot" => "application/msword",
+	"dvi" => "application/x-dvi",
+	"dxr" => "application/x-director",
+	"eps" => "application/postscript",
+	"etx" => "text/x-setext",
+	"evy" => "application/envoy",
+	"exe" => "application/octet-stream",
+	"fif" => "application/fractals",
+	"flr" => "x-world/x-vrml",
 	"gif" => "image/gif",
-	"gtar" => "application/x-gtar", 
-	"gz" => "application/x-gzip", 
-	"h" => "text/plain", 
-	"hdf" => "application/x-hdf", 
-	"hlp" => "application/winhlp", 
-	"hqx" => "application/mac-binhex40", 
-	"hta" => "application/hta", 
-	"htc" => "text/x-component", 
-	"htm" => "text/html", 
+	"gtar" => "application/x-gtar",
+	"gz" => "application/x-gzip",
+	"h" => "text/plain",
+	"hdf" => "application/x-hdf",
+	"hlp" => "application/winhlp",
+	"hqx" => "application/mac-binhex40",
+	"hta" => "application/hta",
+	"htc" => "text/x-component",
+	"htm" => "text/html",
 	"html" => "text/html",
-	"htt" => "text/webviewhtml", 
-	"ico" => "image/x-icon", 
-	"ief" => "image/ief", 
-	"iii" => "application/x-iphone", 
-	"ins" => "application/x-internet-signup", 
-	"isp" => "application/x-internet-signup", 
-	"jfif" => "image/pipeg", 
-	"jpe" => "image/jpeg", 
-	"jpeg" => "image/jpeg", 
+	"htt" => "text/webviewhtml",
+	"ico" => "image/x-icon",
+	"ief" => "image/ief",
+	"iii" => "application/x-iphone",
+	"ins" => "application/x-internet-signup",
+	"isp" => "application/x-internet-signup",
+	"jfif" => "image/pipeg",
+	"jpe" => "image/jpeg",
+	"jpeg" => "image/jpeg",
 	"jpg" => "image/jpeg",
-	"js" => "application/x-javascript", 
-	"latex" => "application/x-latex", 
-	"lha" => "application/octet-stream", 
-	"lsf" => "video/x-la-asf", 
-	"lsx" => "video/x-la-asf", 
-	"lzh" => "application/octet-stream", 
-	"m13" => "application/x-msmediaview", 
-	"m14" => "application/x-msmediaview", 
-	"m3u" => "audio/x-mpegurl", 
+	"js" => "application/x-javascript",
+	"latex" => "application/x-latex",
+	"lha" => "application/octet-stream",
+	"lsf" => "video/x-la-asf",
+	"lsx" => "video/x-la-asf",
+	"lzh" => "application/octet-stream",
+	"m13" => "application/x-msmediaview",
+	"m14" => "application/x-msmediaview",
+	"m3u" => "audio/x-mpegurl",
 	"man" => "application/x-troff-man",
-	"mdb" => "application/x-msaccess", 
-	"me" => "application/x-troff-me", 
-	"mht" => "message/rfc822", 
-	"mhtml" => "message/rfc822", 
-	"mid" => "audio/mid", 
-	"mny" => "application/x-msmoney", 
-	"mov" => "video/quicktime", 
-	"movie" => "video/x-sgi-movie", 
-	"mp2" => "video/mpeg", 
+	"mdb" => "application/x-msaccess",
+	"me" => "application/x-troff-me",
+	"mht" => "message/rfc822",
+	"mhtml" => "message/rfc822",
+	"mid" => "audio/mid",
+	"mny" => "application/x-msmoney",
+	"mov" => "video/quicktime",
+	"movie" => "video/x-sgi-movie",
+	"mp2" => "video/mpeg",
 	"mp3" => "audio/mpeg",
-	"mpa" => "video/mpeg", 
-	"mpe" => "video/mpeg", 
-	"mpeg" => "video/mpeg", 
-	"mpg" => "video/mpeg", 
-	"mpp" => "application/vnd.ms-project", 
-	"mpv2" => "video/mpeg", 
-	"ms" => "application/x-troff-ms", 
-	"mvb" => "application/x-msmediaview", 
-	"nws" => "message/rfc822", 
+	"mpa" => "video/mpeg",
+	"mpe" => "video/mpeg",
+	"mpeg" => "video/mpeg",
+	"mpg" => "video/mpeg",
+	"mpp" => "application/vnd.ms-project",
+	"mpv2" => "video/mpeg",
+	"ms" => "application/x-troff-ms",
+	"mvb" => "application/x-msmediaview",
+	"nws" => "message/rfc822",
 	"oda" => "application/oda",
-	"p10" => "application/pkcs10", 
-	"p12" => "application/x-pkcs12", 
-	"p7b" => "application/x-pkcs7-certificates", 
-	"p7c" => "application/x-pkcs7-mime", 
-	"p7m" => "application/x-pkcs7-mime", 
-	"p7r" => "application/x-pkcs7-certreqresp", 
-	"p7s" => "application/x-pkcs7-signature", 
-	"pbm" => "image/x-portable-bitmap", 
-	"pdf" => "application/pdf", 
+	"p10" => "application/pkcs10",
+	"p12" => "application/x-pkcs12",
+	"p7b" => "application/x-pkcs7-certificates",
+	"p7c" => "application/x-pkcs7-mime",
+	"p7m" => "application/x-pkcs7-mime",
+	"p7r" => "application/x-pkcs7-certreqresp",
+	"p7s" => "application/x-pkcs7-signature",
+	"pbm" => "image/x-portable-bitmap",
+	"pdf" => "application/pdf",
 	"pfx" => "application/x-pkcs12",
-	"pgm" => "image/x-portable-graymap", 
-	"pko" => "application/ynd.ms-pkipko", 
-	"pma" => "application/x-perfmon", 
-	"pmc" => "application/x-perfmon", 
-	"pml" => "application/x-perfmon", 
-	"pmr" => "application/x-perfmon", 
-	"pmw" => "application/x-perfmon", 
-	"png" => "image/png", "pnm" => "image/x-portable-anymap", 
-	"pot" => "application/vnd.ms-powerpoint", 
+	"pgm" => "image/x-portable-graymap",
+	"pko" => "application/ynd.ms-pkipko",
+	"pma" => "application/x-perfmon",
+	"pmc" => "application/x-perfmon",
+	"pml" => "application/x-perfmon",
+	"pmr" => "application/x-perfmon",
+	"pmw" => "application/x-perfmon",
+	"png" => "image/png", "pnm" => "image/x-portable-anymap",
+	"pot" => "application/vnd.ms-powerpoint",
 	"ppm" => "image/x-portable-pixmap",
-	"pps" => "application/vnd.ms-powerpoint", 
-	"ppt" => "application/vnd.ms-powerpoint", 
-	"prf" => "application/pics-rules", 
-	"ps" => "application/postscript", 
-	"pub" => "application/x-mspublisher", 
-	"qt" => "video/quicktime", 
-	"ra" => "audio/x-pn-realaudio", 
-	"ram" => "audio/x-pn-realaudio", 
-	"ras" => "image/x-cmu-raster", 
+	"pps" => "application/vnd.ms-powerpoint",
+	"ppt" => "application/vnd.ms-powerpoint",
+	"prf" => "application/pics-rules",
+	"ps" => "application/postscript",
+	"pub" => "application/x-mspublisher",
+	"qt" => "video/quicktime",
+	"ra" => "audio/x-pn-realaudio",
+	"ram" => "audio/x-pn-realaudio",
+	"ras" => "image/x-cmu-raster",
 	"rgb" => "image/x-rgb",
-	"rmi" => "audio/mid", 
-	"roff" => "application/x-troff", 
-	"rtf" => "application/rtf", 
-	"rtx" => "text/richtext", 
-	"scd" => "application/x-msschedule", 
-	"sct" => "text/scriptlet", 
-	"setpay" => "application/set-payment-initiation", 
-	"setreg" => "application/set-registration-initiation", 
-	"sh" => "application/x-sh", 
+	"rmi" => "audio/mid",
+	"roff" => "application/x-troff",
+	"rtf" => "application/rtf",
+	"rtx" => "text/richtext",
+	"scd" => "application/x-msschedule",
+	"sct" => "text/scriptlet",
+	"setpay" => "application/set-payment-initiation",
+	"setreg" => "application/set-registration-initiation",
+	"sh" => "application/x-sh",
 	"shar" => "application/x-shar",
-	"sit" => "application/x-stuffit", 
-	"snd" => "audio/basic", 
-	"spc" => "application/x-pkcs7-certificates", 
-	"spl" => "application/futuresplash", 
-	"src" => "application/x-wais-source", 
-	"sst" => "application/vnd.ms-pkicertstore", 
-	"stl" => "application/vnd.ms-pkistl", 
-	"stm" => "text/html", 
-	"svg" => "image/svg+xml", 
+	"sit" => "application/x-stuffit",
+	"snd" => "audio/basic",
+	"spc" => "application/x-pkcs7-certificates",
+	"spl" => "application/futuresplash",
+	"src" => "application/x-wais-source",
+	"sst" => "application/vnd.ms-pkicertstore",
+	"stl" => "application/vnd.ms-pkistl",
+	"stm" => "text/html",
+	"svg" => "image/svg+xml",
 	"sv4cpio" => "application/x-sv4cpio",
-	"sv4crc" => "application/x-sv4crc", 
-	"t" => "application/x-troff", 
-	"tar" => "application/x-tar", 
-	"tcl" => "application/x-tcl", 
-	"tex" => "application/x-tex", 
-	"texi" => "application/x-texinfo", 
-	"texinfo" => "application/x-texinfo", 
-	"tgz" => "application/x-compressed", 
-	"tif" => "image/tiff", 
+	"sv4crc" => "application/x-sv4crc",
+	"t" => "application/x-troff",
+	"tar" => "application/x-tar",
+	"tcl" => "application/x-tcl",
+	"tex" => "application/x-tex",
+	"texi" => "application/x-texinfo",
+	"texinfo" => "application/x-texinfo",
+	"tgz" => "application/x-compressed",
+	"tif" => "image/tiff",
 	"tiff" => "image/tiff",
-	"tr" => "application/x-troff", 
-	"trm" => "application/x-msterminal", 
-	"tsv" => "text/tab-separated-values", 
-	"txt" => "text/plain", 
-	"uls" => "text/iuls", 
-	"ustar" => "application/x-ustar", 
-	"vcf" => "text/x-vcard", 
-	"vrml" => "x-world/x-vrml", 
-	"wav" => "audio/x-wav", 
+	"tr" => "application/x-troff",
+	"trm" => "application/x-msterminal",
+	"tsv" => "text/tab-separated-values",
+	"txt" => "text/plain",
+	"uls" => "text/iuls",
+	"ustar" => "application/x-ustar",
+	"vcf" => "text/x-vcard",
+	"vrml" => "x-world/x-vrml",
+	"wav" => "audio/x-wav",
 	"wcm" => "application/vnd.ms-works",
-	"wdb" => "application/vnd.ms-works", 
-	"wks" => "application/vnd.ms-works", 
-	"wmf" => "application/x-msmetafile", 
-	"wps" => "application/vnd.ms-works", 
-	"wri" => "application/x-mswrite", 
-	"wrl" => "x-world/x-vrml", 
-	"wrz" => "x-world/x-vrml", 
-	"xaf" => "x-world/x-vrml", 
-	"xbm" => "image/x-xbitmap", 
+	"wdb" => "application/vnd.ms-works",
+	"wks" => "application/vnd.ms-works",
+	"wmf" => "application/x-msmetafile",
+	"wps" => "application/vnd.ms-works",
+	"wri" => "application/x-mswrite",
+	"wrl" => "x-world/x-vrml",
+	"wrz" => "x-world/x-vrml",
+	"xaf" => "x-world/x-vrml",
+	"xbm" => "image/x-xbitmap",
 	"xla" => "application/vnd.ms-excel",
-	"xlc" => "application/vnd.ms-excel", 
-	"xlm" => "application/vnd.ms-excel", 
-	"xls" => "application/vnd.ms-excel", 
-	"xlt" => "application/vnd.ms-excel", 
-	"xlw" => "application/vnd.ms-excel", 
-	"xof" => "x-world/x-vrml", 
-	"xpm" => "image/x-xpixmap", 
-	"xwd" => "image/x-xwindowdump", 
-	"z" => "application/x-compress", 
+	"xlc" => "application/vnd.ms-excel",
+	"xlm" => "application/vnd.ms-excel",
+	"xls" => "application/vnd.ms-excel",
+	"xlt" => "application/vnd.ms-excel",
+	"xlw" => "application/vnd.ms-excel",
+	"xof" => "x-world/x-vrml",
+	"xpm" => "image/x-xpixmap",
+	"xwd" => "image/x-xwindowdump",
+	"z" => "application/x-compress",
 	"zip" => "application/zip"
 	);
 	private $_key;
@@ -199,15 +199,24 @@ class s3 //extends object
 	public function init()
 	{
 		$this->objLanguage = $this->getObject('language', 'language');
+        $this->objCurl = $this->getObject('curlwrapper', 'utilities');
+        // Read the key and secret
+        try
+        {
+            $this->objConfig = $this->getObject('dbsysconfig', 'sysconfig');
+            $this->_key = $this->objConfig->getValue('mod_buckets_key', 'buckets');
+            $this->_secret = $this->objConfig->getValue('mod_buckets_secret', 'buckets');
+        }
+        catch (customException $e)
+        {
+            echo customException::cleanUp($e);
+            exit();
+        }
 	}
-	
-	public function setup($key = null, $secret = null)
+
+	public function setup()
 	{
-		if($key && $secret)
-		{
-			$this->_key = $key;
-			$this->_secret = $secret;
-		}
+
 
 		// If the path to curl isn't set, try and auto-detect it
 		if($this->_pathToCurl == "")
@@ -250,12 +259,12 @@ class s3 //extends object
 		{
 			$this->deleteObject($bucket, $item["name"]);
 		}
-		
+
 	}
 
 	public function deleteObject($bucket, $object)
 	{
-		if($object[0] != "/" ) 
+		if($object[0] != "/" )
 		{
 			$object = "/$object";
 		}
@@ -265,7 +274,7 @@ class s3 //extends object
 		"headers" => null,
 		"resource" => "/$bucket" . $object,
 		);
-		
+
 		$result = $this->sendRequest($req);
 		return !$this->objectExists($bucket, $object);
 	}
@@ -284,7 +293,7 @@ class s3 //extends object
 		{
 			$object = "/$object";
 		}
-		
+
 		$req = array(	"verb" => "PUT",
 		"md5" => null,
 		"type" => null,
@@ -295,11 +304,11 @@ class s3 //extends object
 		"disposition" => $disposition,
 		"acl" => $acl,
 		);
-		
+
 		$this->sendRequest($req);
 
 		$info = $this->getObjectInfo($bucket, $object);
-		
+
 		return ($info['hash'] == md5_file($filename));
 	}
 
@@ -309,16 +318,16 @@ class s3 //extends object
 		{
 			$object = "/$object";
 		}
-		
+
 		$req = array(	"verb" => "GET",
 		"md5" => null,
 		"type" => null,
 		"headers" => null,
 		"resource" => "/$bucket" . $object,
 		);
-		
+
 		$result = $this->sendRequest($req);
-		
+
 		return $result;
 	}
 
@@ -328,7 +337,7 @@ class s3 //extends object
 		{
 			$object = "/$object";
 		}
-		
+
 		$req = array(	"verb" => "GET",
 		"md5" => null,
 		"type" => null,
@@ -336,7 +345,7 @@ class s3 //extends object
 		"resource" => "/$bucket" . $object,
 		"download" => $saveTo,
 		);
-		
+
 		$result = $this->sendRequest($req);
 		return $result;
 	}
@@ -354,7 +363,7 @@ class s3 //extends object
 		"headers" => null,
 		"resource" => "/",
 		);
-		
+
 		$result = $this->sendRequest($req);
 		preg_match_all("@<Name>(.*?)</Name>@", $result, $matches);
 		return $matches[1];
@@ -372,7 +381,7 @@ class s3 //extends object
 		//var_dump($req); die();
 		$result = $this->sendRequest($req);
 		//var_dump($result);
-		
+
 		return $this->isOk($result);
 	}
 
@@ -384,9 +393,9 @@ class s3 //extends object
 		"headers" => null,
 		"resource" => "/$bucket",
 		);
-		
+
 		$result = $this->sendRequest($req);
-		
+
 		return $this->isOk($result);
 	}
 
@@ -400,7 +409,7 @@ class s3 //extends object
 		else {
 			return TRUE;
 		}
-		
+
 	}
 
 	public function objectExists($bucket, $object)
@@ -410,18 +419,18 @@ class s3 //extends object
 
 	public function getObjectHead($bucket, $object)
 	{
-		if(substr($object, 0, 1) != "/" ) 
+		if(substr($object, 0, 1) != "/" )
 		{
 			$object = "/$object";
 		}
-		
+
 		$req = array(	"verb" => "HEAD",
 		"md5" => null,
 		"type" => null,
 		"headers" => null,
 		"resource" => "/$bucket" . $object,
 		);
-		
+
 		$result = $this->sendRequest($req);
 		return $result;
 	}
@@ -450,13 +459,13 @@ class s3 //extends object
 		"resource" => "/$bucket",
 		);
 
-		if($prefix[0] == "/") 
+		if($prefix[0] == "/")
 		{
 			$prefix[0] = "";
 		}
-		
+
 		$params = array("prefix" => trim($prefix), "marker" => $marker, "delimiter" => $delim);
-		
+
 		$result = $this->sendRequest($req, $params);
 		preg_match_all("@<Contents>(.*?)</Contents>@", $result, $matches);
 		$lastKey = "";
@@ -534,6 +543,7 @@ class s3 //extends object
 		{
 			$args[] = array("-X", $req['verb']);
 		}
+        //$objCurl = $this->getObject("curlwrapper", "utilities");
 
 		$curl = $this->_pathToCurl . " -s ";
 		foreach($args as $arg)
@@ -557,12 +567,12 @@ class s3 //extends object
 		{
 			$curl .= " -I ";
 		}
-		if(isset($req['download'])) 
+		if(isset($req['download']))
 		{
 			$curl .= ' -o "' . $req['download'] . '"';
 		}
-		//echo $curl; die();
-		return `$curl`;
+		echo $curl; die();
+		//return `$curl`;
 	}
 
 	public function signature($req)
@@ -573,7 +583,7 @@ class s3 //extends object
 		{
 			$req['headers'] = array();
 		}
-		
+
 		if(isset($req['acl']))
 		{
 			$req['headers']["x-amz-acl"] = $req['acl'];
@@ -629,15 +639,15 @@ class s3 //extends object
 		}
 		$ipad = (substr($key, 0, 64) ^ str_repeat(chr(0x36), 64));
 		$opad = (substr($key, 0, 64) ^ str_repeat(chr(0x5C), 64));
-		
+
 		return sha1($opad . pack("H40", sha1($ipad . $data)));
 	}
 
 	public function base64($str)
 	{
 		//return base64_encode(urlencode($str));
-		
-		
+
+
 		$ret = "";
 		for($i = 0; $i < strlen($str); $i += 2)
 		{
