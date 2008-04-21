4310fdbc36131dbdd4da3564b121af23c85ef0f1
joconnor <joconnor@gmail.com>
2008-04-21 18:24:32 +0000

    Changed template to use framework methods for HTML output.

--- app/core_modules/modulecatalogue/templates/content/languages_tpl.php
+++ app/core_modules/modulecatalogue/templates/content/languages_tpl.php
@@ -1,4 +1,12 @@
-<form method="POST" action="<?= $this->uri(array('action'=>'downloadpo')) ?>">
-Language: <input name="langname" type="text">
-<input type="submit" value="Submit">
-</form>
+<?php
+$this->loadClass('form','htmlelements');
+$this->loadClass('textinput','htmlelements');
+$this->loadClass('button','htmlelements');
+//
+$form = new form('language',$this->uri(array('action'=>'downloadpo')));
+$textinput = new textinput('langname','');
+$submit = new button('submit',$this->objLanguage->languageText('word_submit'));
+$submit->setToSubmit();
+$form->addToForm($this->objLanguage->languageText('mod_modulecatalogue_language','modulecatalogue').' : '.$textinput->show());
+$form->addToForm('&nbsp;'.$submit->show());
+echo $form->show();
