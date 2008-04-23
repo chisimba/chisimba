8174fc3624e6d5e3e0cd1b2a31c536d26c94fe97
joconnor <joconnor@gmail.com>
2008-04-23 18:16:57 +0000

    Made language a dropdown.

--- app/core_modules/modulecatalogue/templates/content/languages_tpl.php
+++ app/core_modules/modulecatalogue/templates/content/languages_tpl.php
@@ -1,12 +1,33 @@
 <?php
 $this->loadClass('form','htmlelements');
-$this->loadClass('textinput','htmlelements');
+//$this->loadClass('textinput','htmlelements');
+//$this->loadClass('dropdown','htmlelements');
 $this->loadClass('button','htmlelements');
 //
 $form = new form('language',$this->uri(array('action'=>'downloadpo')));
-$textinput = new textinput('langname','');
+//$textinput = new textinput('langname','');
+$dropdown = $this->newObject('dropdown','htmlelements');
+$dropdown->name = 'langname';
+$langs = $this->objLanguage->languagelist();
+$objLanguageCode = $this->getObject('languagecode','language');
+//echo '<pre>';
+//var_dump($langs);
+//var_dump($objLanguageCode->iso_639_2_tags->codes);
+//echo '</pre>';
+foreach ($langs as /*$key=>*/$lang) {
+    $langName = $lang['languagename'];
+    //$_langName = strtolower($langName);
+    $langCode = 'unknown';
+    foreach ($objLanguageCode->iso_639_2_tags->codes as $ISO=>$language) {
+        if ($langName == $language) {
+            $langCode = $ISO;
+            break;
+        }
+    }
+    $dropdown->addOption($langCode,$langName);
+}
 $submit = new button('submit',$this->objLanguage->languageText('word_submit'));
 $submit->setToSubmit();
-$form->addToForm($this->objLanguage->languageText('mod_modulecatalogue_language','modulecatalogue').' : '.$textinput->show());
+$form->addToForm($this->objLanguage->languageText('mod_modulecatalogue_language','modulecatalogue').' : '.$dropdown->show());
 $form->addToForm('&nbsp;'.$submit->show());
 echo $form->show();
