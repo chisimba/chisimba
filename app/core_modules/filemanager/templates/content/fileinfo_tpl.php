069181c37738bee32c49e3155c83fb75bbe0725a
tohir <tohir@gmail.com>
2008-05-01 19:45:26 +0000

    put extension check for lowercase

--- app/core_modules/filemanager/templates/content/fileinfo_tpl.php
+++ app/core_modules/filemanager/templates/content/fileinfo_tpl.php
@@ -32,7 +32,7 @@ $header->type = 1;
 $header->str = $objFileIcons->getFileIcon($file['filename']).' '.str_replace('_', ' ', htmlentities($file['filename']));
 
 if ($mode == 'selectfilewindow' || $mode == 'selectimagewindow' || $mode == 'fckimage' || $mode == 'fckflash' || $mode == 'fcklink') {
-    if (in_array($file['datatype'], $restrictions)) {
+    if (in_array(strtolower($file['datatype']), $restrictions)) {
         $header->str .= ' (<a href="javascript:selectFile();">'.$this->objLanguage->languageText('mod_filemanager_selectfile', 'filemanager', 'Select File').'</a>) ';
     }
     
