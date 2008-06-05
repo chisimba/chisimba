c3118af828097a0585ad38d2b0e7a01770d99c88
wnitsckie <wnitsckie@gmail.com>
2008-06-05 13:47:48 +0000

    Added the user api to authenticate the user from XUL

---
+++ app/core_modules/api/classes/userapi_class_inc.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * User interface class
+ * 
+ * XML-RPC (Remote Procedure call) class
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  Chisimba
+ * @package   api
+ * @author    Wesley Nitsckie
+ * @copyright 2008
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   
+ * @link      http://avoir.uwc.ac.za
+ * @see       core
+ */
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+
+/**
+ * xulmail 1.0 XML-RPC Class
+ * 
+ * Class to provide forum API 1.0 XML-RPC functionality to Chisimba
+ * 
+ * @category  Chisimba
+ * @package   api
+ * @author    Wesley Nitsckie
+ * @copyright 2008
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   Release: @package_version@
+ * @link      http://avoir.uwc.ac.za
+ * @see       core
+ */
+class userapi extends object
+{
+
+	/**
+	* init method
+    * 
+    * Standard Chisimba init method
+    * 
+    * @return void  
+    * @access public
+    */
+	public function init()
+	{
+		try{
+
+			
+		}
+		catch (customException $e)
+		{
+			customException::cleanUp();
+			exit;
+		}
+	}
+
+	/**
+	* Try to login the user
+	* @param string $username The username 
+	* @param
+	* @access public
+	* @return array
+	*/
+	public function tryLogin($username, $password)
+	{
+
+		try{
+$objAuth = $this->getObject('authenticate', 'security');
+
+			//Authenticate the user
+			$result = $objAuth->authenticateUser($username, $password);
+			
+			//set the session if the the user is authenticated
+			$this->setSession('isauthenticated', $result);
+			
+			$postStruct = new XML_RPC_Value($result, "string");
+		//var_dump($postStruct);    	
+
+  			return new XML_RPC_Response($postStruct);
+		}
+		catch (customException $e)
+		{
+			customException::cleanUp();
+			exit;
+		}
+	}
+
+
+	
+
+}
+
+
+?>
