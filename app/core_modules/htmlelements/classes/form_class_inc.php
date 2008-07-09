6c9a4ced4baf5acf34b50ba649218c64b08027b6
pwando <pwando@gmail.com>
2008-07-09 13:51:11 +0000

    saving changes on htmlelements added form enctype property

--- app/core_modules/htmlelements/classes/form_class_inc.php
+++ app/core_modules/htmlelements/classes/form_class_inc.php
@@ -181,7 +181,7 @@ class form implements ifhtml
      *
      * @param int $encType : the value of the encType i.e 'multipart/form-data'
      */
-    public function setEncType($encType = "multipart/form-data")
+    public function setEncType($encType = "application/x-www-form-urlencoded")
     {
         $this->encType = $encType;
     }
