e511f98e5d633f15f14b066925a0fecce1f9c82d
paulscott <paulscott@gmail.com>
2008-10-24 10:20:27 +0000

    formatted

--- app/core_modules/context/classes/dbpagecontent_class_inc.php
+++ app/core_modules/context/classes/dbpagecontent_class_inc.php
@@ -2,24 +2,24 @@
 
 /**
  * Page content
- * 
+ *
  * Page Content Database abstraction class for Chisimba
- * 
+ *
  * PHP version 3
- * 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @category  Chisimba
  * @package   context
  * @author    Wesley Nitsckie <wnitsckie@uwc.ac.za>
@@ -31,23 +31,22 @@
  */
 /* -------------------- dbTable class ----------------*/
 // security check - must be included in all scripts
-if (!
-/**
+if (! /**
  * Description for $GLOBALS
  * @global string $GLOBALS['kewl_entry_point_run']
  * @name   $kewl_entry_point_run
  */
-$GLOBALS['kewl_entry_point_run']) {
-    die("You cannot view this page directly");
-} 
+$GLOBALS ['kewl_entry_point_run']) {
+    die ( "You cannot view this page directly" );
+}
 // end security check
 
 
 /**
  * Page content
- * 
+ *
  * Page Content Database abstraction class for Chisimba
- * 
+ *
  * @category  Chisimba
  * @package   context
  * @author    Wesley Nitsckie <wnitsckie@uwc.ac.za>
@@ -57,18 +56,17 @@ $GLOBALS['kewl_entry_point_run']) {
  * @link      http://avoir.uwc.ac.za
  * @see       core
  */
- class dbpagecontent extends dbTable{
- 
+class dbpagecontent extends dbTable {
+
     /**
-     * Short description for function
-     * 
-     * Long description (if any) ...
-     * 
-     * @return void  
+     * Standard init
+     *
+     * @return void
      * @access public
      */
-     function init(){
-         parent::init('tbl_context_page_content');
-     }
- }
- ?>
+    function init() {
+        parent::init ( 'tbl_context_page_content' );
+    }
+}
+
+?>
