846644ef6c7515c60bc6b9b77ff69d51598f0d98
paulscott <paulscott@gmail.com>
2008-10-24 06:06:14 +0000

    cleaned up code for QA day

--- app/core_modules/blocks/templates/content/dump_tpl.php
+++ app/core_modules/blocks/templates/content/dump_tpl.php
@@ -1,41 +1,32 @@
 <style type="text/css">
 .blockblock {
-    float: left; margin: 5px;
+	float: left;
+	margin: 5px;
 }
+
 .infoblock {
-    display: none;
+	display: none;
 }
+
 .wideblock {
-    width: 500px;
+	width: 500px;
 }
+
 .smallblock {
-    width: 200px;
+	width: 200px;
 }
 </style>
 <?php
-
-
-if (count($blocks) == 0) {
-    
+if (count ( $blocks ) == 0) {
+    echo '<div class="error">' . $this->objLanguage->languageText ( 'mod_blocks_blocknotfound', 'blocks', 'Block not found!' ) . '</div>';
 } else {
-    foreach ($blocks as $block)
-    {
-        if ($block['blocksize'] == 'wide') {
+    foreach ( $blocks as $block ) {
+        if ($block ['blocksize'] == 'wide') {
             $class = 'wideblock';
         } else {
             $class = 'smallblock';
         }
-        
-        echo '<div class="blockblock '.$class.' ">'.$block['module'].' / '.$block['typeofblock'].'<br />'.$this->objDynamicBlocks->showBlockFromArray($block);
-        
-        /*
-        echo '<a href="javascript:jQuery(\'#'.$block['id'].'\').toggle();">Show/Hide</a><div id="'.$block['id'].'" class="infoblock">';
-        echo '<pre>';
-        print_r($block);
-        echo '</pre>';
-        echo '</div>';
-        */
-        
+        echo '<div class="blockblock ' . $class . ' ">' . $block ['module'] . ' / ' . $block ['typeofblock'] . '<br />' . $this->objDynamicBlocks->showBlockFromArray ( $block );
         echo '</div>';
     }
 }
