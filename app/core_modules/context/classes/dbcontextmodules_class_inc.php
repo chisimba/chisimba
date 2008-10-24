26d2146ced0c26f8e4fed15707095fc4ceb8efa0
paulscott <paulscott@gmail.com>
2008-10-24 10:50:35 +0000

    fixed formatting

--- app/core_modules/context/classes/dbcontextmodules_class_inc.php
+++ app/core_modules/context/classes/dbcontextmodules_class_inc.php
@@ -31,14 +31,13 @@
  */
 /* -------------------- dbTable class ----------------*/
 // security check - must be included in all scripts
-if (!
-/**
+if (! /**
  * Description for $GLOBALS
  * @global string $GLOBALS['kewl_entry_point_run']
  * @name   $kewl_entry_point_run
  */
-$GLOBALS['kewl_entry_point_run']) {
-    die("You cannot view this page directly");
+$GLOBALS ['kewl_entry_point_run']) {
+    die ( "You cannot view this page directly" );
 }
 // end security check
 
@@ -57,123 +56,111 @@ $GLOBALS['kewl_entry_point_run']) {
  * @link      http://avoir.uwc.ac.za
  * @see       core
  */
-class dbcontextmodules extends dbTable
-{
+class dbcontextmodules extends dbTable {
     /**
-    * Constructor
-    */
-     public function init()
-     {
-        parent::init('tbl_contextmodules');
-        $this->_objModule =  $this->newObject('modules', 'modulecatalogue');
+     * Constructor
+     */
+    public function init() {
+        parent::init ( 'tbl_contextmodules' );
+        $this->_objModule = $this->newObject ( 'modules', 'modulecatalogue' );
     }
 
     /**
-    * Method to lookup if a module is visible to the current context
-    * @param      $moduleId    string  The moduleId
-    * @param      $contextCode $string The context Code
-    * @return     $ret         boolean Returns true if an enty was found or false when not found
-    * @access     public
-    * @deprecated
-    */
-    public function isVisible($moduleId,$contextCode)
-    {
-
-        // Rename to use getRecordCount
-
-        $rsArr=$this->getAll("WHERE contextcode = '".$contextCode."' AND moduleid='".$moduleId."'");
-        $ret=true;
-        if ($rsArr){
-            foreach($rsArr as $ar)
-            {
-                $ret=(isset($ar['moduleId']))? true:false;
+     * Method to lookup if a module is visible to the current context
+     *
+     * @param      $moduleId    string  The moduleId
+     * @param      $contextCode $string The context Code
+     * @return     $ret         boolean Returns true if an enty was found or false when not found
+     * @access     public
+     * @deprecated
+     */
+    public function isVisible($moduleId, $contextCode) {
+        $rsArr = $this->getAll ( "WHERE contextcode = '" . $contextCode . "' AND moduleid='" . $moduleId . "'" );
+        $ret = true;
+        if ($rsArr) {
+            foreach ( $rsArr as $ar ) {
+                $ret = (isset ( $ar ['moduleId'] )) ? true : false;
             }
+        } else {
+            $ret = false;
         }
-        else
-        {
-            $ret=false;
-        }
+
         return $ret;
     }
 
     /**
-    * Method to make a
-    * module available to a context
-    * @param string $moduleId The moduleId
-    * @param string $contextCode The Context Code
-    * @return string  The new Id
-    * @access     public
-    * @deprecated
-    */
-    public function setVisible($moduleId,$contextCode){
-        return $this->insert(array(
-                'moduleId' => $moduleId,
-                'contextCode' => $contextCode));
+     * Method to make a
+     * module available to a context
+     *
+     * @param string $moduleId The moduleId
+     * @param string $contextCode The Context Code
+     * @return string  The new Id
+     * @access     public
+     * @deprecated
+     */
+    public function setVisible($moduleId, $contextCode) {
+        return $this->insert ( array ('moduleId' => $moduleId, 'contextCode' => $contextCode ) );
     }
 
     /**
-    * Method to make a module
-    * unavailable to a context
-    * @param      $moduleId    string: The moduleId
-    * @param      $contextCode $string : The context Code
-    * @access     public
-    * @deprecated
-    */
-    public function setHidden($moduleId,$contextCode){
-
+     * Method to make a module
+     *
+     * unavailable to a context
+     *
+     * @param      $moduleId    string: The moduleId
+     * @param      $contextCode $string : The context Code
+     * @access     public
+     * @deprecated
+     */
+    public function setHidden($moduleId, $contextCode) {
 
     }
-     /**
-    * Method to delete all the modules
-    * for the context
-    * @param      $contextCode string : the context code
-    * @access     public
-    * @deprecated
-    */
-    public function deleteModulesForContext($contextCode)
-    {
-        $this->delete('contextCode',$contextCode);
+
+    /**
+     * Method to delete all the modules
+     * for the context
+     *
+     * @param      $contextCode string : the context code
+     * @access     public
+     * @deprecated
+     */
+    public function deleteModulesForContext($contextCode) {
+        $this->delete ( 'contextCode', $contextCode );
     }
 
     /**
      * Method to get a list of context sensitive modules
+     *
      * @return array
      */
-    public function getInstallableModules()
-    {
-
+    public function getInstallableModules() {
 
     }
 
     /**
      * Method to add a module to a context
+     *
      * @param  $contextCode The Context Code
      * @return bool
      * @access public
      */
-    public function addModule($contextCode, $moduleId)
-    {
-        $fields = array('contextcode' => $contextCode,
-                         'moduleid' => $moduleId);
-        return $this->insert($fields);
+    public function addModule($contextCode, $moduleId) {
+        $fields = array ('contextcode' => $contextCode, 'moduleid' => $moduleId );
+        return $this->insert ( $fields );
     }
 
     /**
-     *
      * Method to get a list of modules for a context
+     *
      * @param  contextCode The Context Code
      * @return array
      * @access public
      */
-    public function getContextModules($contextCode)
-    {
-
-        $contextModules = $this->getAll("WHERE contextcode='".$contextCode."'");
-
-        $newArray = array();
-        foreach ($contextModules as $module)
-        {
-            $newArray[] = $module['moduleid'];
+    public function getContextModules($contextCode) {
+        $contextModules = $this->getAll ( "WHERE contextcode='" . $contextCode . "'" );
+        $newArray = array ();
+        foreach ( $contextModules as $module ) {
+            $newArray [] = $module ['moduleid'];
         }
 
         return $newArray;
@@ -183,55 +170,46 @@ class dbcontextmodules extends dbTable
      * Method to save the context modules
      *
      */
-    public function save()
-    {
-    	try{
-	        $contextCode = $this->_objDBContext->getContextCode();
-	        $objModules =  $this->newObject('modules', 'modulecatalogue');
-	        $objModuleFile =  $this->newObject('modulefile', 'modulecatalogue');
-	        $modList = $objModules->getModules(2);
-	        //dump all the modules
-	        $this->delete('contextcode', $contextCode);
+    public function save() {
+        try {
+            $contextCode = $this->_objDBContext->getContextCode ();
+            $objModules = $this->newObject ( 'modules', 'modulecatalogue' );
+            $objModuleFile = $this->newObject ( 'modulefile', 'modulecatalogue' );
+            $modList = $objModules->getModules ( 2 );
+            //dump all the modules
+            $this->delete ( 'contextcode', $contextCode );
 
-	        foreach ($modList as $module)
-	        {
+            foreach ( $modList as $module ) {
 
-	            if($objModuleFile->contextPlugin($module['module_id']))
-	            {//print $module['module_id'];
-	                if($this->getParam('mod_'.$module['module_id']) == $module['module_id'])
-	                {
+                if ($objModuleFile->contextPlugin ( $module ['module_id'] )) { //print $module['module_id'];
+                    if ($this->getParam ( 'mod_' . $module ['module_id'] ) == $module ['module_id']) {
 
-	                    //add to database
-	                    $this->addModule($contextCode, $module['module_id']);
+                        //add to database
+                        $this->addModule ( $contextCode, $module ['module_id'] );
 
+                    }
+                }
 
-	                }
-	            }
+            }
 
-	        }
-
-         }
-        catch (customException $e)
-        {
-        	echo customException::cleanUp($e);
-        	die();
+        } catch ( customException $e ) {
+            echo customException::cleanUp ( $e );
+            die ();
         }
     }
 
     /**
-    * Method to check if a module is registered as a plugin
-    * @param  string  $moduleId
-    * @return boolean
-    * @access public
-    */
-    public function isContextPlugin($contextCode, $moduleId)
-    {
-        $arr = $this->getAll("WHERE contextcode = '".$contextCode."' AND moduleid = '".$moduleId."'");
+     * Method to check if a module is registered as a plugin
+     *
+     * @param  string  $moduleId
+     * @return boolean
+     * @access public
+     */
+    public function isContextPlugin($contextCode, $moduleId) {
+        $arr = $this->getAll ( "WHERE contextcode = '" . $contextCode . "' AND moduleid = '" . $moduleId . "'" );
 
-        if(array_key_exists(0,$arr))
-        {
-            if(count($arr[0]) > 0)
-            {
+        if (array_key_exists ( 0, $arr )) {
+            if (count ( $arr [0] ) > 0) {
 
                 return TRUE;
             } else {
@@ -244,18 +222,17 @@ class dbcontextmodules extends dbTable
 
     /**
      * Method to get the module name
+     *
      * @param  string $moduleId
      * @return string
      * @access public
      *
      */
-    public function getModuleName($moduleId)
-    {
-
-         $modInfo = $this->_objModule->getModuleInfo($moduleId);
-         return  $modInfo['name'];
+    public function getModuleName($moduleId) {
+        $modInfo = $this->_objModule->getModuleInfo ( $moduleId );
+        return $modInfo ['name'];
     }
 
- }
+}
 
 ?>
