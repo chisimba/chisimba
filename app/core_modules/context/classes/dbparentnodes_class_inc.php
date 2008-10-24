1bc8b5f540d993017d8cd1788060acfbc102573d
paulscott <paulscott@gmail.com>
2008-10-24 10:19:25 +0000

    fixed up

--- app/core_modules/context/classes/dbparentnodes_class_inc.php
+++ app/core_modules/context/classes/dbparentnodes_class_inc.php
@@ -2,24 +2,24 @@
 
 /**
  * Parent Nodes
- * 
+ *
  * Parent Nodes database abstraction for context
- * 
- * PHP version 3
- * 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * PHP version 5
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @category  Chisimba
  * @package   context
  * @author    Wesley Nitsckie <wnitsckie@uwc.ac.za>
@@ -31,23 +31,22 @@
  */
 /* -------------------- dbTable class ----------------*/
 // security check - must be included in all scripts
-if (!
-/**
+if (! /**
  * Description for $GLOBALS
  * @global string $GLOBALS['kewl_entry_point_run']
  * @name   $kewl_entry_point_run
  */
-$GLOBALS['kewl_entry_point_run']) {
-    die("You cannot view this page directly");
-} 
+$GLOBALS ['kewl_entry_point_run']) {
+    die ( "You cannot view this page directly" );
+}
 // end security check
 
 
 /**
  * Parent Nodes
- * 
+ *
  * Parent Nodes database abstraction for context
- * 
+ *
  * @category  Chisimba
  * @package   context
  * @author    Wesley Nitsckie <wnitsckie@uwc.ac.za>
@@ -57,72 +56,69 @@ $GLOBALS['kewl_entry_point_run']) {
  * @link      http://avoir.uwc.ac.za
  * @see       core
  */
- class dbparentnodes extends dbTable{
- 
-	 /**
-	 * @var object objDBContext;
-	 */
-	 var $objDBContext;
-     
-     /**
-	 * @var object objUser;
-	 */
-	 var $objUser;
-     
-     
+class dbparentnodes extends dbTable {
+
     /**
-     * Short description for function
-     * 
-     * Long description (if any) ...
-     * 
-     * @return void  
+     * Context object
+     *
+     * @var object objDBContext;
+     */
+    public $objDBContext;
+
+    /**
+     * User Object
+     *
+     * @var object objUser;
+     */
+    public $objUser;
+
+    /**
+     * Standard init function
+     *
+     * @return void
      * @access public
      */
-     function init(){
-         parent::init('tbl_context_parentnodes');
-		 $this->objDBContext =  $this->newObject('dbcontext', 'context');
-         $this->objUser =  $this->newObject('user', 'security');
-     }
-     
-	  /**
-	 * Method to get the parentNode
-	 * @param string $contextCode The Context Code
-	 * @return string
-	 */
-	 function getParentNodeId($contextCode=NULL){
-		 if(!isset($contextCode)){
-			 $contextCode = $this->objDBContext->getContextCode();
-		 }
-		 
-		 $row = $this->getRow('tbl_context_parentnodes_has_tbl_context_tbl_context_contextCode', $contextCode);
-		 return $row['id'];
-	 }
-     
-     /**
+    public function init() {
+        parent::init ( 'tbl_context_parentnodes' );
+        $this->objDBContext = $this->newObject ( 'dbcontext', 'context' );
+        $this->objUser = $this->newObject ( 'user', 'security' );
+    }
+
+    /**
+     * Method to get the parentNode
+     *
+     * @param string $contextCode The Context Code
+     * @return string
+     */
+    public function getParentNodeId($contextCode = NULL) {
+        if (! isset ( $contextCode )) {
+            $contextCode = $this->objDBContext->getContextCode ();
+        }
+        $row = $this->getRow ( 'tbl_context_parentnodes_has_tbl_context_tbl_context_contextCode', $contextCode );
+
+        return $row ['id'];
+    }
+
+    /**
      * Method add a entry to the database
+     *
      * @param string $contextId The context ID
      */
-     function createEntry($contextId, $contextCode, $title = NULL){
-       if(!$this->valueExists('tbl_context_parentnodes_has_tbl_context_tbl_context_contextCode',$contextCode)){                                              
-            $rootId=$this->insert(array(
-                'tbl_context_parentnodes_has_tbl_context_tbl_context_id' => $contextId,
-                'tbl_context_parentnodes_has_tbl_context_tbl_context_contextCode' => $contextCode,
-                'title' => $title,
-                'datemodified' => $this->getDate(),
-                'dateCreated' => $this->getDate(),
-                'userId' =>$this->objUser->userId(),
-                'menu_text' => $title));
-            
-        }  
-     
-     }
-     
-     /**
-    *Method to return a formatted date string
-    */
-    function getDate(){
-        return date("Y-m-d H:i:s");
+    public function createEntry($contextId, $contextCode, $title = NULL) {
+        if (! $this->valueExists ( 'tbl_context_parentnodes_has_tbl_context_tbl_context_contextCode', $contextCode )) {
+            $rootId = $this->insert ( array ('tbl_context_parentnodes_has_tbl_context_tbl_context_id' => $contextId, 'tbl_context_parentnodes_has_tbl_context_tbl_context_contextCode' => $contextCode, 'title' => $title, 'datemodified' => $this->getDate (), 'dateCreated' => $this->getDate (), 'userId' => $this->objUser->userId (), 'menu_text' => $title ) );
+
+        }
+
     }
-    
- }
- ?>
+
+    /**
+     * Method to return a formatted date string
+     */
+    public function getDate() {
+        return date ( "Y-m-d H:i:s" );
+    }
+
+}
+
+?>
