4f8c62ab9ee7e4f8e884d80f08f01b30b8ae3ccd
paulscott <paulscott@gmail.com>
2008-10-24 07:38:35 +0000

    fixed up module as per qa requirements

--- app/core_modules/config/classes/sysconfig_kewl_default_skin_class_inc.php
+++ app/core_modules/config/classes/sysconfig_kewl_default_skin_class_inc.php
@@ -2,24 +2,24 @@
 
 /**
  * Skin sysconfig
- * 
+ *
  * Skin system configuration
- * 
+ *
  * PHP version 5
- * 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @category  Chisimba
  * @package   config
  * @author    Paul Scott <pscott@uwc.ac.za>
@@ -30,12 +30,11 @@
  * @see       core
  */
 
-
 /**
  * sysconfig for skins
- * 
+ *
  * Chisimba skin system configuration manipulation class
- * 
+ *
  * @category  Chisimba
  * @package   config
  * @author    Paul Scott <pscott@uwc.ac.za>
@@ -45,62 +44,60 @@
  * @link      http://avoir.uwc.ac.za
  * @see       core
  */
-class sysconfig_kewl_default_skin extends object
-{
+class sysconfig_kewl_default_skin extends object {
 
     /**
-    * Standard Constructor
-    */
-    public function init()
-    { }
-    
+     * Standard Constructor
+     */
+    public function init() {
+
+    }
+
     /**
-    * Method to set the current default value
-    */
-    public function setDefaultValue($value)
-    {
+     * Method to set the current default value
+     *
+     */
+    public function setDefaultValue($value) {
         $this->defaultVaule = $value;
     }
-    
+
     /**
-    * Method to display the sysconfig interface
-    */
-    public function show()
-    {
+     * Method to display the sysconfig interface
+     *
+     */
+    public function show() {
         // Load the Radio button class
-        $this->loadClass('radio', 'htmlelements');
-        
+        $this->loadClass ( 'radio', 'htmlelements' );
+
         // Load the Skin Object
-        $objSkin = $this->getObject('skin', 'skin');
-        
-        $skinsList = $objSkin->getListofSkins();
-        
+        $objSkin = $this->getObject ( 'skin', 'skin' );
+
+        $skinsList = $objSkin->getListofSkins ();
+
         // Input MUST be called 'pvalue'
-        $objElement = new radio ('pvalue');
-        
-        foreach ($skinsList as $element=> $value) {
-           $objElement->addOption($element, $value);
+        $objElement = new radio ( 'pvalue' );
+
+        foreach ( $skinsList as $element => $value ) {
+            $objElement->addOption ( $element, $value );
         }
-        
+
         // Set Default Selected
-        $objElement->setSelected($this->defaultVaule);
-        
+        $objElement->setSelected ( $this->defaultVaule );
+
         // Set radio buttons to be one per line
-        $objElement->setBreakSpace('<br />');
-        
+        $objElement->setBreakSpace ( '<br />' );
+
         // return finished radio button
-        return $objElement->show();
+        return $objElement->show ();
     }
-    
+
     /**
-    * Method to run actions that need to occur once the parameter is updated
-    */
-    public function postUpdateActions()
-    {
+     * Method to run actions that need to occur once the parameter is updated
+     *
+     */
+    public function postUpdateActions() {
         return NULL;
     }
-    
-    
 }
 
 ?>
