7ba6771b6e25167b5e0d52382b1e1cda96977321
charlvn <charlvn@gmail.com>
2008-10-25 16:51:06 +0000

    Core: Removed CVS from @version identifiers

--- app/classes/core/access_class_inc.php
+++ app/classes/core/access_class_inc.php
@@ -19,7 +19,7 @@
  * Free Software Foundation, Inc., 
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  * 
- * @version    CVS: $Id$
+ * @version    $Id$
  * @package    core
  * @subpackage access
  * @author     Paul Scott <pscott@uwc.ac.za>
