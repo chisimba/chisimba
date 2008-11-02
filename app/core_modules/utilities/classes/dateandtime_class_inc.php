ebe2eb84d722f1e31c057cc43734bad7951f06d0
charlvn <charlvn@gmail.com>
2008-11-02 11:38:44 +0000

    Utilities Module: dateandtime::isLeapYear is supposed to return a boolean, not a string.

--- app/core_modules/utilities/classes/dateandtime_class_inc.php
+++ app/core_modules/utilities/classes/dateandtime_class_inc.php
@@ -344,9 +344,9 @@ class dateandtime extends object
     public function isLeapYear($year)
     {
         if (($year % 4 == 0) && (($year % 100 != 0) || ($year % 400 == 0))) {
-            return "true";
+            return TRUE;
         } else {
-            return "false";
+            return FALSE;
         }
     }
 
