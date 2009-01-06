92018c5be04ca854ad7f5520eb19735433211208
paulscott <paulscott@gmail.com>
2009-01-06 10:02:04 +0000

    added basic LU based auth system and incremneted version numbers

--- app/core_modules/security/classes/auth_database_class_inc.php
+++ app/core_modules/security/classes/auth_database_class_inc.php
@@ -2,9 +2,9 @@
 /* -------------------- IFAUTH INTERFACE CLASS ----------------*/
 
 /**
-* 
+*
 * Plugin authenticatoin class to authenticate a user via a database
-* 
+*
 * @author Derek Keats, James Scoble
 * @category Chisimba
 * @package security
@@ -19,25 +19,27 @@ $this->loadClass("ifauth", "security");
 class auth_database extends abauth implements ifauth
 {
     /**
-    * 
+    *
     * Init method. It sets up a connection to the users database table
     * and instantiates required objects.
-    *  
+    *
     */
-    public function init() 
+    public function init()
     {
         parent::init('tbl_users');
     }
-    
+
     /**
-    * 
+    *
     * Method to authenticate the user via the database
     * @param string $username The username supplied in the login
     * @param string $password The password supplied in the login
-    * @return TRUE|FALSE Boolean indication of success of login 
+    * @return TRUE|FALSE Boolean indication of success of login
     */
     public function authenticate($username, $password)
     {
+        $auth = $this->objLu->login($username, $password);
+
         //Retrieve the users data from the database
         $line=$this->getUserDataAsArray($username);
         if ($line) {
@@ -51,7 +53,8 @@ class auth_database extends abauth implements ifauth
             } else {
                 $password=sha1(trim($password));
                 // if the login was successful
-                if ( strtolower($line['pass'])==strtolower($password) ) {
+                if($this->objLu->isloggedIn() == TRUE) {
+                //if ( strtolower($line['pass'])==strtolower($password) ) {
                     $this->_record = $line;
                     return TRUE;
                 }
@@ -66,6 +69,45 @@ class auth_database extends abauth implements ifauth
     * @return array on success, FALSE on failure.
     */
     public function getUserDataAsArray($username)
+    {
+        //echo $this->objLu->getProperty('handle'); die();
+        //var_dump($this->objLu); die();
+        /*$array = array();
+        $array['username'] = $this->objLu->getProperty('handle');
+        $array['userid'] = $this->objLu->getProperty('auth_user_id');
+        $array['isactive'] = $this->objLu->getProperty('is_active');
+        $array['emailaddress'] = $this->objLu->getProperty('email');
+        $array['sex'] = $this->objLu->getProperty('sex');
+
+        var_dump($array); die();*/
+
+        $sql="SELECT
+            tbl_users.username,
+            tbl_users.userid,
+            tbl_users.title,
+            tbl_users.firstname,
+            tbl_users.surname,
+            tbl_users.pass,
+            tbl_users.creationdate,
+            tbl_users.emailaddress,
+            tbl_users.logins,
+            tbl_users.isactive,
+            tbl_users.accesslevel
+        FROM
+            tbl_users
+        WHERE
+            (username = '".addslashes($username)."')";
+        $array=$this->getArray($sql);
+        //var_dump($array[0]); die();
+        if (!empty($array))
+        {
+            return $array[0];
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function getUserDataAsArray2($username)
     {
         $sql="SELECT
             tbl_users.username,
@@ -91,6 +133,6 @@ class auth_database extends abauth implements ifauth
             return FALSE;
         }
     }
-    
+
 }
 ?>
--- app/core_modules/security/classes/block_login_class_inc.php
+++ app/core_modules/security/classes/block_login_class_inc.php
@@ -1,30 +1,30 @@
 <?php
  /**
  * block_login class for Chisimba
- * 
+ *
  * A block class to produce a login box in a block.
- * 
+ *
  * PHP version 5
- *  
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
- * 
+ *
+ *
  * @category  Chisimba
  * @package   files
  * @author    Derek Keats <dkeats@uwc.ac.za>
  * @copyright 2004-2007, University of the Western Cape & AVOIR Project
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   $Id$
  * @link      http://avoir.uwc.ac.za
  */
@@ -37,11 +37,11 @@ if (!$GLOBALS['kewl_entry_point_run'])
 // end security check
 
 /**
-* 
+*
 * A block class to produce a login box in a block
 *
 * @author Derek Keats
-* 
+*
 * $Id$
 *
 */
@@ -51,7 +51,7 @@ class block_login extends object
     * @var string $title The title of the block
     */
     public $title;
-    
+
     /**
     * @var string object $objLanguage String to hold the language object
     */
@@ -80,7 +80,7 @@ class block_login extends object
             customException::cleanUp();
         }
     }
-    
+
     /**
     * Standard block show method. It uses the renderform
     * class to render the login box
@@ -92,7 +92,7 @@ class block_login extends object
                 return NULL;
             } else {
                 $objLogin =  $this->getObject('logininterface', 'security');
-                return $objLogin->renderLoginBox();                
+                return $objLogin->renderLoginBox();
             }
         } catch (customException $e) {
             customException::cleanUp();
