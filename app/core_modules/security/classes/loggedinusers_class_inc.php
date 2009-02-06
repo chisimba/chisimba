3e969d80e4e8446579e75b1eb6ccf0feaa7b1c04
paulscott <paulscott@gmail.com>
2009-02-06 07:11:44 +0000

    fixed up loggedinusers

--- app/core_modules/security/classes/loggedinusers_class_inc.php
+++ app/core_modules/security/classes/loggedinusers_class_inc.php
@@ -1,30 +1,30 @@
 <?php
  /**
  * Loggedinusers class
- * 
+ *
  * This class keeps track of which users are logged into the system at any given time.
- * 
+ *
  * PHP version 5
- *  
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
- * 
+ *
+ *
  * @category  Chisimba
  * @package   security
  * @author AVOIR
  * @copyright 2004-2007, University of the Western Cape & AVOIR Project
- * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   $Id$
  * @link      http://avoir.uwc.ac.za
  */
@@ -82,13 +82,13 @@ class loggedInUsers extends dbTable
             )
             VALUES (
                                 '".date('YmdHis')."',
-                '$userId', 
-                '$ipAddress', 
-                '$sessionId', 
-                CURRENT_TIMESTAMP, 
-                    CURRENT_TIMESTAMP, 
-                '$isInvisible', 
-                '$contextCode', 
+                '$userId',
+                '$ipAddress',
+                '$sessionId',
+                CURRENT_TIMESTAMP,
+                    CURRENT_TIMESTAMP,
+                '$isInvisible',
+                '$contextCode',
                 '$theme'
             )";
         $this->query($sql);
@@ -116,9 +116,9 @@ class loggedInUsers extends dbTable
     public function doUpdateLogin($userId,$contextCode='lobby')
     {
            $sql="UPDATE tbl_loggedinusers
-        SET 
-              whenlastactive = CURRENT_TIMESTAMP, 
-            coursecode='$contextCode'  
+        SET
+              whenlastactive = CURRENT_TIMESTAMP,
+            coursecode='$contextCode'
         WHERE
               userid='$userId'
             AND sessionid ='".session_id()."'
@@ -190,9 +190,9 @@ class loggedInUsers extends dbTable
     * Method to clear inactive users
     */
     public function clearInactive()
-    {        
-        $sql="DELETE FROM tbl_loggedinusers 
-        WHERE 
+    {
+        $sql="DELETE FROM tbl_loggedinusers
+        WHERE
             ((CURRENT_TIMESTAMP-whenlastactive)/100) > '{$this->systemTimeOut}'
         ";
         $this->query($sql);
@@ -203,7 +203,7 @@ class loggedInUsers extends dbTable
     * @param string $userId
     * returns TRUE|FALSE
     */
-    public function isUserOnline($userId)
+    public function isUserOnline($userid)
     {
         $sql="SELECT COUNT(userid) AS count FROM tbl_loggedinusers WHERE userid='$userid'";
           $results = $this->getArray($sql);
@@ -216,7 +216,7 @@ class loggedInUsers extends dbTable
             return false;
         }
     }
-    
+
     /**
     * Method to get a list of logged in users
     * @param string $order Order Clause
@@ -225,7 +225,7 @@ class loggedInUsers extends dbTable
     function getListOnlineUsers($order = 'WhenLastActive DESC')
     {
         $sql = 'SELECT DISTINCT tbl_users.userId, username, firstName, surname FROM tbl_loggedinusers INNER JOIN tbl_users ON (tbl_loggedinusers.userId = tbl_users.userId) ORDER BY '.$order;
-        
+
         return $this->getArray($sql);
     }
 
