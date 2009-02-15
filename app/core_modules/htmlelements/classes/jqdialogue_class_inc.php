538437237523e43a2912df577e7f547f929ecc20
charlvn <charlvn@gmail.com>
2009-02-15 23:07:27 +0000

    Htmlelements Module: Added security check to jqdialogue class.

--- app/core_modules/htmlelements/classes/jqdialogue_class_inc.php
+++ app/core_modules/htmlelements/classes/jqdialogue_class_inc.php
@@ -29,6 +29,18 @@
  * @link      http://avoir.uwc.ac.za/
  * @see       http://docs.jquery.com/UI/Dialog
  */
+
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+
 class jqdialogue extends object
 {
     /**
