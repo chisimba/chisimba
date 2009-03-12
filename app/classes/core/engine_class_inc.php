bb85acb02004278000d6b4b93eb81113b086ac2f
paulscott <paulscott@gmail.com>
2009-03-12 10:04:28 +0000

    incremented version to reflect RC status

--- app/classes/core/engine_class_inc.php
+++ app/classes/core/engine_class_inc.php
@@ -134,7 +134,7 @@ class engine {
      * Version Number of the software. (engine)
      *
      */
-    public $version = '3.0.0-beta2';
+    public $version = '3.0.0RC1';
 
     /**
      * Template variable
