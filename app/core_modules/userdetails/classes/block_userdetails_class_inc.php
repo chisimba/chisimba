d529624781211f7a97a4a374acd1896d0c227d4b
paulscott <paulscott@gmail.com>
2009-03-19 07:36:11 +0000

    "mods for chisimba 3"

--- app/core_modules/userdetails/classes/block_userdetails_class_inc.php
+++ app/core_modules/userdetails/classes/block_userdetails_class_inc.php
@@ -11,26 +11,26 @@ class block_userdetails extends object
         $this->objLanguage = $this->getObject('language', 'language');
         $this->title = $this->objLanguage->languageText('mod_userdetails_name', 'userdetails', 'My Profile');
         $this->loadClass('link', 'htmlelements');
-        
+
         $this->objUser = $this->getObject('user', 'security');
         $this->objUserAdmin = $this->getObject('useradmin_model2','security');
     }
-   
+
     /**
      * Method to render the block
      */
     public function show()
     {
         $user = $this->objUserAdmin->getUserDetails($this->objUser->PKId($this->objUser->userId()));
-        
+
         $objBizCard = $this->getObject('userbizcard', 'useradmin');
         $objBizCard->setUserArray($user);
         $objBizCard->showResetImage = FALSE;
         $objBizCard->resetModule = 'userdetails';
-        
+
         $link = new link($this->uri(NULL, 'userdetails'));
         $link->link = $this->objLanguage->languageText('mod_userdetails_updateyourprofile', 'userdetails', 'Update Your Profile');
-        
+
         return $objBizCard->show().'<p>'.$link->show().'</p>';
     }
 }
