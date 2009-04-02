46ea7dfbd6fb4b0c93837e57c7551d758ef86b42
paulscott <paulscott@gmail.com>
2009-04-02 08:50:59 +0000

    "added func"

--- app/core_modules/tagging/classes/dbtags_class_inc.php
+++ app/core_modules/tagging/classes/dbtags_class_inc.php
@@ -211,6 +211,12 @@ class dbtags extends dbTable
         return $this->getArray($sql);
     }
 
+    public function getHashTagsByModule($module) {
+        $sql = "SELECT DISTINCT meta_value FROM tbl_tags WHERE module='$module' and meta_key='hashtag'";
+
+        return $this->getArray($sql);
+    }
+
     public function getAllPostsByTag($tag, $module = NULL) {
         return $this->getAll("WHERE meta_value = '$tag' and module='$module'");
     }
