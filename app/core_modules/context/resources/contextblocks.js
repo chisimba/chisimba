13e11a1714c33f6b0043c5af3ec853dca7d499be
davidwaf <davidwaf@gmail.com>
2009-05-25 08:57:06 +0000

    fixed the bug of not listing private courses..again!

--- app/core_modules/context/resources/contextblocks.js
+++ app/core_modules/context/resources/contextblocks.js
@@ -1,25 +1,26 @@
 
     // Flag Variable - Update message or not
     var doUpdateMessage = false;
-    
-    
+
+
     var leftBlock = false;
     var rightBlock = false;
     var middleBlock = false;
-    
-    
+
+
     var inEditMode = false;
-    
+
     // Var Current Entered Code
     var currentCode;
-    
+
     // Action to be taken once page has loaded
     jQuery(document).ready(function(){
-        
+
+
         jQuery(".block").prepend('<div class="blockoptions"><a class="moveup" href="javascript:;">'+upIcon+
-            '</a> <a class="movedown" href="javascript:;">'+downIcon+'</a>'+
+            '</a><a class="movedown" href="javascript:;">'+downIcon+'</a>'+
             '<a class="deleteblock" href="javascript:;">'+deleteIcon+'</a> </div>');
-        
+
         if (inEditMode) {
             jQuery("#leftaddblock").show();
             jQuery("#rightaddblock").show();
@@ -31,58 +32,58 @@
             jQuery("#middleaddblock").hide();
             jQuery(".blockoptions").hide();
         }
-        
-        
-        setUpSide('left');
+
+
+       setUpSide('left');
         setUpSide('right');
-        setUpSide('middle');
-        
-        
-        
-        
+       setUpSide('middle');
+
+
+
+
         jQuery(".moveup").livequery('click', function() {
-            
+
             moveBlock(jQuery(this).parent().parent().attr('id'), 'up');
         });
-        
+
         jQuery(".movedown").livequery('click', function() {
             moveBlock(jQuery(this).parent().parent().attr('id'), 'down');
         });
-        
-        
+
+
         jQuery(".deleteblock").livequery('click', function() {
             if (confirm(deleteConfirm)) {
                 removeBlock(jQuery(this).parent().parent().attr('id'));
             }
         });
-        
-        
-        
-        
-        
+
+
+
+
+
         adjustLayout();
-        
-        
+
+
     });
-    
+
     function setUpSide(side)
     {
         jQuery("#dd"+side+"blocks").bind('change', function() {
             getPreview(jQuery("#dd"+side+"blocks").attr('value'), side);
         });
-        
-        jQuery("#"+side+"button").hide();
+
+       // jQuery("#"+side+"button").hide();
         jQuery("#"+side+"button").bind('click', function() {
             addBlock(window[side+'Block'], side)
         });
-        
+
         jQuery("#"+side+"blocks > :first-child").livequery(function() {
             jQuery("#"+side+"blocks .moveup").show();
             jQuery("#"+side+"blocks .movedown").show();
             jQuery("#"+side+"blocks > :first-child a.moveup").hide();
             jQuery("#"+side+"blocks > :last-child a.movedown").hide();
         });
-        
+
         jQuery("#"+side+"blocks > :last-child").livequery(function() {
             jQuery("#"+side+"blocks .moveup").show();
             jQuery("#"+side+"blocks .movedown").show();
@@ -91,145 +92,146 @@
         });
     }
 
-    
+
     function moveBlock(blockId, direction)
     {
         jQuery.ajax({
-            type: "GET", 
-            url: "index.php", 
+            type: "GET",
+            url: "index.php",
             data: "module="+theModule+"&action=moveblock&blockid="+blockId+'&direction='+direction,
             success: function(msg){
-            
+
                 if (msg == 'ok') {
                     if (direction == 'up') {
-                        
+
                         var div = jQuery('#'+blockId).insertBefore(jQuery('#'+blockId).prev());
                     } else {
                         var div = jQuery('#'+blockId).insertAfter(jQuery('#'+blockId).next());
                     }
-                    
+
 
                 } else {
                     alert(unableMoveBlock);
                 }
-                
+
                 adjustLayout();
             }
         });
-        
-        
+
+
     }
-    
+
     function removeBlock(blockId)
     {
         jQuery.ajax({
-            type: "GET", 
-            url: "index.php", 
+            type: "GET",
+            url: "index.php",
             data: "module="+theModule+"&action=removeblock&blockid="+blockId,
             success: function(msg){
-            
+
                 if (msg == 'ok') {
                     jQuery('#'+blockId).remove();
                 } else {
                     alert(unableDeleteBlock);
                 }
-                
+
                 adjustLayout();
             }
         });
     }
-    
+
     function addBlock(blockid, side)
     {
+
         // DO Ajax
         jQuery.ajax({
-            type: "GET", 
-            url: "index.php", 
-            data: "module="+theModule+"&action=addblock&blockid="+blockid+"&side="+side, 
+            type: "GET",
+            url: "index.php",
+            data: "module="+theModule+"&action=addblock&blockid="+blockid+"&side="+side,
             success: function(msg){
-            
+
                 if (msg == '') {
                     alert(unableAddBlock);
                 } else {
                     jQuery("#"+side+"previewcontent .block").attr('id', msg);
-                    
+
                     // First Add Up/Down/Delete
                     jQuery("#"+side+"previewcontent .block").prepend('<div class="blockoptions"><a class="moveup" href="javascript:;">'+upIcon+
             '</a> <a class="movedown" href="javascript:;">'+downIcon+'</a>'+
             '<a class="deleteblock" href="javascript:;">'+deleteIcon+'</a> </div>');
-                    
+
                     // Then Attach
                     jQuery("#"+side+"previewcontent .block").appendTo("#"+side+"blocks");
                     jQuery("#"+side+"button").hide();
-                    
+
 
                 }
-                
+
                 adjustLayout();
             }
         });
     }
-    
+
     function getPreview(blockid, side)
     {
         jQuery("#"+side+"button").hide();
         adjustLayout();
-            
+
         if (blockid=="") {
             jQuery("#"+side+"previewcontent").hide();
             jQuery("#"+side+"button").hide();
             adjustLayout();
         } else {
-        
-            
+
+
             // DO Ajax
             jQuery.ajax({
-                type: "GET", 
-                url: "index.php", 
+                type: "GET",
+                url: "index.php",
                 data: "module="+theModule+"&action=renderblock&blockid="+blockid+"&side="+side,
                 dataType: 'script',
                 success: function(msg){
-                
+
                     jQuery("#"+side+"previewcontent").show();
                     jQuery("#"+side+"previewcontent").html(msg);
-                    
+
                     if (side == 'left') {leftBlock = blockid; }
                     if (side == 'right') {rightBlock = blockid; }
                     if (side == 'middle') {middleBlock = blockid; }
-                    
-                    
+
+
                     if (msg != "") {
                         jQuery("#"+side+"button").show();
                     }
-                    
+
                     adjustLayout();
                 }
             });
-            
-            
+
+
             /*
             // DO Ajax
             jQuery.get("index.php", { module: theModule, action: "renderblock", blockid: blockid, side: side },  function(msg){
-                
+
                     jQuery("#"+side+"previewcontent").show();
                     jQuery("#"+side+"previewcontent").html(msg);
-                    
+
                     if (side == 'left') {leftBlock = blockid; }
                     if (side == 'right') {rightBlock = blockid; }
                     if (side == 'middle') {middleBlock = blockid; }
-                    
-                    
+
+
                     if (msg != "") {
                         jQuery("#"+side+"button").show();
                     }
-                    
+
                     adjustLayout();
                 }
             );*/
         }
-        
+
     }
-    
+
     function switchEditMode()
     {
         if (inEditMode) {
@@ -238,11 +240,8 @@
             jQuery("#middleaddblock").hide();
             jQuery("#editmodeswitchbutton").attr('value', turnEditingOn);
             jQuery(".blockoptions").hide();
-            
-            jQuery(".blockoptions").hide();
-            
             jQuery(".block").removeClass('highlightblock');
-            
+
             inEditMode = false;
         } else {
             jQuery("#leftaddblock").show();
@@ -250,12 +249,10 @@
             jQuery("#middleaddblock").show();
             jQuery("#editmodeswitchbutton").attr('value', turnEditingOff);
             jQuery(".blockoptions").show();
-            
             jQuery(".block").addClass('highlightblock');
-            
             inEditMode = true;
         }
-        
+
         adjustLayout();
     }
 
