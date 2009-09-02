88e5f3e3fa361790d11b35d9fb5970e7bae0af33
Gorthaur <Gorthaur@gmail.com>
2009-09-02 16:54:50 +0000

    Rewrote the smiley filter class to use preg_replace with a callback function instead of str_replace with a large array of replace items.

--- app/core_modules/filters/classes/parse4smileys_class_inc.php
+++ app/core_modules/filters/classes/parse4smileys_class_inc.php
@@ -3,12 +3,7 @@
 /**
  * Class for parsing smilies in a text string
  * 
- * tags such as 
- *   <code>
- *     $a="Hello";
- *     $b="World";
- *     echo $a . " " . $b .".";
- *   </code>
+ * :-) will be replaces with an smiling image
  * 
  * PHP version 5
  * 
@@ -27,7 +22,7 @@
  * 
  * @category  Chisimba
  * @package   filters
- * @author    Derek Keats <dkeats@uwc.ac.za>
+ * @authors   Derek Keats <dkeats@uwc.ac.za>, Dean Wookey <dean@embrace.co.za>
  * @copyright 2007 Derek Keats
  * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
  * @version   $Id$
@@ -47,104 +42,105 @@ $GLOBALS['kewl_entry_point_run'])
 }
 // end security check
 
-/**
- * Class for parsing smilies in a text string
- * tags such as 
- *   <code>
- *     $a="Hello";
- *     $b="World";
- *     echo $a . " " . $b .".";
- *   </code>
- * 
- * @author    Derek Keats
- * @version   $Id$
- * @copyright 2003 GPL
- */
 
 class parse4smileys extends object {
     /**
-     * Method to take a string and return it with smiley
-     * codes replaced by smiley icons
-     * embeded in a layer that is designed for formatting code
-     * 
-     * The array is taken from the Moodle smiley parser, but
-     * I chose to implement it in a different way using preg_repalce
-     * with a regex to avoid doing it inside script blocks. Therefore
-     * I also escaped the smiley part of the array for the test. Note
-     * that this takes places of the preg_replace ability to deal with
-     * arrays. I first make the parseSmiley array just to make it easy
-     * to ensure that the items are in matching pairs, and then use this
-     * array to build the two arrays to be parsed.
-     * 
-     * @param  string $str The string to be parsed
-     * @return the    parsed string
-     *                
-     * @todo   -cparse4smileys Implement do not parse if inside a HTML tag.
-     *         
+     * The array is taken from the Moodle smiley parser. The
+     * idea is to use preg_replace with a callback to increase efficiency.
+     * The regular expression to detect the smileys is created from the 
+     * smiley array. 
      */
-    public function init()
-    {
-        $this->smileyIcons = array(
-        '>-)' => 'alien',
-        'O:-)' => 'angel', 'o:-)' => 'angel',
-        'X-(' => 'angry', 'x-(' => 'angry',
-        '=D>' => 'applause',
-        'b-(' => 'black_eye', 'P-(' => 'black_eye', 'p-(' => 'black_eye',
-        ':-[' => 'bye',
-        ':-p' => 'cheeky', ':-P' => 'cheeky',
-        '~:>' => 'chicken',
-        ':o)' => 'clown', ':O)' => 'clown',
-        ':-/' => 'confused',
-        'B-)' => 'cool',
-        '<):-)>' => 'cowboy',
-        '8-}' => 'crazy',
-        ':-((' => 'cry',
-        '/:-D/' => 'dance_of_joy',
-        '#-o' => 'doh',
-        '=P~' => 'drool', '=p~' => 'drool', '=b~' => 'drool',
-        ':"->' => 'embarrassed',
-        '>:-)' => 'evil',
-        ':-L' => 'frustrated',
-        '>:-D<' => 'hug',
-        ':-D' => 'grin',
-        '@-)' => 'hypnotised',
-        '*-:-)' => 'idea',
-        ':-*' => 'kiss',
-        ':-))' => 'laugh',
-        ':-x' => 'love', ':-X' => 'love',
-        ':-B' => 'nerd',    
-        '[-(' => 'not_talking',
-        '[-o<' => 'praying', '[-O<' => 'praying',
-        '/:-)' => 'raise_eyebrow',
-        '8-|' => 'roll_eyes',
-        '@};-' => 'rose',
-        ':-(' => 'sad',
-        '[-X' => 'shame_on_you', '[-x' => 'shame_on_you',
-        ':-O' => 'shocked', ':-o' => 'shocked',
-        ';;-)' => 'shy',
-        ':-&' => 'sick',
-        '8-X' => 'skull', 'xx-P' => 'skull', '8-x' => 'skull', 'XX-P' => 'skull', 
-        'I-)' => 'sleeping',
-        ':-)>-' => 'victory',
-        ':-)' => 'smile',
-        '(:-|' => 'tired',
-        ':-|' => 'straight_face',
-        ':-?' => 'thinking',
-        ':-"' => 'whistle',
-        ';-)' => 'wink',
-        ':-s' => 'worried', ':-S' => 'worried',
-
-        );
-        // $objIcon = NULL;
+    function init() {
+        $this->objIcon = NULL;
         $this->objIcon = $this->getObject('geticon', 'htmlelements');
+        
+        $this->smileyIcons = array(
+        htmlentities('>-)') => 'alien',
+        htmlentities('O:-)') => 'angel', 
+        htmlentities('o:-)') => 'angel',
+        htmlentities('X-(') => 'angry', 
+        htmlentities('x-(') => 'angry',
+        htmlentities('=D>') => 'applause',
+        htmlentities('b-(') => 'black_eye', 
+        htmlentities('P-(') => 'black_eye', 
+        htmlentities('p-(') => 'black_eye',
+        htmlentities(':-[') => 'bye',
+        htmlentities(':-p') => 'cheeky', 
+        htmlentities(':-P') => 'cheeky',
+        htmlentities('~:>') => 'chicken',
+        htmlentities(':o)') => 'clown', 
+        htmlentities(':O)') => 'clown',
+        htmlentities(':-/') => 'confused',
+        htmlentities('B-)') => 'cool',
+        htmlentities('<):-)>') => 'cowboy',
+        htmlentities('8-}') => 'crazy',
+        htmlentities(':-((') => 'cry',
+        htmlentities('/:-D/') => 'dance_of_joy',
+        htmlentities('#-o') => 'doh',
+        htmlentities('=P~') => 'drool', 
+        htmlentities('=p~') => 'drool', 
+        htmlentities('=b~') => 'drool',
+        htmlentities(':"->') => 'embarrassed',
+        htmlentities('>:-)') => 'evil',
+        htmlentities(':-L') => 'frustrated',
+        htmlentities('>:-D<') => 'hug',
+        htmlentities(':-D') => 'grin',
+        htmlentities('@-)') => 'hypnotised',
+        htmlentities('*-:-)') => 'idea',
+        htmlentities(':-*') => 'kiss',
+        htmlentities(':-))') => 'laugh',
+        htmlentities(':-x') => 'love', 
+        htmlentities(':-X') => 'love',
+        htmlentities(':-B') => 'nerd',    
+        htmlentities('[-(') => 'not_talking',
+        htmlentities('[-o<') => 'praying', 
+        htmlentities('[-O<') => 'praying',
+        htmlentities('/:-)') => 'raise_eyebrow',
+        htmlentities('8-|') => 'roll_eyes',
+        htmlentities('@};-') => 'rose',
+        htmlentities(':-(') => 'sad',
+        htmlentities('[-X') => 'shame_on_you', 
+        htmlentities('[-x') => 'shame_on_you',
+        htmlentities(':-O') => 'shocked', 
+        htmlentities(':-o') => 'shocked',
+        htmlentities(';;-)') => 'shy',
+        htmlentities(':-&') => 'sick',
+        htmlentities('8-X') => 'skull', 
+        htmlentities('xx-P') => 'skull', 
+        htmlentities('8-x') => 'skull', 
+        htmlentities('XX-P') => 'skull', 
+        htmlentities('I-)') => 'sleeping',
+        htmlentities(':-)>-') => 'victory',
+        htmlentities(':-)') => 'smile',
+        htmlentities('(:-|') => 'tired',
+        htmlentities(':-|') => 'straight_face',
+        htmlentities(':-?') => 'thinking',
+        htmlentities(':-"') => 'whistle',
+        htmlentities(';-)') => 'wink',
+        htmlentities(':-s') => 'worried', 
+        htmlentities(':-S') => 'worried',
+        );
+        
     }
-        //$objIcon = &$this->getObject('geticon', 'htmlelements');
-        /* 
-        *  Loop through the array and make the arrays for 
-        *  $test and $replace for the regex. This is done because
-        *  it is otherwise hard to keep track of the smileys 
-        */ 
-    public function parseSmiley($str) {
+    
+    
+    
+    /**Each time a new smiley is added to the list, you
+     * should uncomment 
+     * $regex = $this->createSmileyRegex(); and 
+     * echo str_replace('\\', '\\\\', $regex);
+     * then view a story of page that uses filters, and the regular expression
+     * should print out at the top
+     */
+    function parseSmiley($str)
+    {   
+        $regex = '/(\\&(g(t(\\;(\\-(\\)(|))|\\:(\\-(\\)(|)|D(\\&(l(t(\\;(|))))|))))))|l(t(\\;(\\)(\\:(\\-(\\)(\\&(g(t(\\;(|))))))))))))|O(\\:(\\-(\\)(|))))|o(\\:(\\-(\\)(|))))|X(\\-(\\((|))|X(\\-(P(|))))|x(\\-(\\((|))|x(\\-(P(|))))|\\=(D(\\&(g(t(\\;(|)))))|P(\\~(|))|p(\\~(|))|b(\\~(|)))|b(\\-(\\((|)))|P(\\-(\\((|)))|p(\\-(\\((|)))|\\:(\\-(\\[(|)|p(|)|P(|)|\\/(|)|\\((\\((|)|)|L(|)|D(|)|\\*(|)|\\)(\\)(|)|\\&(g(t(\\;(\\-(|))))|)|)|x(|)|X(|)|B(|)|O(|)|o(|)|\\&(a(m(p(\\;(|))))|q(u(o(t(\\;(|)))))|)|\\|(|)|\\?(|)|s(|)|S(|))|o(\\)(|))|O(\\)(|))|\\&(q(u(o(t(\\;(\\-(\\&(g(t(\\;(|))))))))))))|\\~(\\:(\\&(g(t(\\;(|))))))|B(\\-(\\)(|)))|8(\\-(\\}(|)|\\|(|)|X(|)|x(|)))|\\/(\\:(\\-(D(\\/(|))|\\)(|))))|\\#(\\-(o(|)))|\\@(\\-(\\)(|))|\\}(\\;(\\-(|))))|\\*(\\-(\\:(\\-(\\)(|)))))|\\[(\\-(\\((|)|o(\\&(l(t(\\;(|))))|)|O(\\&(l(t(\\;(|))))|)|X(|)|x(|)))|\\;(\\;(\\-(\\)(|)))|\\-(\\)(|)))|I(\\-(\\)(|)))|\\((\\:(\\-(\\|(|)))))/e';
+        //Uncomment the two following lines to generate a new regular expression
+        //$regex = $this->createSmileyRegex();
+        //echo str_replace('\\', '\\\\', $regex);
+        return preg_replace($regex, "\$this->getSmiley('\\1')", $str);
+        
+        /* Previous Code
         foreach ($this->smileyIcons as $smiley => $image) {
             $test[] = $smiley;
             $this->objIcon->setIcon($image, 'gif', 'icons/smileys/');
@@ -153,24 +149,140 @@ class parse4smileys extends object {
         }
         
         return str_replace($test, $replace, $str);
-
+        */
+        
     } # end of function
     
 
     /**
-     * Short description for function
-     * 
-     * Long description (if any) ...
-     * 
-     * @param  unknown $str Parameter description (if any) ...
-     * @return unknown Return description (if any) ...
-     * @access public 
+     * Method to take a string and return it with smiley
+     * codes replaced by smiley icons
+     * embeded in a layer that is designed for formatting code
+     *
+     * @param  string $str The string to be parsed
+     * @return the    parsed string
+     *                
+     * @todo   -cparse4smileys Implement do not parse if inside a HTML tag.
+     *         
      */
     public function parse($str)
     {
         return $this->parseSmiley($str);
     }
+    
+    
+    
+    /**
+     * Method that takes a smiley (;-)) as a parameter and returns its
+     * associated image in html
+     *
+     * @param string $smiley The smiley to be converted to an image
+     * @return the html string corresponding to the given smiley
+     */
+    public function getSmiley($smiley) {
+      $this->objIcon->setIcon($this->smileyIcons[$smiley], 'gif', 'icons/smileys/');
+      return $this->objIcon->show();
+    }
+    
+    
+    
+    /**
+     * Method that creates a regular expression that matches all
+     * smileys given in the smileyIcons array
+     * @returns a regular expression that matches all given smileys
+     */
+    public function createSmileyRegex() {
+      $regexGraph = $this->constructSmileyRegexGraph();
+      $regex = '/(' . $this->depthFirstRegex($regexGraph) . ')/e';
+      return $regex;
+    }
+    
+    /**
+     * Method that creates a tree from all smileys given
+     * in the smileyIcons array. For example, :-) and :-(
+     * both have the same root :-, but different characters
+     * in the 3rd position. The aim of the tree is to optimise
+     * the regular expression by doing the following:
+     * /:-(\)|\()/
+     * If a smiley such as :-) and another smiley :-)< are
+     * both in the array, :-) wouldn't be recognised. The solution
+     * was to add a value in the tree to let it know if it is ok to
+     * stop at that point in the tree.
+     * A sample graph looks like so:
+     * Array (
+     *   [:] = Array ( 
+     *       [iscomplete] = false,
+     *       [nextlevel]  = Array (
+     *           [)] = Array(
+     *               [iscomplete] = true,
+     *               [nextlevel] = Array()
+     *           )
+     *           [(] = Array(
+     *               [iscomplete] = true,
+     *               [nextlevel] = Array()
+     *           )
+     *       )
+     *   )
+     * )
+     * This will recognise both :) and :(, but not : by
+     * itself.
+     * @returns an array as described above
+     */           
+    function constructSmileyRegexGraph() {
+      $nodes = array();
+      foreach ($this->smileyIcons as $smiley=>$image) {
+        $array = &$nodes;
+        $counter = 0;
+        $smileyChars = str_split($smiley);
+        for ($counter = 0; $counter < strlen($smiley); $counter++) {
+          if (!array_key_exists($smileyChars[$counter], $array)) {
+            $array[$smileyChars[$counter]]['nextlevel'] = array();
+            $array[$smileyChars[$counter]]['iscomplete'] = 0;
+          }
+          if ($counter == (strlen($smiley)-1)) {
+            $array[$smileyChars[$counter]]['iscomplete'] = 1;
+          }
+          $array = &$array[$smileyChars[$counter]]['nextlevel'];
+        }
+      }
+      return $nodes;
+    }
+    
+    
+    
+    /**
+     * A recursive method that traverses a graph and builds a regular
+     * expression as it goes.
+     *
+     * @param array $graph - a tree array as described in
+     * constructSmileyRegexGraph() comments.
+     * @returns a regular expression string
+     */
+    function depthFirstRegex($graph) {
+      $regex = "";
+      $counter = 0;
+      $isend = '';
+      if (count($graph) == 0) {
+        return "";
+      }
+      foreach ($graph as $character=>$value) {
+        if ($graph[$character]['iscomplete'] == 1) {
+          $isend = '|';
+        }
+        if ($counter != 0) {
+          $regex .= '|';
+        }
+        $charRegex = '/^[A-Z0-9a-z]/';
+        $printchr = $character;
+        if (preg_match($charRegex,$character) == 0) {
+          $printchr = '\\' . $character;
+        }
+        $next = $this->depthFirstRegex($graph[$character]['nextlevel']);
+        $regex .= $printchr . '(' . $next . $isend . ')';
+        $counter++;
+      }
+      return $regex . '';
+    }
        
 } # end of class
-
 ?>
