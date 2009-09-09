edf93551ddf0a54d65c5ec83cf6135022689066a
joconnor <joconnor@gmail.com>
2009-09-09 13:59:26 +0000

    Rewrote a part of the script so that all group memberships are preserved, and fixed a bug.

--- app/core_modules/groupadmin/patches/installscripts_class_inc.php
+++ app/core_modules/groupadmin/patches/installscripts_class_inc.php
@@ -9,6 +9,7 @@ class groupadmin_installscripts extends dbTable {
 
     public function preinstall($version = NULL) {
         log_debug("Doing preinstall code");
+    	//echo '<pre>';
         switch ($version) {
             default :
                 $pusers = $this->objGroupOps->getAllUsers();
@@ -43,37 +44,59 @@ class groupadmin_installscripts extends dbTable {
                                     // create the group
                                     log_debug("group doesnt exist, creating $ogsname");
                                     $ongrpid = $this->objGroupModel->addGroup( $ogsname, NULL, null );
-                                    $ongrpid = $this->objGroupModel->getId($ogsname); 
+                                    $ongrpid = $this->objGroupModel->getId($ogsname);
                                     log_debug("Adding subgroups now");
                                     $this->objGroupModel->addSubGroups($ogsname, $ongrpid);
                                 }
                             }
-                        }  
-                        // re-create the users groups
+                        }
+                        // re-create the user's groups
                         $sql = "SELECT group_id from tbl_groupadmin_groupuser where user_id='".$user['id']."'";
                         $oldgroups = $this->getArray($sql);
+            			//var_dump($oldgroups);
                         foreach($oldgroups as $og) {
-                            // get the group name of the id we have from the old group
-                            $ogrpid = $og['group_id'];
-                            $oldname = $this->getArray("SELECT name, parent_id from tbl_groupadmin_group WHERE id = '$ogrpid' AND parent_id IS NULL");
-                            if(!empty($oldname))  {
-                                $oldgrpname = $oldname[0]['name'];
-                                $ngrpid = $this->objGroupModel->getId($oldgrpname."^Students");
-                                // now we add the user to the group
-                                log_debug("adding user with userid $userid to group with id $ngrpid");
-                                // get the users perm user id
-                                $usrdata = $this->objGroupOps->getUserByUserId($userid);
-                                $userid = $usrdata['perm_user_id'];
-                                $this->objGroupModel->addGroupUser( $ngrpid, $userid );
-                            }
+                            $oldGroupPath = $this->getOldGroupPath($og['group_id']);
+                            $ngrpid = $this->objGroupModel->getId($oldGroupPath);
+                            // now we add the user to the group
+                            log_debug("adding user with userid $userid to group with id $ngrpid [$oldGroupPath]");
+                            // get the users perm user id
+                            $usrdata = $this->objGroupOps->getUserByUserId($userid);
+                            $userid_ = $usrdata['perm_user_id'];
+                            $this->objGroupModel->addGroupUser( $ngrpid, $userid_ );
                         }
                     }
                 }
                 log_debug("preinstall code complete");
                 break;
         }
-
+    	//echo '</pre>';
         return 'preinstall done';
+    }
+
+    /**
+    * Recurse through the old group structure.
+    * @param string The old group ID
+    * @return string The old group path
+    */
+    private function getOldGroupPath($groupId)
+    {
+        // get the group name of the id we have from the old group
+        //$ogrpid = $og['group_id'];
+        //echo "*$ogrpid\n";
+        $rs = $this->getArray("SELECT name, parent_id from tbl_groupadmin_group WHERE id = '$groupId'"); // AND parent_id IS NULL
+        //var_dump($oldgroup_rs);
+        if (empty($rs))  {
+            return '';
+        }
+        else {
+            $row = $rs[0];
+            if (NULL == $row['parent_id']) {
+                return $row['name'];
+            }
+            else {
+                return $this->getOldGroupPath($row['parent_id']).'^'.$row['name'];
+            }
+        }
     }
 
     public function postinstall() {
