9140a5eca4116d7212e08f026622d9d6ebfb1c55
davidwaf <davidwaf@gmail.com>
2009-11-02 08:21:40 +0000

    new ckeditor

---
+++ app/core_modules/htmlelements/resources/ckeditor/config.js
@@ -0,0 +1,34 @@
+﻿/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+        config.extraPlugins = 'contexttools';
+        config.toolbar = 'ChisimbaToolbarSet';
+        config.image_browseServer=true;
+      
+      	config.toolbar_ChisimbaToolbarSet =
+	[
+            ['Source','-','Save','NewPage','Preview','-','Templates'],
+            ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+            ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+            ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+            '/',
+            ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+            ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+            ['Link','Unlink','Anchor'],
+            ['Image','Flash','contexttools','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+            '/',
+            ['Styles','Format','Font','FontSize'],
+            ['TextColor','BGColor'],
+            ['Maximize', 'ShowBlocks','-','About']
+      ];
+
+
+};
