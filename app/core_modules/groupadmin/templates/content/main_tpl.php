46295e8834074ef268a07506d799e9f157d6660e
qfenama <qfenama@gmail.com>
2009-11-05 14:39:10 +0000

    Redirected the resources to the new extjs module

--- app/core_modules/groupadmin/templates/content/main_tpl.php
+++ app/core_modules/groupadmin/templates/content/main_tpl.php
@@ -4,7 +4,7 @@
 
 
 	$objSysConfig  = $this->getObject('altconfig','config');
-	$ext .=$this->getJavaScriptFile('ext-3.0-rc2/ext-all-debug.js', 'htmlelements');
+	$ext .=$this->getJavaScriptFile('ext-3.0-rc2/ext-all-debug.js', 'extjs');
 	
 	$ext .=$this->getJavaScriptFile('ColumnNodeUI.js', 'groupadmin');
 	$ext .=$this->getJavaScriptFile('Ext.ux.grid.Search.js', 'groupadmin');
