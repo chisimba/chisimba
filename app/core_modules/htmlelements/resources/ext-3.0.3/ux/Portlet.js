45774e2f8a14fb2ca2486a784e8e1cb794253d5a
davidwaf <davidwaf@gmail.com>
2009-12-15 14:31:05 +0000

    util controls

---
+++ app/core_modules/htmlelements/resources/ext-3.0.3/ux/Portlet.js
@@ -0,0 +1,15 @@
+/*!
+ * Ext JS Library 3.0.3
+ * Copyright(c) 2006-2009 Ext JS, LLC
+ * licensing@extjs.com
+ * http://www.extjs.com/license
+ */
+Ext.ux.Portlet = Ext.extend(Ext.Panel, {
+    anchor : '100%',
+    frame : true,
+    collapsible : true,
+    draggable : true,
+    cls : 'x-portlet'
+});
+
+Ext.reg('portlet', Ext.ux.Portlet);
---
+++ app/core_modules/htmlelements/resources/ext-3.0.3/ux/SlidingPager.js
@@ -0,0 +1,38 @@
+/*!
+ * Ext JS Library 3.0.3
+ * Copyright(c) 2006-2009 Ext JS, LLC
+ * licensing@extjs.com
+ * http://www.extjs.com/license
+ */
+Ext.ux.SlidingPager = Ext.extend(Object, {
+    init : function(pbar){
+        Ext.each(pbar.items.getRange(2,6), function(c){
+            c.hide();
+        });
+        var slider = new Ext.Slider({
+            width: 114,
+            minValue: 1,
+            maxValue: 1,
+            plugins: new Ext.ux.SliderTip({
+                getText : function(s){
+                    return String.format('Page <b>{0}</b> of <b>{1}</b>', s.value, s.maxValue);
+                }
+            }),
+            listeners: {
+                changecomplete: function(s, v){
+                    pbar.changePage(v);
+                }
+            }
+        });
+        pbar.insert(5, slider);
+        pbar.on({
+            change: function(pb, data){
+                slider.maxValue = data.pages;
+                slider.setValue(data.activePage);
+            },
+            beforedestroy: function(){
+                slider.destroy();
+            }
+        });
+    }
+});
