4377d6ea156cb728ff8af22cf297fdb120a41df5
davidwaf <davidwaf@gmail.com>
2009-12-15 16:08:34 +0000

    removed src, not needed

--- app/core_modules/htmlelements/resources/ext-3.0-rc2/src/adapter/ext-base-dom-more.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Ext JS Library 3.0.3
- * Copyright(c) 2006-2009 Ext JS, LLC
- * licensing@extjs.com
- * http://www.extjs.com/license
- */
-Ext.lib.Dom.getRegion = function(el) {
-    return Ext.lib.Region.getRegion(el);
-};
