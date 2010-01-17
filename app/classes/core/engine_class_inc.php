d89da4af43c898a7d47b49b8595eef9ce8b18913
dkeats <dkeats@gmail.com>
2010-01-17 15:34:04 +0000

    Increased version number so it updates remote sites

--- app/classes/core/engine_class_inc.php
+++ app/classes/core/engine_class_inc.php
@@ -134,7 +134,7 @@ class engine {
      * Version Number of the software. (engine)
      *
      */
-    public $version = '3.1.2';
+    public $version = '3.1.3';
 
     /**
      * Template variable
