5728f30f6a15c1476ef6feb9e5c053514137f3bc
davidwaf <davidwaf@gmail.com>
2010-01-17 18:48:39 +0000

    Added DefaultWithoutSave config for the ckeditor, as used in assigments

--- app/core_modules/htmlelements/resources/ckeditor/config.js
+++ app/core_modules/htmlelements/resources/ckeditor/config.js
@@ -56,6 +56,26 @@ CKEDITOR.editorConfig = function( config )
     ];
 
 
+    config.toolbar_DefaultWithoutSave =
+    [
+    ['Source','DocProps','-','NewPage','Preview','-','Templates'],
+    ['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+    ['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
+    '/',
+    ['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+    ['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote'],
+    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+    ['Link','Unlink','Anchor'],
+    ['Image','Flash','Table','Rule','Smiley','SpecialChar','PageBreak'],
+    '/',
+    ['Styles','Format','Font','FontSize'],
+    ['TextColor','BGColor'],
+    ['FitWindow','ShowBlocks','-','About']		// No comma for the last row.
+
+    ];
+
+
     config.toolbar_cms =
     [
     ['Source','-','Save','Preview', 'Templates'],
