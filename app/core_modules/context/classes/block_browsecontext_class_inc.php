cf53003a6b9238994f3db91401902ea6ccea32b0
pwando <pwando@gmail.com>
2010-01-25 20:15:07 +0000

    Used uri() function to return the site root instead of getSiteRoot()

--- app/core_modules/context/classes/block_browsecontext_class_inc.php
+++ app/core_modules/context/classes/block_browsecontext_class_inc.php
@@ -123,7 +123,7 @@ class block_browsecontext extends object
 			var uri = "'.str_replace('&amp;','&',$this->uri(array('module' => 'context', 'action' => 'jsonlistcontext'))).'"; 
         	var usercontexturi = "'.str_replace('&amp;','&',$this->uri(array('module' => 'context', 'action' => 'jsonusercontexts'))).'"; 
 			var othercontexturi = "'.str_replace('&amp;','&',$this->uri(array('module' => 'context', 'action' => 'jsonusercontexts'))).'"; 
-        		var baseuri = "'.$objSysConfig->getsiteRoot().'index.php";
+        		var baseuri = "'.$this->uri().'index.php";
         		var isAdmin = '.$isAdmin.';
         		contextPrivateMessage="'.$this->objLanguage->code2Txt('mod_context_privatecontextexplanation', 'context', NULL, 'This is a closed [-context-] only accessible to members').'"; </script>');
 		
