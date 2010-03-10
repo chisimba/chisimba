07b244ca15f6c378db2af802a82078c8106087f8
charlvn <charlvn@gmail.com>
2010-03-10 12:36:12 +0000

    Added query method.

--- app/core_modules/api/classes/multisearchapi_class_inc.php
+++ app/core_modules/api/classes/multisearchapi_class_inc.php
@@ -8,4 +8,21 @@ class multisearchapi extends object
     {
         $this->objOps = $this->getObject('multisearchops', 'multisearch');
     }
+
+    public function query($params)
+    {
+        $query = $params->getParam(0)->scalarval();
+        $builtQuery = $this->objOps->buildQuery($query);
+        $data = $this->objOps->doQuery($builtQuery);
+        $output = $this->objOps->formatQuery($data);
+        $text = '';
+        foreach ($output as $type => $html) {
+            $text .= sprintf("--- %s ---\n", ucfirst($type));
+            $text .= strip_tags($html);
+        }
+        $value = new XML_RPC_Value($text, 'string');
+        $response = new XML_RPC_Response($value);
+
+        return $response;
+    }
 }
