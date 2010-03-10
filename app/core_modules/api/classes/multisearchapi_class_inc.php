c311abd2e720c0b73a10a86ecc2573e28ce08a75
charlvn <charlvn@gmail.com>
2010-03-10 14:21:22 +0000

    Implode instead of loop.

--- app/core_modules/api/classes/multisearchapi_class_inc.php
+++ app/core_modules/api/classes/multisearchapi_class_inc.php
@@ -15,10 +15,7 @@ class multisearchapi extends object
         $builtQuery = $this->objOps->buildQuery($query);
         $data = $this->objOps->doQuery($builtQuery);
         $output = $this->objOps->formatQuery($data, 'plaintext');
-        $text = '';
-        foreach ($output as $type => $html) {
-            $text .= htmlentities($html);
-        }
+        $text = implode('', $output);
         $value = new XML_RPC_Value($text, 'string');
         $response = new XML_RPC_Response($value);
 
