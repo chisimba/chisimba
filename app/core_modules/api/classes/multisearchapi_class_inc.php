c80b75815600beaba28ff3530b324499f892083d
charlvn <charlvn@gmail.com>
2010-03-10 12:23:50 +0000

    Added objOps property and init method.

--- app/core_modules/api/classes/multisearchapi_class_inc.php
+++ app/core_modules/api/classes/multisearchapi_class_inc.php
@@ -2,4 +2,10 @@
 
 class multisearchapi extends object
 {
+    protected $objOps;
+
+    public function init()
+    {
+        $this->objOps = $this->getObject('multisearchops', 'multisearch');
+    }
 }
