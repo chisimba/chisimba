4b36cee447a95c2e1f17c9d312b5c3d430acb82e
davidwaf <davidwaf@gmail.com>
2010-04-30 22:41:49 +0000

    added htmlspecialchars, to preserve html

--- app/core_modules/htmlelements/classes/htmlarea_class_inc.php
+++ app/core_modules/htmlelements/classes/htmlarea_class_inc.php
@@ -208,7 +208,7 @@ class htmlarea extends object {
 
         $rawvalue=$this->value;
  
-        $this->editor.='<textarea name="'.$this->name.'">'.$rawvalue.'</textarea>';
+        $this->editor.='<textarea name="'.$this->name.'">'.htmlspecialchars($rawvalue).'</textarea>';
         $this->editor.="
         <script type=\"text/javascript\">
         CKEDITOR.replace( '$this->name',
