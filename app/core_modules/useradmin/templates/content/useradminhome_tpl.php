ee0e34b4922681839b1a5aed6a9bb16ef80be912
qfenama <qfenama@gmail.com>
2010-05-26 12:13:16 +0000

    Modified to use uri funtion instead of getSiteRoot

--- app/core_modules/useradmin/templates/content/useradminhome_tpl.php
+++ app/core_modules/useradmin/templates/content/useradminhome_tpl.php
@@ -1,21 +1,24 @@
 <?php
-	$objSysConfig  = $this->getObject('altconfig','config');
-	$objExtJS = $this->getObject('extjs','ext');
-	$objExtJS->show();
+    $objExtJS = $this->getObject('extjs','ext');
+    $objExtJS->show();
 
-	$this->appendArrayVar('headerParams', '
-        	<script type="text/javascript">
-        	var baseuri = "'.$objSysConfig->getsiteRoot().'index.php";
-        	</script>');
+    $fullUri = $this->uri(NULL);
+    $fullUri = explode("?",$fullUri);
+    $siteUri = $fullUri[0];
 
-	$ext =$this->getJavaScriptFile('Ext.data.country.js', 'useradmin');
-	$ext .=$this->getJavaScriptFile('CheckColumn.js', 'useradmin');
-	$ext .=$this->getJavaScriptFile('Ext.ux.grid.Search.js', 'useradmin');
-	$ext .=$this->getJavaScriptFile('edituser.js', 'useradmin');
-	$ext .=$this->getJavaScriptFile('adduser.js', 'useradmin');
-	$ext .=$this->getJavaScriptFile('useradmin.js', 'useradmin');	
-	
+    $this->appendArrayVar('headerParams', '
+        <script type="text/javascript">
+        var baseuri = "'.$siteUri.'";
+        </script>');
+
+    $ext =$this->getJavaScriptFile('Ext.data.country.js', 'useradmin');
+    $ext .=$this->getJavaScriptFile('CheckColumn.js', 'useradmin');
+    $ext .=$this->getJavaScriptFile('Ext.ux.grid.Search.js', 'useradmin');
+    $ext .=$this->getJavaScriptFile('edituser.js', 'useradmin');
+    $ext .=$this->getJavaScriptFile('adduser.js', 'useradmin');
+    $ext .=$this->getJavaScriptFile('useradmin.js', 'useradmin');	
+
     $this->appendArrayVar('headerParams', $ext);
 
-	echo '<div id="user-grid"></div>	</p>';
+    echo '<div id="user-grid"></div>	</p>';
 ?>
