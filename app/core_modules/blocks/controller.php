3c35d69bf74495eca881772dfaa0a31ef3f3ea9a
dkeats <dkeats@gmail.com>
2010-07-01 09:53:47 +0000

    Added config parameter

--- app/core_modules/blocks/controller.php
+++ app/core_modules/blocks/controller.php
@@ -56,6 +56,15 @@ $GLOBALS ['kewl_entry_point_run']) {
 class blocks extends controller {
 
     /**
+    *
+    * @var string $objLanguage String object property for holding the
+    * language object
+    * @access public
+    *
+    */
+    public $objLanguage;
+
+    /**
      *
      * Intialiser for the blocks controller
      * @access public
@@ -63,6 +72,8 @@ class blocks extends controller {
      */
     public function init() {
         $this->objDynamicBlocks = $this->getObject ( 'dynamicblocks' );
+        $this->objBlock = $this->getObject ( 'blocks', 'blocks' );
+        $this->objLanguage = $this->getObject('language', 'language');
     }
 
     /**
@@ -75,10 +86,122 @@ class blocks extends controller {
      *
      */
     public function dispatch() {
-        $blocks = $this->objDynamicBlocks->getAll ();
+        //Get action from query string and set default to view
+        $action=$this->getParam('action', 'getall');
+        /*
+        * Convert the action into a method (alternative to
+        * using case selections)
+        */
+        $method = $this->__getMethod($action);
+        /*
+        * Return the template determined by the method resulting
+        * from action
+        */
+        return $this->$method();
+    }
+
+    public function __getall()
+    {
+        $blocks = $this->objDynamicBlocks->getAll();
         $this->setVarByRef ( 'blocks', $blocks );
         return 'dump_tpl.php';
     }
-}
 
+    public function __getForAjax()
+    {
+        $this->setPageTemplate('page_template.php');
+        
+        $block = "thirdtest";
+        $module = "dynamiccanvas";
+        $str = $this->objBlocks->showBlock($block, $module);
+        $this->setVarByRef( 'str', $str);
+        return 'extblock_tpl.php';
+    }
+
+    /**
+    *
+    * Method to return an error when the action is not a valid
+    * action method
+    *
+    * @access private
+    * @return string The dump template populated with the error message
+    *
+    */
+    private function __actionError()
+    {
+        $this->setVar('str', "<h3>"
+          . $this->objLanguage->languageText("phrase_unrecognizedaction")
+          .": " . $action . "</h3>");
+        return 'dump_tpl.php';
+    }
+
+    /**
+    *
+    * Method to check if a given action is a valid method
+    * of this class preceded by double underscore (__). If it __action
+    * is not a valid method it returns FALSE, if it is a valid method
+    * of this class it returns TRUE.
+    *
+    * @access private
+    * @param string $action The action parameter passed byref
+    * @return boolean TRUE|FALSE
+    *
+    */
+    function __validAction(& $action)
+    {
+        if (method_exists($this, "__".$action)) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+    *
+    * Method to convert the action parameter into the name of
+    * a method of this class.
+    *
+    * @access private
+    * @param string $action The action parameter passed byref
+    * @return stromg the name of the method
+    *
+    */
+    function __getMethod(& $action)
+    {
+        if ($this->__validAction($action)) {
+            return "__" . $action;
+        } else {
+            return "__actionError";
+        }
+    }
+
+    /*------------- END: Set of methods to replace case selection ------------*/
+
+
+
+    /**
+    *
+    * This is a method to determine if the user has to
+    * be logged in or not. Note that this is an example,
+    * and if you use it view will be visible to non-logged in
+    * users. Delete it if you do not want to allow annonymous access.
+    * It overides that in the parent class
+    *
+    * @return boolean TRUE|FALSE
+    *
+    */
+    public function requiresLogin()
+    {
+        $action=$this->getParam('action','NULL');
+        switch ($action)
+        {
+            case 'view':
+                return FALSE;
+                break;
+            default:
+                return TRUE;
+                break;
+        }
+     }
+}
 ?>
