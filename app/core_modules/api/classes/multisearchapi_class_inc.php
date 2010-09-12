462dbccaaa7fd698e5f422a9b0ef2ca01d494004
paulscott <paulscott@gmail.com>
2010-09-12 06:54:17 +0000

    added multisearch check incremented version

--- app/core_modules/api/classes/multisearchapi_class_inc.php
+++ app/core_modules/api/classes/multisearchapi_class_inc.php
@@ -3,22 +3,37 @@
 class multisearchapi extends object
 {
     protected $objOps;
+    protected $dataCapable;
 
     public function init()
     {
-        $this->objOps = $this->getObject('multisearchops', 'multisearch');
+        $this->objModuleCat  = $this->getObject('modules', 'modulecatalogue');
+        if($this->objModuleCat->checkIfRegistered('multisearch'))
+        {
+            $this->dataCapable = TRUE;
+            $this->objOps = $this->getObject('multisearchops', 'multisearch');
+        }
+        else {
+            $this->dataCapable = FALSE;
+        }
     }
 
     public function query($params)
     {
-        $query = $params->getParam(0)->scalarval();
-        $builtQuery = $this->objOps->buildQuery($query);
-        $data = $this->objOps->doQuery($builtQuery);
-        $output = $this->objOps->formatQuery($data, 'plaintext');
-        $text = implode('', $output);
-        $value = new XML_RPC_Value($text, 'string');
-        $response = new XML_RPC_Response($value);
-
-        return $response;
+        if($this->dataCapable == TRUE) {
+            $query = $params->getParam(0)->scalarval();
+            $builtQuery = $this->objOps->buildQuery($query);
+            $data = $this->objOps->doQuery($builtQuery);
+            $output = $this->objOps->formatQuery($data, 'plaintext');
+            $text = implode('', $output);
+            $value = new XML_RPC_Value($text, 'string');
+            $response = new XML_RPC_Response($value);
+            return $response;
+        }
+        else {
+            $ret = "This server is not yet capable of handling multisearch data. Please install the multisearch module!";
+            $val = new XML_RPC_Value($ret, 'string');
+            return new XML_RPC_Response($val);
+        }
     }
 }
