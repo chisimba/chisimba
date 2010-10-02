8c434000a5c451ec62175fcfc41cf876a7023bad
dkeats <dkeats@gmail.com>
2010-10-02 17:46:34 +0000

    Made it compatible with Chisimba, but only just

--- app/core_modules/groupadmin/templates/content/main_tpl.php
+++ app/core_modules/groupadmin/templates/content/main_tpl.php
@@ -1,63 +1,79 @@
 <?php  
 //do the check to check if TII is accessable
 
-	$objExtJS = $this->getObject('extjs','ext');
-	$objExtJS->show();
+    $objExtJS = $this->getObject('extjs','ext');
+    $objExtJS->show();
 
-	$fullUri = $this->uri(NULL);
-    $fullUri = explode("?",$fullUri);
-    $siteUri = $fullUri[0];
+    $fullUri = $this->uri(NULL);
+$fullUri = explode("?",$fullUri);
+$siteUri = $fullUri[0];
 
-	$ext = $this->getJavaScriptFile('ColumnNodeUI.js', 'groupadmin');
-	$ext .= $this->getJavaScriptFile('Ext.ux.grid.Search.js', 'groupadmin');
-	$ext .= $this->getJavaScriptFile('users.js', 'groupadmin');
-	$ext .= $this->getJavaScriptFile('interface.js', 'groupadmin');
+    $ext = $this->getJavaScriptFile('ColumnNodeUI.js', 'groupadmin');
+    $ext .= $this->getJavaScriptFile('Ext.ux.grid.Search.js', 'groupadmin');
+    $ext .= $this->getJavaScriptFile('users.js', 'groupadmin');
+    $ext .= $this->getJavaScriptFile('interface.js', 'groupadmin');
 
-	//setup the dynamicuri
-	$this->appendArrayVar('headerParams', '
-	        	<script type="text/javascript">	        		
-	        		var baseUri = "'.$siteUri.'";
-	        	</script>');
+    //setup the dynamicuri
+    $this->appendArrayVar('headerParams', '
+                    <script type="text/javascript">
+                            var baseUri = "'.$siteUri.'";
+                    </script>');
 
-	$ext .= "<style>
-	
-			#main-interface{
-				padding:10px;
-				margin:10px;
-			}
-			pre {
-   				font-size:11px; 
-			}
-			
-			.x-tab-panel-body .x-panel-body {
-			    padding:10px;
-			}
-			
-			/* default loading indicator for ajax calls */
-			.loading-indicator {
-				font-size:8pt;
-				background-image:url('../../resources/images/default/grid/loading.gif');
-				background-repeat: no-repeat;
-				background-position: left;
-				padding-left:20px;
-			}
-			
-			.new-tab {
-			    background-image:url(../feed-viewer/images/new_tab.gif) !important;
-			}
-			
-			
-			.tabs {
-			    background-image:url( ../desktop/images/tabs.gif ) !important;
-			}
-			
-			p { width:650px; }
+    // THIS IS CRAP and needs to be done properly -- Dkeats, 2010 10 03
+    $ext .= "<style>
 
-			
-			</style>";
+        #main-interface{
+                padding:10px;
+                margin:10px;
+        }
+        pre {
+                font-size:11px;
+        }
 
-    $this->appendArrayVar('headerParams', $ext);
+        .x-tab-panel-body .x-panel-body {
+            padding:10px;
+        }
+
+        /* default loading indicator for ajax calls */
+        .loading-indicator {
+                font-size:8pt;
+                background-image:url('../../resources/images/default/grid/loading.gif');
+                background-repeat: no-repeat;
+                background-position: left;
+                padding-left:20px;
+        }
+
+        .new-tab {
+            background-image:url(../feed-viewer/images/new_tab.gif) !important;
+        }
+
+
+        .tabs {
+            background-image:url( ../desktop/images/tabs.gif ) !important;
+        }
+
+        p { width:650px; }
+
+
+</style>";
+
+$this->appendArrayVar('headerParams', $ext);
+
+$middleColumn = '
+    <div id="mainPanel"></div>
+    <div id="combo"></div>
+';
+
+// Create an Instance of the CSS Layout
+$cssLayout = $this->newObject('csslayout', 'htmlelements');
+// Get the admin menu for the left menu
+$adminMenu = $this->getObject('adminmenu', 'toolbar');
+// Set the left menu into the left CSS layout
+$cssLayout->setLeftColumnContent($adminMenu->show());
+
+// Set the Content of middle column
+$cssLayout->setMiddleColumnContent($middleColumn);
+
+// Display the Layout
+echo $cssLayout->show();
 ?>
-
-<center><div id="mainPanel"></div></center>
-<div id="combo"></div>
