a0710a3641fdd9555d420463d84134e80c01e8ff
davidwaf <davidwaf@gmail.com>
2010-10-07 22:03:18 +0000

    added configurable shortcuts block on context home page

--- app/core_modules/context/templates/content/context_home_tpl.php
+++ app/core_modules/context/templates/content/context_home_tpl.php
@@ -158,7 +158,7 @@ if ($this->dbSysConfig->getValue('SHOW_SHORTCUTS_BLOCK', 'context') == "TRUE") {
         $imgPath = "";
 
         $link = new link($this->uri(array('action' => 'viewlogs'), 'contextcontent'));
-        $link->link = $trackerimg . '&nbsp;' . $this->objLanguage->languageText('mod_contextcontent_useractivitylogs', 'contextcontent',null,'User activity');
+        $link->link = $trackerimg . '&nbsp;' . $this->objLanguage->languageText('mod_contextcontent_useractivitylogs', 'contextcontent','User activity');
 
 
         $trackerlink .= '<br />' . $link->show() . '';
