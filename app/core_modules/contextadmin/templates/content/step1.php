ce5acab417ee12746f7ac44389ac54fd88d4fafd
davidwaf <davidwaf@gmail.com>
2010-10-12 10:45:41 +0000

    changed theming to show only if editing course

--- app/core_modules/contextadmin/templates/content/step1.php
+++ app/core_modules/contextadmin/templates/content/step1.php
@@ -14,15 +14,12 @@ $this->loadClass('link', 'htmlelements');
 $objIcon = $this->newObject('geticon', 'htmlelements');
 $objIcon->setIcon('loader');
 
-$contextexists=$this->objLanguage->code2Txt('mod_contextadmin_contextcodeexists', 'contextadmin');
+$contextexists = $this->objLanguage->code2Txt('mod_contextadmin_contextcodeexists', 'contextadmin');
 if ($mode == 'edit') {
     $fixup = NULL;
 
     $formAction = 'updatecontext';
-    $headerTitle = ucwords($this->objLanguage->code2Txt('mod_context_updatecontext', 'context', NULL, 'Update [-context-]')).': '.$context['title'];
-
-
-
+    $headerTitle = ucwords($this->objLanguage->code2Txt('mod_context_updatecontext', 'context', NULL, 'Update [-context-]')) . ': ' . $context['title'];
 } else {
     $formAction = 'savestep1';
     $headerTitle = $this->objLanguage->code2Txt('mod_contextadmin_createnewcontext', 'contextadmin', NULL, 'Create New [-Context-]');
@@ -80,7 +77,7 @@ if ($mode == 'edit') {
                     
                     // Set message to checking
                     jQuery("#contextcodemessage").removeClass("success");
-                    jQuery("#contextcodemessage").html("<span id=\"contextcodecheck\">'.addslashes($objIcon->show()).' Checking ...</span>");
+                    jQuery("#contextcodemessage").html("<span id=\"contextcodecheck\">' . addslashes($objIcon->show()) . ' Checking ...</span>");
                     
                     
                     // Set current Code
@@ -98,7 +95,7 @@ if ($mode == 'edit') {
                                 
                                 // IF code exists
                                 if (msg == "exists") {
-                                    jQuery("#contextcodemessage").html("'.$contextexists.'");
+                                    jQuery("#contextcodemessage").html("' . $contextexists . '");
                                     jQuery("#contextcodemessage").addClass("error");
                                     jQuery("#input_contextcode").addClass("inputerror");
                                     jQuery("#contextcodemessage").removeClass("success");
@@ -125,16 +122,16 @@ if ($mode == 'edit') {
 
 $objStepMenu = $this->newObject('stepmenu', 'navigation');
 if ($mode == 'edit') {
-    $objStepMenu->addStep(str_replace('[-num-]', 1, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')).' - '.ucwords($this->objLanguage->code2Txt('mod_context_contextsettings', 'context', NULL, '[-context-] Settings')), ucwords($this->objLanguage->code2Txt('mod_contextadmin_updatecontextitlesettings', 'contextadmin', NULL, 'Update [-context-] Title and Settings')));
+    $objStepMenu->addStep(str_replace('[-num-]', 1, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')) . ' - ' . ucwords($this->objLanguage->code2Txt('mod_context_contextsettings', 'context', NULL, '[-context-] Settings')), ucwords($this->objLanguage->code2Txt('mod_contextadmin_updatecontextitlesettings', 'contextadmin', NULL, 'Update [-context-] Title and Settings')));
 } else {
-    $objStepMenu->addStep(str_replace('[-num-]', 1, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')).' - '.ucwords($this->objLanguage->code2Txt('mod_context_contextsettings', 'context', NULL, '[-context-] Settings')), $this->objLanguage->code2Txt('mod_contextadmin_checkcontextcodeavailable', 'contextadmin', NULL, 'Enter [-context-] settings and check whether [-context-] code is available'));
+    $objStepMenu->addStep(str_replace('[-num-]', 1, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')) . ' - ' . ucwords($this->objLanguage->code2Txt('mod_context_contextsettings', 'context', NULL, '[-context-] Settings')), $this->objLanguage->code2Txt('mod_contextadmin_checkcontextcodeavailable', 'contextadmin', NULL, 'Enter [-context-] settings and check whether [-context-] code is available'));
 }
-$objStepMenu->addStep(str_replace('[-num-]', 2, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')).' - '.ucwords($this->objLanguage->code2Txt('mod_contextadmin_contextinformation', 'contextadmin', NULL, '[-context-] Information')), $this->objLanguage->code2Txt('mod_contextadmin_enterinfoaboutcontext', 'contextadmin', NULL, 'Enter more information about your [-context-] and select a [-context-] image'));
+$objStepMenu->addStep(str_replace('[-num-]', 2, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')) . ' - ' . ucwords($this->objLanguage->code2Txt('mod_contextadmin_contextinformation', 'contextadmin', NULL, '[-context-] Information')), $this->objLanguage->code2Txt('mod_contextadmin_enterinfoaboutcontext', 'contextadmin', NULL, 'Enter more information about your [-context-] and select a [-context-] image'));
 
-$objStepMenu->addStep(str_replace('[-num-]', 3, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')).' - '.ucwords($this->objLanguage->code2Txt('mod_contextadmin_courseoutcomes', 'contextadmin', NULL, 'Course Outcomes')), $this->objLanguage->code2Txt('mod_context_enteroutcomecontext', 'contextadmin', NULL, 'Enter the main Outcomes / Goals of the [-context-]'));
+$objStepMenu->addStep(str_replace('[-num-]', 3, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')) . ' - ' . ucwords($this->objLanguage->code2Txt('mod_contextadmin_courseoutcomes', 'contextadmin', NULL, 'Course Outcomes')), $this->objLanguage->code2Txt('mod_context_enteroutcomecontext', 'contextadmin', NULL, 'Enter the main Outcomes / Goals of the [-context-]'));
 
 
-$objStepMenu->addStep(str_replace('[-num-]', 4, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')).' - '.ucwords($this->objLanguage->code2Txt('mod_context_contextpluginsabs', 'context', array('plugins'=>'plugins'), '[-context-] [-plugins-]')), $this->objLanguage->code2Txt('mod_contextadmin_selectpluginsforcontextabs', 'contextadmin', array('plugins'=>'plugins'), 'Select the [-plugins-] you would like to use in this [-context-]'));
+$objStepMenu->addStep(str_replace('[-num-]', 4, $this->objLanguage->code2Txt('mod_contextadmin_stepnumber', 'contextadmin', NULL, 'Step [-num-]')) . ' - ' . ucwords($this->objLanguage->code2Txt('mod_context_contextpluginsabs', 'context', array('plugins' => 'plugins'), '[-context-] [-plugins-]')), $this->objLanguage->code2Txt('mod_contextadmin_selectpluginsforcontextabs', 'contextadmin', array('plugins' => 'plugins'), 'Select the [-plugins-] you would like to use in this [-context-]'));
 $objStepMenu->current = 1;
 echo $objStepMenu->show();
 
@@ -143,26 +140,26 @@ $header = new htmlheading();
 $header->type = 1;
 $header->str = ucwords($headerTitle);
 
-echo '<br />'.$header->show();
+echo '<br />' . $header->show();
 
 
 // CREATE FORM
-$form = new form ('createcontext', $this->uri(array('action'=>$formAction)));
+$form = new form('createcontext', $this->uri(array('action' => $formAction)));
 
 
 $code = new textinput('contextcode');
 
 if ($mode == 'add' && is_array($fixup)) {
     if ($fixup['contextcode'] == '') {
-        $contextCodeMessage = '<span class="warning">'.$this->objLanguage->code2Txt('mod_contextadmin_didnotentercontextcode', 'contextadmin', NULL, 'You did not enter a [-context-] code').'</span>';
+        $contextCodeMessage = '<span class="warning">' . $this->objLanguage->code2Txt('mod_contextadmin_didnotentercontextcode', 'contextadmin', NULL, 'You did not enter a [-context-] code') . '</span>';
     } else {
-        $contextCodeMessage = '<span class="warning">'.$this->objLanguage->languageText('mod_contextadmin_youentered', 'contextadmin', 'You entered').' <strong><u>'.$fixup['contextcode'].'</u></strong> '.$this->objLanguage->languageText('mod_contextadmin_buthasalreadybeentaken', 'contextadmin', 'but that has been taken already').'</span>';
+        $contextCodeMessage = '<span class="warning">' . $this->objLanguage->languageText('mod_contextadmin_youentered', 'contextadmin', 'You entered') . ' <strong><u>' . $fixup['contextcode'] . '</u></strong> ' . $this->objLanguage->languageText('mod_contextadmin_buthasalreadybeentaken', 'contextadmin', 'but that has been taken already') . '</span>';
     }
 } else {
     $contextCodeMessage = '';
 }
 
-$codeLabel = new label (ucwords($this->objLanguage->code2Txt('mod_context_contextcode', 'context', NULL, '[-context-] Code')), 'input_contextcode');
+$codeLabel = new label(ucwords($this->objLanguage->code2Txt('mod_context_contextcode', 'context', NULL, '[-context-] Code')), 'input_contextcode');
 
 
 $title = new textinput('title');
@@ -174,19 +171,19 @@ if ($mode == 'add' && is_array($fixup)) {
     $title->value = $context['title'];
 }
 
-$titleLabel = new label ($this->objLanguage->languageText('word_title', 'system', 'Title'), 'input_title');
-$objConfig=$this->getObject('altconfig','config');
-$skinName=$objConfig->getdefaultSkin();
+$titleLabel = new label($this->objLanguage->languageText('word_title', 'system', 'Title'), 'input_title');
+$objConfig = $this->getObject('altconfig', 'config');
+$skinName = $objConfig->getdefaultSkin();
 
-$validCanvases = array_map('basename', glob('usrfiles/context/'.$this->objContext->getContextCode().'/canvases/*', GLOB_ONLYDIR));
+$validCanvases = array_map('basename', glob('usrfiles/context/' . $this->objContext->getContextCode() . '/canvases/*', GLOB_ONLYDIR));
 
 
-$canvas = new dropdown ('canvas');
+$canvas = new dropdown('canvas');
 //$status->setBreakSpace('<br />');
 
-$canvas->addOption('None','None');
-foreach($validCanvases as $validCanvas){
-    $canvas->addOption($validCanvas,$validCanvas);
+$canvas->addOption('None', 'None');
+foreach ($validCanvases as $validCanvas) {
+    $canvas->addOption($validCanvas, $validCanvas);
 }
 
 //$canvas->size = 50;
@@ -197,10 +194,10 @@ if ($mode == 'add' && is_array($fixup)) {
     $canvas->setSelected($context['canvas']);
 }
 
-$canvasLabel = new label ($this->objLanguage->languageText('mod_contextadmin_theme', 'contextadmin', 'Theme'), 'input_canvas');
+$canvasLabel = new label($this->objLanguage->languageText('mod_contextadmin_theme', 'contextadmin', 'Theme'), 'input_canvas');
 
 
-$status = new dropdown ('status');
+$status = new dropdown('status');
 //$status->setBreakSpace('<br />');
 $status->addOption('Published', $this->objLanguage->languageText('word_published', 'system', 'Published'));
 $status->addOption('Unpublished', $this->objLanguage->languageText('word_unpublished', 'system', 'Unpublished'));
@@ -211,11 +208,11 @@ if ($mode == 'add' && is_array($fixup)) {
     $status->setSelected($context['status']);
 }
 
-$access= new radio ('access');
+$access = new radio('access');
 $access->setBreakSpace('<br />');
-$access->addOption('Public', '<strong>'.$this->objLanguage->languageText('word_public', 'system', 'Public').'</strong> - <span class="caption">'.$this->objLanguage->code2Txt('mod_context_publichelp', 'context', NULL, '[-context-] can be accessed by all users, including anonymous users').'</span>');
-$access->addOption('Open', '<strong>'.$this->objLanguage->languageText('word_open', 'system', 'Open').'</strong> - <span class="caption">'.$this->objLanguage->code2Txt('mod_context_opencontextdescription', 'context', NULL, '[-context-] can be accessed by all users that are logged in').'</span>');
-$access->addOption('Private', '<strong>'.$this->objLanguage->languageText('word_private', 'system', 'Private').'</strong> - <span class="caption">'.$this->objLanguage->code2Txt('mod_context_privatecontextdescription', 'context', NULL, 'Only [-context-] members can enter the [-context-]').'<span class="caption">');
+$access->addOption('Public', '<strong>' . $this->objLanguage->languageText('word_public', 'system', 'Public') . '</strong> - <span class="caption">' . $this->objLanguage->code2Txt('mod_context_publichelp', 'context', NULL, '[-context-] can be accessed by all users, including anonymous users') . '</span>');
+$access->addOption('Open', '<strong>' . $this->objLanguage->languageText('word_open', 'system', 'Open') . '</strong> - <span class="caption">' . $this->objLanguage->code2Txt('mod_context_opencontextdescription', 'context', NULL, '[-context-] can be accessed by all users that are logged in') . '</span>');
+$access->addOption('Private', '<strong>' . $this->objLanguage->languageText('word_private', 'system', 'Private') . '</strong> - <span class="caption">' . $this->objLanguage->code2Txt('mod_context_privatecontextdescription', 'context', NULL, 'Only [-context-] members can enter the [-context-]') . '<span class="caption">');
 
 
 if ($mode == 'add' && is_array($fixup)) {
@@ -231,14 +228,14 @@ $table = $this->newObject('htmltable', 'htmlelements');
 if ($mode == 'edit') {
     $table->startRow();
     $table->addCell($this->objLanguage->code2Txt('mod_context_contextcode', 'context', NULL, '[-context-] Code'), 100);
-    $table->addCell('<strong title="'.$this->objLanguage->code2Txt('mod_contextadmin_contextcodecannotbechanged', 'contextadmin', NULL, '[-context-] code can not be changed').'">'.strtoupper($context['contextcode']).'</strong>');
+    $table->addCell('<strong title="' . $this->objLanguage->code2Txt('mod_contextadmin_contextcodecannotbechanged', 'contextadmin', NULL, '[-context-] code can not be changed') . '">' . strtoupper($context['contextcode']) . '</strong>');
     $table->endRow();
     $hiddenInput = new hiddeninput('editcontextcode', $context['contextcode']);
     $form->addToForm($hiddenInput->show());
 } else {
     $table->startRow();
     $table->addCell($codeLabel->show(), 100);
-    $table->addCell($code->show().' <span id="contextcodemessage">'.$contextCodeMessage.'</span>');
+    $table->addCell($code->show() . ' <span id="contextcodemessage">' . $contextCodeMessage . '</span>');
     $table->endRow();
 }
 
@@ -248,13 +245,14 @@ $table->addCell($title->show());
 $table->endRow();
 
 
-$uploadlink=new link($this->uri(array(),"filemanager"));
-$uploadlink->link='<strong>'.$this->objLanguage->languageText('mod_contextadmin_upload', 'contextadmin', 'Upload').'</strong>';
+$uploadlink = new link($this->uri(array(), "filemanager"));
+$uploadlink->link = '<strong>' . $this->objLanguage->languageText('mod_contextadmin_upload', 'contextadmin', 'Upload') . '</strong>';
 
-$table->startRow();
-$table->addCell($canvasLabel->show());
-$table->addCell($canvas->show().$uploadlink->show());
-
+if ($mode == 'edit') {
+    $table->startRow();
+    $table->addCell($canvasLabel->show());
+    $table->addCell($canvas->show() . $uploadlink->show());
+}
 $table->endRow();
 
 $table->startRow();
@@ -272,10 +270,10 @@ $table->addCell('&nbsp;');
 $table->addCell('&nbsp;');
 $table->endRow();
 
-$showcomment = new dropdown ('showcomment');
+$showcomment = new dropdown('showcomment');
 //$status->setBreakSpace('<br />');
-$showcomment->addOption('1', $this->objLanguage->languageText('word_yes', 'system', 'No')." ");
-$showcomment->addOption('0', $this->objLanguage->languageText('word_no', 'system', 'No')." ");
+$showcomment->addOption('1', $this->objLanguage->languageText('word_yes', 'system', 'No') . " ");
+$showcomment->addOption('0', $this->objLanguage->languageText('word_no', 'system', 'No') . " ");
 
 if ($mode == 'add' && is_array($fixup)) {
     $showcomment->setSelected($fixup['showcomment']);
@@ -285,7 +283,7 @@ if ($mode == 'add' && is_array($fixup)) {
 
 $table->startRow();
 $table->addCell($this->objLanguage->languageText('mod_contextadmin_comment', 'contextadmin', 'Comment'));
-$table->addCell($showcomment->show()." *".$this->objLanguage->languageText('mod_contextadmin_comments', 'contextadmin', 'Enable or Disable users to post comments on page content'));
+$table->addCell($showcomment->show() . " *" . $this->objLanguage->languageText('mod_contextadmin_comments', 'contextadmin', 'Enable or Disable users to post comments on page content'));
 $table->endRow();
 
 $table->startRow();
@@ -298,14 +296,14 @@ $table->addCell($this->objLanguage->languageText('word_access', 'system', 'Acces
 $table->addCell($access->show());
 $table->endRow();
 
-$emailAlert = new checkbox('emailalertopt',$this->objLanguage->languageText('mod_contextadmin_emailalert', 'contextadmin', 'Email alerts'),true);  // this will checked
+$emailAlert = new checkbox('emailalertopt', $this->objLanguage->languageText('mod_contextadmin_emailalert', 'contextadmin', 'Email alerts'), true);  // this will checked
 
-$alerts=array();
-if($mode == 'edit'){
- $alerts=explode("|", $context['alerts']);   
+$alerts = array();
+if ($mode == 'edit') {
+    $alerts = explode("|", $context['alerts']);
 }
 
-if(count($alerts) > 0){
+if (count($alerts) > 0) {
     $emailAlert->setChecked($alerts[0] == 'e');
 }
 $table->startRow();
@@ -313,11 +311,11 @@ $table->addCell($this->objLanguage->languageText('mod_contextadmin_emailalert', 
 $table->addCell($emailAlert->show());
 $table->endRow();
 
-$button = new button ('savecontext', $this->objLanguage->languageText('mod_contextadmin_gotonextstep', 'contextadmin', 'Go to Next Step'));
+$button = new button('savecontext', $this->objLanguage->languageText('mod_contextadmin_gotonextstep', 'contextadmin', 'Go to Next Step'));
 $button->cssId = 'savebutton';
 $button->setToSubmit();
 
-$form->addToForm($table->show().'<p><br />'.$button->show().'</p>');
+$form->addToForm($table->show() . '<p><br />' . $button->show() . '</p>');
 
 $hiddenInput = new hiddeninput('mode', $mode);
 $form->addToForm($hiddenInput->show());
@@ -328,5 +326,4 @@ if ($mode == 'add') {
 $form->addRule('title', $this->objLanguage->languageText('mod_contextadmin_pleaseentertitle', 'contextadmin', 'Please enter a title'), 'required');
 
 echo $form->show();
-
 ?>
