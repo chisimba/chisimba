9d21a5097516f8c4d14b46822663a6937ba8c7d3
davidwaf <davidwaf@gmail.com>
2010-11-01 09:20:33 +0000

    added configurable value to display number of students in a course list

--- app/core_modules/context/classes/displaycontext_class_inc.php
+++ app/core_modules/context/classes/displaycontext_class_inc.php
@@ -29,18 +29,17 @@
  * @link      http://avoir.uwc.ac.za
  * @see       core
  */
-/* -------------------- dbTable class ----------------*/
+/* -------------------- dbTable class ---------------- */
 // security check - must be included in all scripts
-if (! /**
- * Description for $GLOBALS
- * @global entry point $GLOBALS['kewl_entry_point_run']
- * @name   $kewl_entry_point_run
- */
-$GLOBALS ['kewl_entry_point_run']) {
-    die ( "You cannot view this page directly" );
+if (!/**
+         * Description for $GLOBALS
+         * @global entry point $GLOBALS['kewl_entry_point_run']
+         * @name   $kewl_entry_point_run
+         */
+        $GLOBALS ['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
 }
 // end security check
-
 
 /**
  * Display Context
@@ -57,20 +56,19 @@ $GLOBALS ['kewl_entry_point_run']) {
  * @see       core
  */
 class displaycontext extends object {
+
     /**
      * The user Object
      *
      * @var object $objUser
      */
     public $objUser;
-
     /**
      * Module to go to when editing/deleting a course
      *
      * @var string $module
      */
     public $module = 'contextadmin';
-
     /**
      * Image to display when no course image is available
      *
@@ -79,28 +77,30 @@ class displaycontext extends object {
     private $noImage;
 
     /**
-     *Initialize by send the table name to be accessed
+     * Initialize by send the table name to be accessed
      *
      */
     public function init() {
-        $this->objContextImage = $this->getObject ( 'contextimage' );
-        $this->objUser = $this->getObject ( 'user', 'security' );
-        $this->objContext = $this->getObject ( 'dbcontext' );
-        $this->objUserContext = $this->getObject ( 'usercontext' );
-        $this->loadClass ( 'link', 'htmlelements' );
-        $this->loadClass ( 'htmlheading', 'htmlelements' );
-        $this->objLanguage = $this->getObject ( 'language', 'language' );
-		$this->objDBContext = $this->getObject('dbcontext', 'context');
+        $this->objContextImage = $this->getObject('contextimage');
+        $this->objUser = $this->getObject('user', 'security');
+        $this->objContext = $this->getObject('dbcontext');
+        $this->objUserContext = $this->getObject('usercontext');
+        $this->loadClass('link', 'htmlelements');
+        $this->loadClass('htmlheading', 'htmlelements');
+        $this->objLanguage = $this->getObject('language', 'language');
+        $this->objDBContext = $this->getObject('dbcontext', 'context');
+        $this->dbSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
+        $showStudentCount = $this->dbSysConfig->getValue('SHOW_STUDENT_COUNT', 'context');
 
-        $objIcon = $this->getObject ( 'geticon', 'htmlelements' );
-        $objIcon->setIcon ( 'imagepreview' );
+        $objIcon = $this->getObject('geticon', 'htmlelements');
+        $objIcon->setIcon('imagepreview');
 
-        $this->noImage = $objIcon->show ();
+        $this->noImage = $objIcon->show();
 
-        if ($this->objUser->isLoggedIn ()) {
-            $this->userContexts = $this->objUserContext->getUserContext ( $this->objUser->userId () );
+        if ($this->objUser->isLoggedIn()) {
+            $this->userContexts = $this->objUserContext->getUserContext($this->objUser->userId());
         } else {
-            $this->userContexts = array ();
+            $this->userContexts = array();
         }
     }
 
@@ -120,22 +120,22 @@ class displaycontext extends object {
         // Flag on whether to show link for private courses
         $showLink = $disablePrivateAccess;
 
-        if (! $disablePrivateAccess) {
+        if (!$disablePrivateAccess) {
             // If admin, show link
-			$access = $this->objDBContext->getField('access', $context ['contextcode']);
-            if ($this->objUser->isAdmin () || 
-					in_array ( $context ['contextcode'], $this->userContexts ) || 
-					$access == 'Public' ||
-					$access == 'Open' ) {
+            $access = $this->objDBContext->getField('access', $context ['contextcode']);
+            if ($this->objUser->isAdmin() ||
+                    in_array($context ['contextcode'], $this->userContexts) ||
+                    $access == 'Public' ||
+                    $access == 'Open') {
                 $showLink = TRUE;
             }
         }
 
         if ($showLink) {
-            $link = new link ( $this->uri ( array ('action' => 'joincontext', 'contextcode' => $context ['contextcode'] ) ) );
+            $link = new link($this->uri(array('action' => 'joincontext', 'contextcode' => $context ['contextcode'])));
             $link->link = $context ['title'];
         } else {
-            $link = new link ( 'javascript:contextPrivate();' );
+            $link = new link('javascript:contextPrivate();');
             $link->link = $context ['title'];
         }
 
@@ -143,7 +143,7 @@ class displaycontext extends object {
         $str = '';
 
         // Get Context Image
-        $contextImage = $this->objContextImage->getContextImage ( $context ['contextcode'] );
+        $contextImage = $this->objContextImage->getContextImage($context ['contextcode']);
 
         // Show if it has an image
         if ($contextImage == FALSE) {
@@ -153,196 +153,199 @@ class displaycontext extends object {
         }
 
         if ($showLink) {
-            $contextImageLink = new link ( $this->uri ( array ('action' => 'joincontext', 'contextcode' => $context ['contextcode'] ) ) );
+            $contextImageLink = new link($this->uri(array('action' => 'joincontext', 'contextcode' => $context ['contextcode'])));
         } else {
-            $contextImageLink = new link ( 'javascript:contextPrivate();' );
+            $contextImageLink = new link('javascript:contextPrivate();');
         }
         $contextImageLink->link = $contextImage;
 
         $str = '';
 
-        $str .= '<p><strong>' . ucwords ( $this->objLanguage->code2Txt ( 'mod_context_contextcode', 'system', NULL, '[-context-] Code' ) ) . '</strong>: ' . $context ['contextcode'] . '</p>';
+        $str .= '<p><strong>' . ucwords($this->objLanguage->code2Txt('mod_context_contextcode', 'system', NULL, '[-context-] Code')) . '</strong>: ' . $context ['contextcode'] . '</p>';
 
-        $lecturers = $this->objUserContext->getContextLecturers ( $context ['contextcode'] );
-        if (count ( $lecturers ) > 0) {
-            $str .= '<p><strong>' . ucwords ( $this->objLanguage->code2Txt ( 'word_lecturers', 'system', NULL, '[-authors-]' ) ) . '</strong>: ';
+        $lecturers = $this->objUserContext->getContextLecturers($context ['contextcode']);
+        if (count($lecturers) > 0) {
+            $str .= '<p><strong>' . ucwords($this->objLanguage->code2Txt('word_lecturers', 'system', NULL, '[-authors-]')) . '</strong>: ';
             $divider = '';
 
-            foreach ( $lecturers as $lecturer ) {
+            foreach ($lecturers as $lecturer) {
                 $str .= $divider . $lecturer ['firstname'] . ' ' . $lecturer ['surname'];
                 $divider = ', ';
 
-                if ($this->objUser->userId () == $lecturer ['userid']) {
+                if ($this->objUser->userId() == $lecturer ['userid']) {
                     $canEdit = TRUE;
                 }
             }
         }
 
-        switch (strtolower ( $context ['access'] )) {
+        switch (strtolower($context ['access'])) {
             case 'public' :
-                $access = $this->objLanguage->code2Txt ( 'mod_context_publiccontextexplanation', 'context', NULL, 'This is an open [-context-] that any user may enter' );
+                $access = $this->objLanguage->code2Txt('mod_context_publiccontextexplanation', 'context', NULL, 'This is an open [-context-] that any user may enter');
                 break;
 
             case 'open' :
-                $access = $this->objLanguage->code2Txt ( 'mod_context_opencontextexplanation', 'context', NULL, 'This is an open [-context-] that any logged-in user may enter' );
+                $access = $this->objLanguage->code2Txt('mod_context_opencontextexplanation', 'context', NULL, 'This is an open [-context-] that any logged-in user may enter');
                 break;
 
             default :
-                $access = $this->objLanguage->code2Txt ( 'mod_context_privatecontextexplanation', 'context', NULL, 'This is a closed [-context-] only accessible to members' );
+                $access = $this->objLanguage->code2Txt('mod_context_privatecontextexplanation', 'context', NULL, 'This is a closed [-context-] only accessible to members');
                 break;
         }
 
-        $str .= '<p><strong>' . ucwords ( $this->objLanguage->languageText ( 'mod_context_accessettings', 'context', 'Access Settings' ) ) . '</strong>: ' . $context ['access'] . ' - ' . $access . '</p>';
+        $str .= '<p><strong>' . ucwords($this->objLanguage->languageText('mod_context_accessettings', 'context', 'Access Settings')) . '</strong>: ' . $context ['access'] . ' - ' . $access . '</p>';
 
-        $table = $this->newObject ( 'htmltable', 'htmlelements' );
-        $table->startRow ();
-        $table->addCell ( $contextImageLink->show (), 120 );
-        $table->addCell ( $str );
+        $table = $this->newObject('htmltable', 'htmlelements');
+        $table->startRow();
+        $table->addCell($contextImageLink->show(), 120);
+        $table->addCell($str);
 
         $content = $str;
 
         $title = '';
 
-        if ($this->objUser->isAdmin ()) {
+        if ($this->objUser->isAdmin()) {
             $canEdit = TRUE;
         }
 
         if ($showEditDeleteLinks && $canEdit) {
-            $objIcon = $this->newObject ( 'geticon', 'htmlelements' );
-            $objIcon->setIcon ( 'edit' );
+            $objIcon = $this->newObject('geticon', 'htmlelements');
+            $objIcon->setIcon('edit');
 
-            $editLink = new link ( $this->uri ( array ('action' => 'edit', 'contextcode' => $context ['contextcode'] ), $this->module ) );
-            $editLink->link = $objIcon->show ();
+            $editLink = new link($this->uri(array('action' => 'edit', 'contextcode' => $context ['contextcode']), $this->module));
+            $editLink->link = $objIcon->show();
 
-            $editOptions = ' ' . $editLink->show ();
+            $editOptions = ' ' . $editLink->show();
 
-            $objIcon->setIcon ( 'delete' );
+            $objIcon->setIcon('delete');
 
-            $deleteLink = new link ( $this->uri ( array ('action' => 'delete', 'contextcode' => $context ['contextcode'] ), $this->module ) );
-            $deleteLink->link = $objIcon->show ();
+            $deleteLink = new link($this->uri(array('action' => 'delete', 'contextcode' => $context ['contextcode']), $this->module));
+            $deleteLink->link = $objIcon->show();
 
-            $editOptions .= ' ' . $deleteLink->show ();
+            $editOptions .= ' ' . $deleteLink->show();
 
             $title = '<div style="float: right">' . $editOptions . '</div>';
         }
 
-        $title .= $link->show ();
+        $title .= $link->show();
 
         if ($includeFeatureBox) {
-            $objFeatureBox = $this->newObject ( 'featurebox', 'navigation' );
-            return $objFeatureBox->show ( $title, $content );
+            $objFeatureBox = $this->newObject('featurebox', 'navigation');
+            return $objFeatureBox->show($title, $content);
         } else {
             $header = new htmlHeading ( );
             $header->type = 3;
             $header->str = $title;
-            return $header->show () . $content;
+            return $header->show() . $content;
         }
-
     }
+
     /**
      * Method to display the information of a context
      *
      * @param array $context
      * @return array
      */
-    public function jsonContextDisplayBlock( $context ) {
+    public function jsonContextDisplayBlock($context) {
         $canEdit = FALSE;
 
         // Flag on whether to show link for private courses
         $showLink = FALSE;
         $disablePrivateAccess = FALSE;
 
-        if (! $disablePrivateAccess) {
+        if (!$disablePrivateAccess) {
             // If admin, show link
-			         $access = $this->objDBContext->getField('access', $context ['contextcode']);
-            if ($this->objUser->isAdmin () || 
-					in_array ( $context ['contextcode'], $this->userContexts ) || 
-					$access == 'Public' ||
-					$access == 'Open' ) {
+            $access = $this->objDBContext->getField('access', $context ['contextcode']);
+            if ($this->objUser->isAdmin() ||
+                    in_array($context ['contextcode'], $this->userContexts) ||
+                    $access == 'Public' ||
+                    $access == 'Open') {
                 $showLink = TRUE;
             }
         }
 
         if ($showLink) {
-            $link = new link ( $this->uri ( array ('action' => 'joincontext', 'contextcode' => $context ['contextcode'] ) ) );
+            $link = new link($this->uri(array('action' => 'joincontext', 'contextcode' => $context ['contextcode'])));
             $link->link = $context ['title'];
         } else {
-            $link = new link ( 'javascript:contextPrivate();' );
+            $link = new link('javascript:contextPrivate();');
             $link->link = $context ['title'];
         }
         $courseInfoArray = array();
-        $title = $link->show ();
-        $courseInfoArray['code'] = $context ['contextcode'];        
-        $courseInfoArray['coursecode'] = ucwords ( $this->objLanguage->code2Txt ( 'mod_context_contextcode', 'system', NULL, '[-context-] Code' ) ).' : ' . $context ['contextcode'];
-        $courseInfoArray['title'] = $context ['title'];
+        $title = $link->show();
+        $courseInfoArray['code'] = $context ['contextcode'];
+        $courseInfoArray['coursecode'] = ucwords($this->objLanguage->code2Txt('mod_context_contextcode', 'system', NULL, '[-context-] Code')) . ' : ' . $context ['contextcode'];
+        $studentCount = "";
+        if (strtoupper($showStudentCount) == 'TRUE') {
+            $studentCount = '&nbsp;(' . count($this->objUserContext->getContextStudents($context ['contextcode'])) . ')';
+        }
+        $courseInfoArray['title'] = $context ['title'] . $studentCount;
         $courseInfoArray['status'] = "none";
-        $lecturers = $this->objUserContext->getContextLecturers ( $context ['contextcode'] );
-        if (count ( $lecturers ) > 0) {
-            $str = "";            
-            $courseInfoArray['lecturertitle'] = ucwords ( $this->objLanguage->code2Txt ( 'word_lecturers', 'system', NULL, '[-authors-]' ) ) ;
+        $lecturers = $this->objUserContext->getContextLecturers($context ['contextcode']);
+        if (count($lecturers) > 0) {
+            $str = "";
+            $courseInfoArray['lecturertitle'] = ucwords($this->objLanguage->code2Txt('word_lecturers', 'system', NULL, '[-authors-]'));
             $divider = '';
 
-            foreach ( $lecturers as $lecturer ) {
+            foreach ($lecturers as $lecturer) {
                 $str .= $divider . $lecturer ['firstname'] . ' ' . $lecturer ['surname'];
                 $divider = ', ';
 
-                if ($this->objUser->userId () == $lecturer ['userid']) {
+                if ($this->objUser->userId() == $lecturer ['userid']) {
                     $canEdit = TRUE;
                     $courseInfoArray['status'] = $context['status'];
                 }
             }
             $courseInfoArray['lecturers'] = $str;
         }
-		
+
         //do not show this course if the its unpublished and the user is not a lecturer
-        if($context['status'] == 'Unpublished' && $canEdit == FALSE)
-        {        	
-        	return false;
+        if ($context['status'] == 'Unpublished' && $canEdit == FALSE) {
+            return false;
         }
-        
-        switch (strtolower ( $context ['access'] )) {
+
+        switch (strtolower($context ['access'])) {
             case 'public' :
-                $access = $this->objLanguage->code2Txt ( 'mod_context_publiccontextexplanation', 'context', NULL, 'This is an open [-context-] that any user may enter' );
+                $access = $this->objLanguage->code2Txt('mod_context_publiccontextexplanation', 'context', NULL, 'This is an open [-context-] that any user may enter');
                 break;
 
             case 'open' :
-                $access = $this->objLanguage->code2Txt ( 'mod_context_opencontextexplanation', 'context', NULL, 'This is an open [-context-] that any logged-in user may enter' );
+                $access = $this->objLanguage->code2Txt('mod_context_opencontextexplanation', 'context', NULL, 'This is an open [-context-] that any logged-in user may enter');
                 break;
 
             default :
-                $access = $this->objLanguage->code2Txt ( 'mod_context_privatecontextexplanation', 'context', NULL, 'This is a closed [-context-] only accessible to members' );
+                $access = $this->objLanguage->code2Txt('mod_context_privatecontextexplanation', 'context', NULL, 'This is a closed [-context-] only accessible to members');
                 break;
         }
-        $courseInfoArray['accesstitle'] = ucwords ( $this->objLanguage->languageText ( 'mod_context_accessettings', 'context', 'Access Settings' ) );
-        $courseInfoArray['access'] = $context ['access'] .' - '. $access;
+        $courseInfoArray['accesstitle'] = ucwords($this->objLanguage->languageText('mod_context_accessettings', 'context', 'Access Settings'));
+        $courseInfoArray['access'] = $context ['access'] . ' - ' . $access;
 
         return $courseInfoArray;
-
     }
+
     /**
      * Added by Paul Mungai
      * Method to display the user context for json
      * @param array $jsonContext
      * @return array
      */
-    public function jsonContextOutput( $userContexts ) {
-     	$objUserContext = $this->getObject('usercontext', 'context');
-     	$countUserContexts = $objUserContext->getUserContext($this->objUser->userId());
-     	//$activityCount = ( count ( $countUserContexts ) );
-      	$str = '{"contextcount":"'.$activityCount.'","usercontexts":[';
-      	$contextArray = array();
-    	foreach( $userContexts as $userContext ){
-    		$thisContext = $this->objContext->getContext( $userContext );
-       		$contxtDetails = $this->jsonContextDisplayBlock( $thisContext );
-       		if(!$contxtDetails == false){
-       			$contextArray[] = $contxtDetails;
-       		}	    	
-    			
-    	 }
-    	 
-    	 $activityCount = ( count ($contextArray));
-    	 return json_encode(array('contextcount' => $activityCount, 'usercourses' =>  $contextArray));
+    public function jsonContextOutput($userContexts) {
+        $objUserContext = $this->getObject('usercontext', 'context');
+        $countUserContexts = $objUserContext->getUserContext($this->objUser->userId());
+        //$activityCount = ( count ( $countUserContexts ) );
+        $str = '{"contextcount":"' . $activityCount . '","usercontexts":[';
+        $contextArray = array();
+        foreach ($userContexts as $userContext) {
+            $thisContext = $this->objContext->getContext($userContext);
+            $contxtDetails = $this->jsonContextDisplayBlock($thisContext);
+            if (!$contxtDetails == false) {
+                $contextArray[] = $contxtDetails;
+            }
+        }
+
+        $activityCount = ( count($contextArray));
+        return json_encode(array('contextcount' => $activityCount, 'usercourses' => $contextArray));
     }
+
 }
 
 ?>
