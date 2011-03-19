54c119c96eb81f3d3fedcc7eb96d2cdca4148667
dkeats <dkeats@gmail.com>
2011-03-19 20:30:11 +0000

    Changed default to 1.4.2 and added 1.5.0 for experimentation

--- app/core_modules/jquery/classes/jquery_class_inc.php
+++ app/core_modules/jquery/classes/jquery_class_inc.php
@@ -49,7 +49,7 @@ class jquery extends object
     public function init()
     { 
         //Default jQuery version (Global)
-        $this->version = '1.2.3';
+        $this->version = '1.4.2';
     }
 
     /**
