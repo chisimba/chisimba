f2f044b766fa5fec56373b74aa5dbc9d7eac8e81
joconnor <joconnor@gmail.com>
2011-06-20 10:16:50 +0000

    Fixed block to make it use correct source of data.

--- app/core_modules/context/classes/block_contextoutcomes_class_inc.php
+++ app/core_modules/context/classes/block_contextoutcomes_class_inc.php
@@ -2,24 +2,24 @@
 
 /**
  * Learner Outcomes block
- * 
+ *
  * This class generates a block that displays the about learner outcomes of a course
- * 
+ *
  * PHP version 5
- * 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @category  Chisimba
  * @package   context
  * @author    Paul Mungai <paulwando@gmail.com>
@@ -47,9 +47,9 @@ $GLOBALS['kewl_entry_point_run'])
 
 /**
  * Learner Outcomes block
- * 
+ *
  * This class generates a block that displays the about learner outcomes of a course
- * 
+ *
  * @category  Chisimba
  * @package   context
  * @author    Paul Mungai <paulwando@gmail.com>
@@ -65,7 +65,7 @@ class block_contextoutcomes extends object
     * @var string $title The title of the block
     */
     public $title;
-    
+
     /**
     * @var object $objLanguage String to hold the language object
     */
@@ -77,7 +77,7 @@ class block_contextoutcomes extends object
     */
     public function init()
     {
-        
+
         try {
             $this->objLanguage =  $this->getObject('language', 'language');
             $this->title = ucWords($this->objLanguage->code2Txt('mod_context_learneroutcomes', 'context'));
@@ -85,7 +85,7 @@ class block_contextoutcomes extends object
             customException::cleanUp();
         }
     }
-    
+
     /**
     * Standard block show method.
     */
@@ -93,7 +93,14 @@ class block_contextoutcomes extends object
     {
         $objWashout = $this->getObject('washout', 'utilities');
         $objContext = $this->getObject('dbcontext');
-        return $objWashout->parseText($objContext->getGoals());
+        $contextCode = $objContext->getContextCode();
+        $objDBLearnerOutcomes = $this->getObject('dbcontext_learneroutcomes', 'context');
+        $contextLO = $objDBLearnerOutcomes->getContextOutcomes($contextCode);
+        $s = "";
+        foreach ($contextLO as $LO){
+            $s .= "<p>" . $LO["learningoutcome"] . "</p>";
+        }
+        return $objWashout->parseText($s); //$objContext->getGoals()
     }
 }
 ?>
