ba04231c58d0070154ad18d946d1d332b2fe202f
dkeats <dkeats@gmail.com>
2012-04-28 08:04:19 +0000

    Added divs for improved skinnability

--- app/core_modules/contextadmin/templates/content/main.php
+++ app/core_modules/contextadmin/templates/content/main.php
@@ -1,12 +1,12 @@
 <?php
-
+$ret = "";
 $this->loadClass('htmlheading', 'htmlelements');
 
 $header = new htmlheading();
 $header->type = 1;
 $header->str = ucwords($title);
 
-echo $header->show();
+$ret .= $header->show();
 
 
 if ($this->getParam('message') != '' ) {
@@ -21,34 +21,32 @@ if ($this->getParam('message') != '' ) {
             $title = $this->objContext->getTitle($contextcode);
             if(empty($title))
                 $title = $this->objContext->getMenuText($contextcode);
-            $message = $title.' '.$this->objLanguage->languageText('mod_contextadmin_successfullyupdated', 'contextadmin', 'has been successfully updated').'!';
+                $message = $title.' '.$this->objLanguage->languageText('mod_contextadmin_successfullyupdated', 'contextadmin', 'has been successfully updated').'!';
             break;
         case 'contextdeleted':
             $message = $this->getParam('title').' '.$this->objLanguage->languageText('mod_contextadmin_hasbeendeleted', 'contextadmin', 'has been successfully deleted');
             break;
     }
-    
+
     if ($message != '' ) {
-        
         $this->setSession('displayconfirmationmessage', FALSE);
-        
         $timeoutMessage = $this->getObject('timeoutmessage', 'htmlelements');
         $timeoutMessage->setMessage($message);
         $timeoutMessage->timeout = 10000;
-        
-        echo '<p>'.$timeoutMessage->show().'</p>';
+        $ret .= '<p>'.$timeoutMessage->show().'</p>';
     }
 }
 
 if (isset($content)) {
-    echo $content;
+    $ret .= $content;
 } else {
     $objDisplayContext = $this->getObject('displaycontext', 'context');
-    
+
     foreach ($contexts as $context)
     {
-        echo $objDisplayContext->formatContextDisplayBlock($context);
+        $ret .= $objDisplayContext->formatContextDisplayBlock($context);
     }
 }
 
+echo "<div class='contextadmin_contextlist'>$ret</div>"
 ?>
