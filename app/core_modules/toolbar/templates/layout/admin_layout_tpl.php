bc821afb9b297d48f34b775bf6409402b6f01a40
dkeats <dkeats@gmail.com>
2012-04-30 08:21:29 +0000

    Added divs for skinning

--- app/core_modules/toolbar/templates/layout/admin_layout_tpl.php
+++ app/core_modules/toolbar/templates/layout/admin_layout_tpl.php
@@ -6,7 +6,6 @@
 /**
 * Layout template for the test module
 */
-
 $cssLayout = $this->newObject('csslayout', 'htmlelements');
 $leftMenu = $this->newObject('adminmenu','toolbar');
 $rightMenu = $this->getObject('userLoginHistory','security');
@@ -14,8 +13,12 @@ $tabpane = $this->newObject('tabpane', 'htmlelements');
 $objTable = $this->newObject('htmltable', 'htmlelements');
 //$tab = $this->newObject('tabbedbox', 'htmlelements');
 $cssLayout->setNumColumns(2);
-$cssLayout->setLeftColumnContent($leftMenu->show());
-$cssLayout->setMiddleColumnContent($this->getContent());
+
+$left = '<div class="toolbar_left">' . $leftMenu->show() . '</div>';
+$cssLayout->setLeftColumnContent($left);
+
+$ret = '<div class="toolbar_main">' . $this->getContent() . '</div>';
+$cssLayout->setMiddleColumnContent($ret);
 
 echo $cssLayout->show();
 ?>
