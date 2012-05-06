9fa63f59f000932b88c8e69889c6a5a9d31f5ad6
dkeats <dkeats@gmail.com>
2012-05-06 09:33:38 +0000

    Temporary fix to toolbar not working
                //$goto = $objBookmark->showGotoLink();
                $goto = "WORKING HERE...";

--- app/core_modules/toolbar/classes/toolbar_elearn_class_inc.php
+++ app/core_modules/toolbar/classes/toolbar_elearn_class_inc.php
@@ -17,47 +17,47 @@ if (!$GLOBALS['kewl_entry_point_run']){
 
 class toolbar_elearn extends object
 {
-    
-    
+
+
     private $menuItems;
-    
+
     private $default = 'home';
-    
+
     public $params;
-    
+
     /**
     * Method to construct the class
     */
     function init()
     {
-        
+
         $this->loadClass('link','htmlelements');
         $this->objContext = $this->getObject('dbcontext', 'context');
         $this->objUser = $this->getObject('user', 'security');
         $this->contextCode = $this->objContext->getContextCode();
-        
-        
+
+
         $this->objLanguage = $this->getObject('language', 'language');
-        
+
         $this->objModule = $this->getObject('modules','modulecatalogue');
-        
+
         $this->menuItems['home'] = array('text'=>$this->objLanguage->languageText('word_home', 'system', 'Home'), 'link'=>$this->uri(NULL, '_default'), 'class'=>'homelink');
     }
-    
+
     /**
      * Method to send any params the toolbar may need to send to the header
      * This toolbar doesn't need to, so it does nothing
      */
     public function getParams()
     {}
-    
+
     /**
      * Method to display the elearn toolbar
      */
     public function show()
     {
         $objBreadcrumbs = $this->getObject('breadcrumbs');
-        
+
         // Take Decision on this
         // Should something be shown to not logged in users
         if ($this->objUser->isLoggedIn()) {
@@ -65,37 +65,37 @@ class toolbar_elearn extends object
         } else {
             return '<div id="breadcrumbs">'.$objBreadcrumbs->show().'</div>';
         }
-        
-        
+
+
     }
-    
+
     /**
      * Method to generate the elearn toolbar menu items
      */
     private function generateMenu()
     {
         $isRegistered = $this->objModule->checkIfRegistered('personalspace');
-        
+
         if ($isRegistered) {
             $this->menuItems['personalspace'] = array('text'=>$this->objLanguage->languageText('mod_personalspace_myworkspace', 'personalspace', 'My Work Space'), 'link'=>$this->uri(NULL, 'personalspace'));
         }
-        
+
         if ($this->contextCode != '') {
             $this->menuItems['context'] = array('text'=>ucwords($this->objLanguage->code2Txt('mod_context_contexthome', 'context', NULL, '[-context-] Home')), 'link'=>$this->uri(NULL, 'context'), 'title'=>$this->objContext->getTitle());
         }
-        
+
         $this->menuItems['contextadmin'] = array('text'=>ucwords($this->objLanguage->code2Txt('phrase_mycourses', 'system', NULL, 'My [-contexts-]')), 'link'=>$this->uri(NULL, 'contextadmin'));
-        
+
         if ($this->objUser->isAdmin()) {
             $this->menuItems['admin'] = array('text'=>$this->objLanguage->languageText('category_admin', 'toolbar', 'Admin'), 'link'=>$this->uri(NULL, 'toolbar'));
         }
-        
-        
-        
+
+
+
         $this->determineDefault($this->getParam('module', '_default'));
         return $this->generateOutput();
     }
-    
+
     /**
      * Method to place the logout JavaScript
      */
@@ -109,7 +109,7 @@ class toolbar_elearn extends object
 jQuery('.homelink').attr('href', 'javascript:;');
 // Bind a confirmation to link
 jQuery('.homelink').bind('click', function() {
-        
+
         if (confirm('".$this->objLanguage->code2Txt('mod_toolbar_requestcontextlogoutoption', 'toolbar', NULL, 'Would you like to log out of this [-context-]')."?')) {
             window.location.href = '{$homeUri}';
         }
@@ -119,7 +119,7 @@ jQuery('.homelink').bind('click', function() {
 </script>
             ";
     }
-    
+
     /**
      * Method to determine which tab should be highlighted dependent on the current module being viewed
      * @param string $module Name of the Current Module
@@ -137,7 +137,7 @@ jQuery('.homelink').bind('click', function() {
             case 'context':
                 $this->default = 'context';
                 return;
-            
+
             case 'blog':
             case 'buddies':
             case 'homepage':
@@ -148,7 +148,7 @@ jQuery('.homelink').bind('click', function() {
             case 'userdetails':
                 $this->default = 'personalspace';
                 return;
-            
+
             case 'context':
                 $this->default = 'context';
                 return;
@@ -183,59 +183,60 @@ jQuery('.homelink').bind('click', function() {
             default:
                 break;
         }
-        
+
         if ($this->objModule->dependsContext($module)) {
             $this->default = 'context';
             return;
         }
-        
+
         if ($this->objModule->isContextAware($module) && $this->contextCode != '') {
             $this->default = 'context';
             return;
         }
-        
+
         return;
     }
-    
+
     /**
      * Method to generate the actual toolbar
      */
     private function generateOutput()
     {
         //See if the site map module is registered
-        
+
         $isRegistered = $this->objModule->checkIfRegistered('sitemap');
-        
+
         if ($isRegistered) {
             $this->menuItems['sitemap'] = array('text'=>$this->objLanguage->languageText('phrase_sitemap', 'sitemap', 'Site Map'), 'link'=>$this->uri(NULL, 'sitemap'));
         }
-        
+
         // Logout is always last
         $this->menuItems['logout'] = array('text'=>$this->objLanguage->languageText('word_logout', 'system', 'Logout'), 'link'=>$this->uri(array('action'=>'logoff'), 'security'));
-        
+
         $str = '<span class="glossymenu"><ul class="glossytabs">';
-        
+
         foreach ($this->menuItems as $menuItem=>$menuInfo)
         {
             $link = new link ($menuInfo['link']);
             $link->link = '<strong>'.$menuInfo['text'].'</strong>';
-            
+
             if (isset($menuInfo['title'])) {
                 $link->title = $menuInfo['title'];
             }
-            
+
             if (isset($menuInfo['class'])) {
                 $link->cssClass = $menuInfo['class'];
             }
-            
+
             $css = ($this->default == $menuItem) ? ' class="current"' : '';
             $str .= '<li '.$css.'>'.$link->show().'</li>';
         }
-        
+
         if ($this->objModule->checkIfRegistered('bookmarks'))
         {
             $objBookmark = $this->getObject('bookmarksops', 'bookmarks');
-            $goto = $objBookmark->showGotoLink();            
+            //$goto = $objBookmark->showGotoLink();
+            $goto = "WORKING HERE...";
             if (!empty($goto))
             {
                 $str .= '<li class="bookmark_icon">' . $goto . '</li>';
@@ -245,12 +246,12 @@ jQuery('.homelink').bind('click', function() {
         }
 
         $str .= '</ul></span>';
-        
+
         if ($this->contextCode != '') {
             $str .= $this->addContextLogoutJS();
         }
         return $str;
     }
-    
+
 }
 ?>
