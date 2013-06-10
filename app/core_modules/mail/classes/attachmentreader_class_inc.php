f9a08668524f06c5289cdc6c0c7c4bf6810f3fd6
Monwabisi Sifumba <wsifumba@gmail.com>
2013-06-10 09:18:15 +0200

    Added security check to attachment class

--- app/core_modules/mail/classes/attachmentreader_class_inc.php
+++ app/core_modules/mail/classes/attachmentreader_class_inc.php
@@ -1,4 +1,16 @@
 <?
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run'])
+{
+    die("You cannot view this page directly");
+}
+// end security check
 
 class AttachmentReader
 {
