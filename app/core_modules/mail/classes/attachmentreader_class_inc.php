60192739623da665c1a5d30ef665b84b55af273b
monwabisi sifumba <wsifumba@gmail.com>
2013-08-15 12:05:34 +0200

    Added inbox to email options

--- app/core_modules/mail/classes/attachmentreader_class_inc.php
+++ app/core_modules/mail/classes/attachmentreader_class_inc.php
@@ -23,7 +23,7 @@ class AttachmentReader {
          * Constructor - Establishes Connection to Email Host
          */
         function __construct($emailHost, $emailPort, $emailOptions, $emailLogin, $emailPassword, $catchAllBase=NULL) {
-                $hostString = "{{$emailHost}:{$emailPort}/{$emailOptions}}";
+                $hostString = "{{$emailHost}:{$emailPort}/{$emailOptions}} INBOX";
 
                 $this->mbox = imap_open($hostString, $emailLogin, $emailPassword, OP_SILENT) or die("can't connect: " . imap_last_error());
 
